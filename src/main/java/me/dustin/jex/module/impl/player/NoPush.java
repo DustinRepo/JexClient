bb2b9cc5c49d27e87ac44ef298ce03b8e1185e9a
DustinRepo <tinmann4300@gmail.com>
2021-03-18 12:46:49 -0700

    Edited NoPush to make it more future proof

--- src/main/java/me/dustin/jex/module/impl/player/NoPush.java
+++ src/main/java/me/dustin/jex/module/impl/player/NoPush.java
@@ -1,9 +1,9 @@
 package me.dustin.jex.module.impl.player;
 
+import me.dustin.events.core.Event;
 import me.dustin.events.core.annotate.EventListener;
-import me.dustin.jex.event.player.EventPlayerUpdates;
+import me.dustin.jex.event.player.EventPushAwayFromEntity;
 import me.dustin.jex.event.world.EventWaterVelocity;
-import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.module.core.Module;
 import me.dustin.jex.module.core.annotate.ModClass;
 import me.dustin.jex.module.core.enums.ModCategory;
@@ -17,37 +17,11 @@ public class NoPush extends Module {
     @Op(name = "Water")
     public boolean water = true;
 
-    private float savedPush = -999;
-
-    @EventListener(events = {EventPlayerUpdates.class})
-    private void runEvent(EventPlayerUpdates eventPlayerUpdates) {
-        if (eventPlayerUpdates.getMode() == EventPlayerUpdates.Mode.PRE) {
-
-            if (mobs) {
-                if (savedPush == -999)
-                    savedPush = Wrapper.INSTANCE.getLocalPlayer().pushSpeedReduction;
-
-                Wrapper.INSTANCE.getLocalPlayer().pushSpeedReduction = 1;
-            } else if (savedPush != -999) {
-                Wrapper.INSTANCE.getLocalPlayer().pushSpeedReduction = savedPush;
-                savedPush = -999;
-            }
-        }
+    @EventListener(events = {EventWaterVelocity.class, EventPushAwayFromEntity.class})
+    private void runMethod(Event event) {
+        if (water && event instanceof EventWaterVelocity)
+            event.cancel();
+        if (mobs && event instanceof EventPushAwayFromEntity)
+            event.cancel();
     }
-
-    @EventListener(events = {EventWaterVelocity.class})
-    private void runMethod(EventWaterVelocity eventWaterVelocity) {
-        if (water)
-            eventWaterVelocity.cancel();
-    }
-
-    @Override
-    public void onDisable() {
-        if (Wrapper.INSTANCE.getLocalPlayer() != null && savedPush != -999) {
-            Wrapper.INSTANCE.getLocalPlayer().pushSpeedReduction = savedPush;
-            savedPush = -999;
-        }
-        super.onDisable();
-    }
-
 }
