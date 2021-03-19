cff2dc7f4776c8d9070a391be84506674cbde199
DustinRepo <tinmann4300@gmail.com>
2021-03-18 18:52:40 -0700

    Added AirPlace

--- src/main/java/me/dustin/jex/module/impl/misc/MiddleClickFriend.java
+++ src/main/java/me/dustin/jex/module/impl/misc/MiddleClickFriend.java
@@ -1,11 +1,10 @@
 package me.dustin.jex.module.impl.misc;
 
 import me.dustin.events.core.annotate.EventListener;
-import me.dustin.jex.event.misc.EventTick;
+import me.dustin.jex.event.misc.EventMouseButton;
 import me.dustin.jex.file.FriendFile;
 import me.dustin.jex.friend.Friend;
 import me.dustin.jex.helper.misc.ChatHelper;
-import me.dustin.jex.helper.misc.MouseHelper;
 import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.module.core.Module;
 import me.dustin.jex.module.core.annotate.ModClass;
@@ -18,19 +17,12 @@ import net.minecraft.util.hit.HitResult;
 @ModClass(name = "MiddleClickFriend", category = ModCategory.MISC, description = "Middle click people to add them as friends.")
 public class MiddleClickFriend extends Module {
 
-    private boolean pressed;
-
-    @EventListener(events = {EventTick.class})
-    private void runMethod(EventTick eventTick) {
-        boolean currentlyPressed = MouseHelper.INSTANCE.isMouseButtonDown(2);
-        if (pressed && !currentlyPressed) {
-            pressed = false;
-        }
-        if (!pressed && currentlyPressed) {
+    @EventListener(events = {EventMouseButton.class})
+    private void runMethod(EventMouseButton eventMouseButton) {
+        if (eventMouseButton.getButton() == 2 && eventMouseButton.getClickType() == EventMouseButton.ClickType.IN_GAME) {
             HitResult hitResult = Wrapper.INSTANCE.getMinecraft().crosshairTarget;
 
             if (hitResult != null && hitResult.getType() == HitResult.Type.ENTITY) {
-                System.out.println("entity found");
                 Entity entity = ((EntityHitResult) hitResult).getEntity();
                 if (entity instanceof PlayerEntity) {
                     String name = entity.getName().asString();
@@ -45,7 +37,6 @@ public class MiddleClickFriend extends Module {
                     }
                 }
             }
-            pressed = true;
         }
     }
 
