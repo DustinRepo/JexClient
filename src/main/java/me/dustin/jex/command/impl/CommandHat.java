f8f876cd7c3ef6c64cc77e3564557bd353232e3b
DustinRepo <tinmann4300@gmail.com>
2021-04-03 09:58:40 -0700

    Added some creative commands

---
+++ src/main/java/me/dustin/jex/command/impl/CommandHat.java
@@ -0,0 +1,24 @@
+package me.dustin.jex.command.impl;
+
+import me.dustin.jex.command.core.Command;
+import me.dustin.jex.command.core.annotate.Cmd;
+import me.dustin.jex.helper.misc.ChatHelper;
+import me.dustin.jex.helper.misc.Wrapper;
+import me.dustin.jex.helper.network.NetworkHelper;
+import me.dustin.jex.helper.player.InventoryHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.c2s.play.CreativeInventoryActionC2SPacket;
+
+@Cmd(name = "Hat", description = "Put your current held item on your head (Creative only)", syntax = ".hat")
+public class CommandHat extends Command {
+
+    @Override
+    public void runCommand(String command, String[] args) {
+        if (!Wrapper.INSTANCE.getLocalPlayer().isCreative()) {
+            ChatHelper.INSTANCE.addClientMessage("This command is for creative mode only!");
+        }
+        ItemStack stack = InventoryHelper.INSTANCE.getInventory().getMainHandStack();
+
+        NetworkHelper.INSTANCE.sendPacket(new CreativeInventoryActionC2SPacket(5, stack));
+    }
+}
