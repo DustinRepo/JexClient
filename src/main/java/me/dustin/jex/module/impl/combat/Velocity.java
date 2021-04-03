8b9f23ddbc5d0474be635d61f0e7d8de6fa17f17
DustinRepo <tinmann4300@gmail.com>
2021-04-03 08:49:59 -0700

    Added percentage slider to Velocity

--- src/main/java/me/dustin/jex/module/impl/combat/Velocity.java
+++ src/main/java/me/dustin/jex/module/impl/combat/Velocity.java
@@ -37,7 +37,6 @@ public class Velocity extends Module {
                         }
                     }
                 } catch (Exception e) {
-                    e.printStackTrace();
                 }
             }
         }
