b48eb422ad98bee3fb63deb31b3f8bb9e26d8dcd
DustinRepo <tinmann4300@gmail.com>
2021-04-10 09:51:39 -0700

    Moved Attack option out of AutoTool to it's own mod

--- src/main/java/me/dustin/jex/module/impl/player/AutoTool.java
+++ src/main/java/me/dustin/jex/module/impl/player/AutoTool.java
@@ -1,8 +1,6 @@
 package me.dustin.jex.module.impl.player;
 
-import me.dustin.events.core.Event;
 import me.dustin.events.core.annotate.EventListener;
-import me.dustin.jex.event.player.EventAttackEntity;
 import me.dustin.jex.event.world.EventClickBlock;
 import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.helper.network.NetworkHelper;
@@ -10,33 +8,22 @@ import me.dustin.jex.helper.player.InventoryHelper;
 import me.dustin.jex.module.core.Module;
 import me.dustin.jex.module.core.annotate.ModClass;
 import me.dustin.jex.module.core.enums.ModCategory;
-import me.dustin.jex.option.annotate.Op;
-import me.dustin.jex.option.annotate.OpChild;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
-import net.minecraft.item.*;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
-
-import java.util.Map;
 
 @ModClass(name = "AutoTool", category = ModCategory.PLAYER, description = "Switch to the best tool for your needs.")
 public class AutoTool extends Module {
 
-    @Op(name = "Attack")
-    public boolean swords = true;
-    @OpChild(name = "Mode", all = {"Sword", "Sword&Axe", "All Tools"}, parent = "Attack")
-    public String mode = "Sword";
-
-    @EventListener(events = {EventClickBlock.class, EventAttackEntity.class})
-    public void run(Event event) {
+    @EventListener(events = {EventClickBlock.class})
+    public void run(EventClickBlock eventClickBlock) {
         if (AutoEat.isEating)
             return;
-        if (event instanceof EventClickBlock && !Wrapper.INSTANCE.getLocalPlayer().isCreative()) {
+        if (!Wrapper.INSTANCE.getLocalPlayer().isCreative()) {
             int slot = InventoryHelper.INSTANCE.getInventory().selectedSlot;
-            BlockState blockState = Wrapper.INSTANCE.getWorld().getBlockState(((EventClickBlock) event).getBlockPos());
+            BlockState blockState = Wrapper.INSTANCE.getWorld().getBlockState(eventClickBlock.getBlockPos());
             float best = 1;
             boolean found = false;
             for (int index = 0; index < 9; index++) {
@@ -53,8 +40,6 @@ public class AutoTool extends Module {
                             found = true;
                         }
                     }
-
-
                 }
             }
             if (slot == InventoryHelper.INSTANCE.getInventory().selectedSlot && !found) {
@@ -67,73 +52,6 @@ public class AutoTool extends Module {
                 InventoryHelper.INSTANCE.getInventory().selectedSlot = slot;
             }
         }
-        if (event instanceof EventAttackEntity && swords) {
-            EventAttackEntity eventAttackEntity = (EventAttackEntity) event;
-            int slot = -1;
-            float str = 1;
-            ItemStack stack = null;
-            for (int i = 0; i < 9; i++) {
-                ItemStack stackInSlot = InventoryHelper.INSTANCE.getInventory().getStack(i);
-                if (stackInSlot != null) {
-                    if (!isGoodItem(stackInSlot.getItem()))
-                        continue;
-                    float damage = getAdjustedDamage(stackInSlot);
-
-                    if (damage > str) {
-                        str = damage;
-                        slot = i;
-                        stack = stackInSlot;
-                    }
-                    if (damage == str && str != 1) {
-                        if (InventoryHelper.INSTANCE.compareEnchants(stack, stackInSlot, Enchantments.SHARPNESS)) {
-                            str = damage;
-                            slot = i;
-                            stack = stackInSlot;
-                        }
-                    }
-                }
-
-            }
-            if (slot != -1 && slot != InventoryHelper.INSTANCE.getInventory().selectedSlot) {
-                NetworkHelper.INSTANCE.sendPacket(new UpdateSelectedSlotC2SPacket(slot));
-                InventoryHelper.INSTANCE.getInventory().selectedSlot = slot;
-            }
-        }
-    }
-
-    private boolean isGoodItem(Item item) {
-        switch (mode.toLowerCase()) {
-            case "sword":
-                return item instanceof SwordItem;
-            case "sword&axe":
-                return item instanceof SwordItem || item instanceof AxeItem;
-            case "all tools":
-                return item instanceof ToolItem;
-        }
-        return false;
-    }
-
-    private float getAdjustedDamage(ItemStack itemStack) {
-        float damage = 1;
-        if (itemStack.getItem() instanceof SwordItem) {
-            SwordItem itemSword = (SwordItem) itemStack.getItem();
-            damage = itemSword.getAttackDamage();
-        } else if (itemStack.getItem() instanceof MiningToolItem) {
-            MiningToolItem miningToolItem = (MiningToolItem) itemStack.getItem();
-            damage = miningToolItem.getAttackDamage();
-        }
-        return damage + getSharpnessModifier(itemStack);
-    }
-
-    public float getSharpnessModifier(ItemStack itemStack) {
-        if (itemStack.hasEnchantments()) {
-            Map<Enchantment, Integer> equippedEnchants = EnchantmentHelper.get(itemStack);
-            if (equippedEnchants.containsKey(Enchantments.SHARPNESS)) {
-                int level = equippedEnchants.get(Enchantments.SHARPNESS);
-                return 0.5f * level + 0.5f;
-            }
-        }
-        return 0;
     }
 
     private int getNonDamageSlot() {
