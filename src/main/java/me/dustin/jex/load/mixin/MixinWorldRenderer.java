01fa92fe4ff4cc6927a21bf61f7c3239a43443a4
DustinRepo <tinmann4300@gmail.com>
2021-04-24 07:17:47 -0700

    Fixed ShaderESP throwing error on launch

--- src/main/java/me/dustin/jex/load/mixin/MixinWorldRenderer.java
+++ src/main/java/me/dustin/jex/load/mixin/MixinWorldRenderer.java
@@ -1,25 +1,30 @@
 package me.dustin.jex.load.mixin;
 
+import com.google.gson.JsonSyntaxException;
 import me.dustin.jex.event.render.EventBlockOutlineColor;
 import me.dustin.jex.event.render.EventRenderRain;
 import me.dustin.jex.event.render.EventWorldRender;
+import me.dustin.jex.feature.impl.render.esp.ESP;
 import me.dustin.jex.helper.render.Render2DHelper;
 import me.dustin.jex.load.impl.IWorldRenderer;
-import me.dustin.jex.feature.impl.render.esp.ESP;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.Framebuffer;
+import net.minecraft.client.gl.ShaderEffect;
 import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Matrix4f;
 import net.minecraft.util.shape.VoxelShape;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.awt.*;
+import java.io.IOException;
 
 @Mixin(WorldRenderer.class)
 public class MixinWorldRenderer implements IWorldRenderer {
@@ -28,6 +33,9 @@ public class MixinWorldRenderer implements IWorldRenderer {
     @Final
     private BufferBuilderStorage bufferBuilders;
 
+    @Shadow private @Nullable ShaderEffect entityOutlineShader;
+    @Shadow @Final private MinecraftClient client;
+    @Shadow private @Nullable Framebuffer entityOutlinesFramebuffer;
     private Identifier my_outline = new Identifier("jex", "shaders/entity_outline.json");
     private Identifier mojang_outline = new Identifier("shaders/post/entity_outline.json");
 
@@ -36,7 +44,25 @@ public class MixinWorldRenderer implements IWorldRenderer {
         new EventWorldRender(matrices, tickDelta).run();
     }
 
-    @Redirect(method = "loadEntityOutlineShader", at = @At(value = "NEW", target = "net/minecraft/util/Identifier"))
+    @Inject(method = "loadEntityOutlineShader", at = @At("HEAD"), cancellable = true)
+    public void loadEntityOutlineShader1(CallbackInfo ci) {
+        if (this.entityOutlineShader != null) {
+            this.entityOutlineShader.close();
+        }
+
+        Identifier identifier = getIDForOutline("shaders/post/entity_outline.json");
+
+        try {
+            this.entityOutlineShader = new ShaderEffect(this.client.getTextureManager(), this.client.getResourceManager(), this.client.getFramebuffer(), identifier);
+            this.entityOutlineShader.setupDimensions(this.client.getWindow().getFramebufferWidth(), this.client.getWindow().getFramebufferHeight());
+            this.entityOutlinesFramebuffer = this.entityOutlineShader.getSecondaryTarget("final");
+        } catch (IOException | JsonSyntaxException var3) {
+            this.entityOutlineShader = null;
+            this.entityOutlinesFramebuffer = null;
+        }
+        ci.cancel();
+    }
+
     public Identifier getIDForOutline(String id) {
         try {
             if (ESP.INSTANCE.getState() && ESP.INSTANCE.mode.equalsIgnoreCase("Shader")) {
