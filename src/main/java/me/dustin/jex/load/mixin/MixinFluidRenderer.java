9f399666d02f309c8a21be1ee9d60f9912fe85bb
DustinRepo <tinmann4300@gmail.com>
2021-04-24 06:44:28 -0700

    Fixed Xray with Sodium mod

--- src/main/java/me/dustin/jex/load/mixin/MixinFluidRenderer.java
+++ src/main/java/me/dustin/jex/load/mixin/MixinFluidRenderer.java
@@ -1,11 +1,15 @@
 package me.dustin.jex.load.mixin;
 
 import me.dustin.jex.event.render.EventRenderFluid;
+import me.dustin.jex.event.render.EventShouldDrawSide;
+import me.dustin.jex.helper.world.WorldHelper;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.block.FluidRenderer;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockRenderView;
+import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,6 +23,13 @@ public class MixinFluidRenderer {
         EventRenderFluid eventRenderFluid = new EventRenderFluid(state.getBlockState().getBlock()).run();
         if (eventRenderFluid.isCancelled())
             cir.setReturnValue(false);
+    }
+
+    @Inject(method = "isSideCovered", at = @At("HEAD"), cancellable = true)
+    private static void isSideCovered1(BlockView world, BlockPos pos, Direction direction, float maxDeviation, CallbackInfoReturnable<Boolean> cir) {
+        EventShouldDrawSide eventShouldDrawSide = new EventShouldDrawSide(WorldHelper.INSTANCE.getBlock(pos), pos).run();
+        if (eventShouldDrawSide.isCancelled())
+            cir.setReturnValue(!eventShouldDrawSide.isShouldDrawSide());
     }
 
 }
