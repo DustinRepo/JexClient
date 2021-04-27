781b4ae947b887710905982a8031dafdf7bad64d
DustinRepo <tinmann4300@gmail.com>
2021-04-27 11:39:16 -0700

    Added living only option to Criticals

--- src/main/java/me/dustin/jex/feature/impl/combat/Criticals.java
+++ src/main/java/me/dustin/jex/feature/impl/combat/Criticals.java
@@ -2,17 +2,20 @@ package me.dustin.jex.feature.impl.combat;
 
 import me.dustin.events.core.annotate.EventListener;
 import me.dustin.jex.event.player.EventAttackEntity;
-import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.feature.core.Feature;
 import me.dustin.jex.feature.core.annotate.Feat;
 import me.dustin.jex.feature.core.enums.FeatureCategory;
+import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.option.annotate.Op;
 import me.dustin.jex.option.annotate.OpChild;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
 @Feat(name = "Criticals", category = FeatureCategory.COMBAT, description = "Automatically deal critical strikes when attacking.")
 public class Criticals extends Feature {
 
+    @Op(name = "Living Only")
+    public boolean livingOnly = true;
     @Op(name = "Extra Particles")
     public boolean extraParticles;
     @OpChild(name = "Amount", min = 1, max = 20, inc = 1, parent = "Extra Particles")
@@ -20,6 +23,8 @@ public class Criticals extends Feature {
 
     @EventListener(events = {EventAttackEntity.class})
     private void runMethod(EventAttackEntity eventAttackEntity) {
+        if (livingOnly && !(eventAttackEntity.getEntity() instanceof LivingEntity))
+            return;
         if (extraParticles) {
             for (int i = 0; i < amount; i++) {
                 Wrapper.INSTANCE.getLocalPlayer().addCritParticles(eventAttackEntity.getEntity());
