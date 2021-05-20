a38e7cb3685b4400bc0569ef4df423336934ee6b
DustinRepo <tinmann4300@gmail.com>
2021-05-20 12:49:58 -0700

    Update to Jex Version

--- src/main/java/me/dustin/jex/JexClient.java
+++ src/main/java/me/dustin/jex/JexClient.java
@@ -42,7 +42,7 @@ import java.util.Arrays;
 
 public enum JexClient {
     INSTANCE;
-    private String version = "0.2.8";
+    private String version = "0.2.9";
     private boolean autoSaveModules = false;
     private boolean soundOnLaunch = true;
 
