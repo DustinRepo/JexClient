5ad2e957dd98cd211e6a5badc8ee04107658aa24
Dustin <tinmann4300@gmail.com>
2021-06-10 07:39:49 -0700

    Fixed Item rendering not showing damage bar

--- src/main/java/me/dustin/jex/helper/render/Render2DHelper.java
+++ src/main/java/me/dustin/jex/helper/render/Render2DHelper.java
@@ -295,39 +295,37 @@ public enum Render2DHelper {
     public void renderGuiItemOverlay(TextRenderer renderer, ItemStack stack, float x, float y, float scale, @Nullable String countLabel) {
         if (!stack.isEmpty()) {
             MatrixStack matrixStack = new MatrixStack();
-            matrixStack.scale(scale, scale, scale);
             if (stack.getCount() != 1 || countLabel != null) {
                 String string = countLabel == null ? String.valueOf(stack.getCount()) : countLabel;
-                matrixStack.translate(0.0D, 0.0D, (double) (Wrapper.INSTANCE.getMinecraft().getItemRenderer().zOffset + 200.0F));
+                matrixStack.translate(0.0D, 0.0D, (double)(Wrapper.INSTANCE.getMinecraft().getItemRenderer().zOffset + 200.0F));
                 VertexConsumerProvider.Immediate immediate = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
-                renderer.draw(string, (float) (x + 19 - 2 - renderer.getWidth(string)), (float) (y + 6 + 3), 16777215, true, matrixStack.peek().getModel(), immediate, false, 0, 15728880);
+                renderer.draw(string, (float)(x + 19 - 2 - renderer.getWidth(string)), (float)(y + 6 + 3), 16777215, true, matrixStack.peek().getModel(), immediate, false, 0, 15728880);
                 immediate.draw();
             }
+
             if (stack.isItemBarVisible()) {
                 RenderSystem.disableDepthTest();
                 RenderSystem.disableTexture();
                 RenderSystem.disableBlend();
-                Tessellator tessellator = Tessellator.getInstance();
-                BufferBuilder bufferBuilder = tessellator.getBuffer();
                 int i = stack.getItemBarStep();
                 int j = stack.getItemBarColor();
-                this.renderGuiQuad(bufferBuilder, x + 2, y + 13, 13, 2, 0, 0, 0, 255);
-                this.renderGuiQuad(bufferBuilder, x + 2, y + 13, i, 1, j >> 16 & 255, j >> 8 & 255, j & 255, 255);
+                this.fill(matrixStack, x + 2, y + 13, x + 2 + 13, y + 13 + 2, 0xff000000);
+                this.fill(matrixStack, x + 2, y + 13, x + 2 + i, y + 13 + 1, new Color(j >> 16 & 255, j >> 8 & 255, j & 255, 255).getRGB());
                 RenderSystem.enableBlend();
                 RenderSystem.enableTexture();
                 RenderSystem.enableDepthTest();
             }
 
             ClientPlayerEntity clientPlayerEntity = MinecraftClient.getInstance().player;
-            float k = clientPlayerEntity == null ? 0.0F : clientPlayerEntity.getItemCooldownManager().getCooldownProgress(stack.getItem(), MinecraftClient.getInstance().getTickDelta());
-            if (k > 0.0F) {
+            float f = clientPlayerEntity == null ? 0.0F : clientPlayerEntity.getItemCooldownManager().getCooldownProgress(stack.getItem(), MinecraftClient.getInstance().getTickDelta());
+            if (f > 0.0F) {
                 RenderSystem.disableDepthTest();
                 RenderSystem.disableTexture();
                 RenderSystem.enableBlend();
                 RenderSystem.defaultBlendFunc();
                 Tessellator tessellator2 = Tessellator.getInstance();
                 BufferBuilder bufferBuilder2 = tessellator2.getBuffer();
-                this.renderGuiQuad(bufferBuilder2, x, y + MathHelper.floor(16.0F * (1.0F - k)), 16, MathHelper.ceil(16.0F * k), 255, 255, 255, 127);
+                this.renderGuiQuad(bufferBuilder2, x, y + MathHelper.floor(16.0F * (1.0F - f)), 16, MathHelper.ceil(16.0F * f), 255, 255, 255, 127);
                 RenderSystem.enableTexture();
                 RenderSystem.enableDepthTest();
             }
