6f8b85145780a2807ca087725718bd1d523a60d4
Dustin <tinmann4300@gmail.com>
2021-06-26 04:09:11 -0700

    Changed Blink to show a player

--- src/main/java/me/dustin/jex/feature/impl/player/Blink.java
+++ src/main/java/me/dustin/jex/feature/impl/player/Blink.java
@@ -1,65 +1,65 @@
 package me.dustin.jex.feature.impl.player;
 
-import me.dustin.events.core.Event;
+import com.mojang.authlib.GameProfile;
 import me.dustin.events.core.annotate.EventListener;
+import me.dustin.jex.entity.FakePlayerEntity;
 import me.dustin.jex.event.packet.EventPacketSent;
-import me.dustin.jex.event.render.EventRender3D;
-import me.dustin.jex.helper.math.ColorHelper;
-import me.dustin.jex.helper.misc.Wrapper;
-import me.dustin.jex.helper.network.NetworkHelper;
-import me.dustin.jex.helper.player.PlayerHelper;
-import me.dustin.jex.helper.render.Render3DHelper;
 import me.dustin.jex.feature.core.Feature;
 import me.dustin.jex.feature.core.annotate.Feat;
 import me.dustin.jex.feature.core.enums.FeatureCategory;
-import net.minecraft.client.render.*;
+import me.dustin.jex.helper.misc.Wrapper;
+import me.dustin.jex.helper.network.NetworkHelper;
+import me.dustin.jex.helper.player.PlayerHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
-import net.minecraft.util.math.Vec3d;
 
-import java.awt.*;
 import java.util.ArrayList;
+import java.util.UUID;
 
 @Feat(name = "Blink", category = FeatureCategory.PLAYER, description = "Delay your movements to the server, making it seem like you teleported.")
 public class Blink extends Feature {
 
     private ArrayList<PlayerMoveC2SPacket> packets = new ArrayList<>();
+    public static PlayerEntity playerEntity;
 
-    @EventListener(events = {EventPacketSent.class, EventRender3D.class})
-    private void runMethod(Event event) {
-        if (event instanceof EventPacketSent eventPacketSent) {
-            if (Wrapper.INSTANCE.getLocalPlayer() == null) {
-                packets.clear();
-                this.setState(false);
-                return;
+    @EventListener(events = {EventPacketSent.class})
+    private void runMethod(EventPacketSent eventPacketSent) {
+        if (Wrapper.INSTANCE.getLocalPlayer() == null) {
+            packets.clear();
+            this.setState(false);
+            return;
+        }
+        if (eventPacketSent.getPacket() instanceof PlayerMoveC2SPacket) {
+            if (PlayerHelper.INSTANCE.isMoving()) {
+                packets.add((PlayerMoveC2SPacket)eventPacketSent.getPacket());
             }
-            if (eventPacketSent.getPacket() instanceof PlayerMoveC2SPacket) {
-                if (PlayerHelper.INSTANCE.isMoving()) {
-                    packets.add((PlayerMoveC2SPacket)eventPacketSent.getPacket());
-                }
-                eventPacketSent.cancel();
-            }
-        } else if (event instanceof EventRender3D) {
-            if (packets.size() > 1) {
-                Color color = ColorHelper.INSTANCE.getColor(ColorHelper.INSTANCE.getClientColor());
-                Render3DHelper.INSTANCE.setup3DRender(true);
-                BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
-                bufferBuilder.begin(VertexFormat.DrawMode.DEBUG_LINES, VertexFormats.POSITION_COLOR);
-                for (PlayerMoveC2SPacket packet : packets) {
-                    Vec3d renderPos = Render3DHelper.INSTANCE.getRenderPosition(new Vec3d(packet.getX(0), packet.getY(0), packet.getZ(0)));
-                    bufferBuilder.vertex(renderPos.getX(), renderPos.getY(), renderPos.getZ()).color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).next();
-                }
-                bufferBuilder.end();
-                BufferRenderer.draw(bufferBuilder);
-                Render3DHelper.INSTANCE.end3DRender();
-            }
-            this.setSuffix(String.valueOf(packets.size()));
+            eventPacketSent.cancel();
         }
     }
+
+    @Override
+    public void onEnable() {
+        if (Wrapper.INSTANCE.getLocalPlayer() != null) {
+            playerEntity = new FakePlayerEntity(Wrapper.INSTANCE.getWorld(), new GameProfile(UUID.randomUUID(), Wrapper.INSTANCE.getMinecraft().getSession().getUsername()));
+            playerEntity.copyFrom(Wrapper.INSTANCE.getLocalPlayer());
+            playerEntity.copyPositionAndRotation(Wrapper.INSTANCE.getLocalPlayer());
+            Wrapper.INSTANCE.getWorld().addEntity(42069, playerEntity);
+        }
+        super.onEnable();
+    }
+
 
     @Override
     public void onDisable() {
         super.onDisable();
         packets.forEach(NetworkHelper.INSTANCE::sendPacket);
         packets.clear();
+        if (playerEntity != null) {
+            playerEntity.setPos(Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+            if (Wrapper.INSTANCE.getWorld() != null)
+                Wrapper.INSTANCE.getWorld().removeEntity(playerEntity.getId(), Entity.RemovalReason.DISCARDED);
+            playerEntity = null;
+        }
     }
 }
