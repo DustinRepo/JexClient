a18b8b3f58677b542970dbccab400b03ae02da85
Dustin <tinmann4300@gmail.com>
2021-07-04 13:28:10 -0700

    Fixed AutoTotem attempting to place totems in off-hand while a screen is open

--- src/main/java/me/dustin/jex/feature/impl/combat/AutoTotem.java
+++ src/main/java/me/dustin/jex/feature/impl/combat/AutoTotem.java
@@ -2,11 +2,13 @@ package me.dustin.jex.feature.impl.combat;
 
 import me.dustin.events.core.annotate.EventListener;
 import me.dustin.jex.event.player.EventPlayerPackets;
+import me.dustin.jex.feature.core.Feature;
 import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.helper.player.InventoryHelper;
-import me.dustin.jex.feature.core.Feature;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.screen.slot.SlotActionType;
 
 @Feature.Manifest(name = "AutoTotem", category = Feature.Category.COMBAT, description = "Keep a Totem in your offhand at all times.")
@@ -15,6 +17,8 @@ public class AutoTotem extends Feature {
     @EventListener(events = {EventPlayerPackets.class})
     public void runEvent(EventPlayerPackets event) {
         if (event.getMode() == EventPlayerPackets.Mode.PRE) {
+            if (!(Wrapper.INSTANCE.getLocalPlayer().currentScreenHandler instanceof PlayerScreenHandler) || Wrapper.INSTANCE.getMinecraft().currentScreen instanceof InventoryScreen)
+                return;
             if (Wrapper.INSTANCE.getLocalPlayer().getOffHandStack() == null || Wrapper.INSTANCE.getLocalPlayer().getOffHandStack().getItem() == Items.AIR && getFirstTotem() != -1) {
                 InventoryHelper.INSTANCE.windowClick(Wrapper.INSTANCE.getLocalPlayer().currentScreenHandler, getFirstTotem() < 9 ? getFirstTotem() + 36 : getFirstTotem(), SlotActionType.PICKUP);
                 InventoryHelper.INSTANCE.windowClick(Wrapper.INSTANCE.getLocalPlayer().currentScreenHandler, 45, SlotActionType.PICKUP);
