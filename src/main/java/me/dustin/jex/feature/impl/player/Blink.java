7eb6d2fe05bd9aee7e50e61e6184b34066883465
Dustin <tinmann4300@gmail.com>
2021-07-09 09:30:01 -0700

    Added Buffer Packets mode to Blink

--- src/main/java/me/dustin/jex/feature/impl/player/Blink.java
+++ src/main/java/me/dustin/jex/feature/impl/player/Blink.java
@@ -4,10 +4,13 @@ import com.mojang.authlib.GameProfile;
 import me.dustin.events.core.annotate.EventListener;
 import me.dustin.jex.entity.FakePlayerEntity;
 import me.dustin.jex.event.packet.EventPacketSent;
+import me.dustin.jex.event.player.EventPlayerPackets;
 import me.dustin.jex.feature.core.Feature;
 import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.helper.network.NetworkHelper;
 import me.dustin.jex.helper.player.PlayerHelper;
+import me.dustin.jex.option.annotate.Op;
+import me.dustin.jex.option.annotate.OpChild;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
@@ -18,17 +21,23 @@ import java.util.UUID;
 @Feature.Manifest(name = "Blink", category = Feature.Category.PLAYER, description = "Delay your movements to the server, making it seem like you teleported.")
 public class Blink extends Feature {
 
+    @Op(name = "Buffer Packets")
+    public boolean bufferPackets = true;
+    @OpChild(name = "Send Amount PT", min = 5, max = 50, parent = "Buffer Packets")
+    public int amountPT = 25;
+
     private ArrayList<PlayerMoveC2SPacket> packets = new ArrayList<>();
     public static PlayerEntity playerEntity;
+    private boolean stopCatching;
 
     @EventListener(events = {EventPacketSent.class})
     private void runMethod(EventPacketSent eventPacketSent) {
-        if (Wrapper.INSTANCE.getLocalPlayer() == null) {
+        if (Wrapper.INSTANCE.getLocalPlayer() == null || (packets.isEmpty() && stopCatching)) {
             packets.clear();
             this.setState(false);
             return;
         }
-        if (eventPacketSent.getPacket() instanceof PlayerMoveC2SPacket) {
+        if (!stopCatching && eventPacketSent.getPacket() instanceof PlayerMoveC2SPacket) {
             if (PlayerHelper.INSTANCE.isMoving()) {
                 packets.add((PlayerMoveC2SPacket)eventPacketSent.getPacket());
             }
@@ -36,8 +45,19 @@ public class Blink extends Feature {
         }
     }
 
+    @EventListener(events = {EventPlayerPackets.class})
+    private void runPlayerPackets(EventPlayerPackets eventPlayerPackets) {
+        if (eventPlayerPackets.getMode() == EventPlayerPackets.Mode.PRE) {
+            if (stopCatching && !packets.isEmpty()) {
+                for (int i = 0; i < amountPT; i++) {
+                    NetworkHelper.INSTANCE.sendPacket(packets.get(i));
+                }
+            }
+        }
+    }
     @Override
     public void onEnable() {
+        stopCatching = false;
         if (Wrapper.INSTANCE.getLocalPlayer() != null) {
             playerEntity = new FakePlayerEntity(Wrapper.INSTANCE.getWorld(), new GameProfile(UUID.randomUUID(), Wrapper.INSTANCE.getMinecraft().getSession().getUsername()));
             playerEntity.copyFrom(Wrapper.INSTANCE.getLocalPlayer());
@@ -50,8 +70,11 @@ public class Blink extends Feature {
 
     @Override
     public void onDisable() {
+        stopCatching = true;
+        if (!bufferPackets || packets.isEmpty())
         super.onDisable();
-        packets.forEach(NetworkHelper.INSTANCE::sendPacket);
+        if (!bufferPackets)
+            packets.forEach(NetworkHelper.INSTANCE::sendPacket);
         packets.clear();
         if (playerEntity != null) {
             playerEntity.setPos(Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
