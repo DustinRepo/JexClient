4eb3696460e53f03239b2504167de9523ca09ea2
Dustin <tinmann4300@gmail.com>
2021-08-05 13:23:36 -0700

    Reworked Announcer

--- src/main/java/me/dustin/jex/feature/mod/impl/misc/Announcer.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/misc/Announcer.java
@@ -1,9 +1,17 @@
 package me.dustin.jex.feature.mod.impl.misc;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+
 import me.dustin.events.core.annotate.EventListener;
+import me.dustin.jex.JexClient;
+import me.dustin.jex.event.packet.EventPacketReceive;
 import me.dustin.jex.event.player.EventPlayerPackets;
+import me.dustin.jex.feature.mod.core.Feature;
+import me.dustin.jex.feature.option.annotate.Op;
 import me.dustin.jex.helper.file.FileHelper;
 import me.dustin.jex.helper.file.JsonHelper;
 import me.dustin.jex.helper.file.ModFileHelper;
@@ -12,13 +20,10 @@ import me.dustin.jex.helper.misc.ChatHelper;
 import me.dustin.jex.helper.misc.Timer;
 import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.helper.network.NetworkHelper;
-import me.dustin.jex.feature.mod.core.Feature;
-import me.dustin.jex.feature.option.annotate.Op;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
-
-import java.io.File;
-import java.util.ArrayList;
+import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
+import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket.Entry;
 
 @Feature.Manifest(name = "Announcer", category = Feature.Category.MISC, description = "Fastest way to get muted! Fully customizable with files in the Jex folder")
 public class Announcer extends Feature {
@@ -31,61 +36,37 @@ public class Announcer extends Feature {
     private ArrayList<String> joinMessages = new ArrayList<>();
     private ArrayList<String> leaveMessages = new ArrayList<>();
 
-    private ArrayList<String> playerNames = new ArrayList<>();
-
     private Timer timer = new Timer();
-
-    @EventListener(events = {EventPlayerPackets.class})
-    private void runMethod(EventPlayerPackets eventPlayerPackets) {
-        if (eventPlayerPackets.getMode() != EventPlayerPackets.Mode.PRE)
-            return;
-        if (Wrapper.INSTANCE.getLocalPlayer().age < 30 || !timer.hasPassed(messageDelay)) {
-            copyPlayerNames();
-            return;
-        }
-        ArrayList<String> currentNames = getPlayerNames();
-        for (String name : playerNames) {
-            if (!currentNames.contains(name) && !name.equalsIgnoreCase(Wrapper.INSTANCE.getLocalPlayer().getGameProfile().getName())) {//someone left
-                int rand = ClientMathHelper.INSTANCE.getRandom(leaveMessages.size());
-                NetworkHelper.INSTANCE.sendPacket(new ChatMessageC2SPacket(leaveMessages.get(rand).replace("%player", name)));
-                timer.reset();
-            }
-        }
-        for (String name : currentNames) {
-            if (!playerNames.contains(name) && !name.equalsIgnoreCase(Wrapper.INSTANCE.getLocalPlayer().getGameProfile().getName())) {//someone joined
-                int rand = ClientMathHelper.INSTANCE.getRandom(joinMessages.size());
-                NetworkHelper.INSTANCE.sendPacket(new ChatMessageC2SPacket(joinMessages.get(rand).replace("%player", name)));
-                timer.reset();
-            }
-        }
-        copyPlayerNames();
-    }
-
-    private ArrayList<String> getPlayerNames() {
-        ArrayList<String> names = new ArrayList<>();
-        for (PlayerListEntry playerListEntry : Wrapper.INSTANCE.getMinecraft().getNetworkHandler().getPlayerList()) {
-            if (playerListEntry == null || playerListEntry.getProfile() == null || playerListEntry.getProfile() == Wrapper.INSTANCE.getLocalPlayer().getGameProfile())
-                continue;
-            names.add(playerListEntry.getProfile().getName());
-        }
-        return names;
-    }
-
-    private void copyPlayerNames() {
-        if (Wrapper.INSTANCE.getMinecraft().getNetworkHandler() == null)
-            return;
-        playerNames.clear();
-        for (PlayerListEntry playerListEntry : Wrapper.INSTANCE.getMinecraft().getNetworkHandler().getPlayerList()) {
-            if (playerListEntry == null || playerListEntry.getProfile() == null || playerListEntry.getProfile() == Wrapper.INSTANCE.getLocalPlayer().getGameProfile())
-                continue;
-            playerNames.add(playerListEntry.getProfile().getName());
-        }
+    
+    @EventListener(events = {EventPacketReceive.class})
+    private void runMethod(EventPacketReceive eventPacketReceive) {
+    	if (eventPacketReceive.getPacket() instanceof PlayerListS2CPacket playerListPacket) {
+            if (Wrapper.INSTANCE.getLocalPlayer().age < 30 || !timer.hasPassed(messageDelay))
+            	return;
+            
+    		if (playerListPacket.getAction() == PlayerListS2CPacket.Action.REMOVE_PLAYER) {
+    			Entry entry = playerListPacket.getEntries().get(0);
+    			if (entry != null) {
+    				String name = entry.getProfile().getName();
+					int rand = ClientMathHelper.INSTANCE.getRandom(leaveMessages.size());
+		            NetworkHelper.INSTANCE.sendPacket(new ChatMessageC2SPacket(leaveMessages.get(rand).replace("%player", name)));
+		            timer.reset();
+    			}
+    		} else if (playerListPacket.getAction() == PlayerListS2CPacket.Action.ADD_PLAYER) {
+    			Entry entry = playerListPacket.getEntries().get(0);
+    			if (entry != null) {
+    				String name = entry.getProfile().getName();
+    				int rand = ClientMathHelper.INSTANCE.getRandom(joinMessages.size());
+                    NetworkHelper.INSTANCE.sendPacket(new ChatMessageC2SPacket(joinMessages.get(rand).replace("%player", name)));
+                    timer.reset();
+    			}
+    		}
+    	}
     }
 
     @Override
     public void onEnable() {
         loadFiles();
-        copyPlayerNames();
         super.onEnable();
     }
 
