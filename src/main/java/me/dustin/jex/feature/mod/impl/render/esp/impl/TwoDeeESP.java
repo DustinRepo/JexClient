8806ef47f44672ccbfc34affe377493e38ec443a
Dustin <tinmann4300@gmail.com>
2021-08-15 14:38:59 -0700

    Changed look of 2D ESP

--- src/main/java/me/dustin/jex/feature/mod/impl/render/esp/impl/TwoDeeESP.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/render/esp/impl/TwoDeeESP.java
@@ -18,6 +18,7 @@ import net.minecraft.client.render.BufferRenderer;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexFormat;
+import net.minecraft.client.render.VertexFormat.DrawMode;
 import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
@@ -79,6 +80,37 @@ public class TwoDeeESP extends FeatureExtension {
             });
             bufferBuilder.end();
             BufferRenderer.draw(bufferBuilder);
+            bufferBuilder.begin(VertexFormat.DrawMode.DEBUG_LINES, VertexFormats.POSITION_COLOR);
+            headPos.keySet().forEach(entity -> {
+                Vec3d top = headPos.get(entity);
+                Vec3d bottom = footPos.get(entity);
+                if (Render2DHelper.INSTANCE.isOnScreen(top) && Render2DHelper.INSTANCE.isOnScreen(bottom)) {
+                    float x = (float) top.x;
+                    float y = (float) top.y;
+                    float x2 = (float) bottom.x;
+                    float y2 = (float) bottom.y;
+                    if (y > y2) {
+                        float saved = y;
+                        y = y2;
+                        y2 = saved;
+                    }
+                    if (x > x2) {
+                        float saved = x;
+                        x = x2;
+                        x2 = saved;
+                    }
+                    float dif = Math.abs(y2 - y);
+
+                    if (entity instanceof ItemEntity)
+                        dif /= 2;
+                    else
+                        dif /= ClientMathHelper.INSTANCE.clamp(entity.getWidth() * 5f, 1f, 10f);
+                    outlineBox(eventRender2D.getMatrixStack(), x - dif, y + 1, x2 + dif, y2, entity);
+                }
+            });
+            bufferBuilder.end();
+            BufferRenderer.draw(bufferBuilder);
+            
             Render2DHelper.INSTANCE.end2DRender();
         }
     }
@@ -94,6 +126,28 @@ public class TwoDeeESP extends FeatureExtension {
         RenderSystem.setShader(GameRenderer::getPositionColorShader);
         bufferBuilder.vertex(matrix, (float)x, (float)y2, 0.0F).color(g, h, k, f).next();
         bufferBuilder.vertex(matrix, (float)x2, (float)y2, 0.0F).color(g, h, k, f).next();
+        bufferBuilder.vertex(matrix, (float)x2, (float)y, 0.0F).color(g, h, k, f).next();
+        bufferBuilder.vertex(matrix, (float)x, (float)y, 0.0F).color(g, h, k, f).next();
+    }
+    
+    public void outlineBox(MatrixStack matrixStack, float x, float y, float x2, float y2, Entity entity) {
+        int color = 0xff000000;
+        Matrix4f matrix = matrixStack.peek().getModel();
+        BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+        float f = (float)(color >> 24 & 255) / 255.0F;
+        float g = (float)(color >> 16 & 255) / 255.0F;
+        float h = (float)(color >> 8 & 255) / 255.0F;
+        float k = (float)(color & 255) / 255.0F;
+        RenderSystem.setShader(GameRenderer::getPositionColorShader);
+        bufferBuilder.vertex(matrix, (float)x, (float)y, 0.0F).color(g, h, k, f).next();
+        bufferBuilder.vertex(matrix, (float)x, (float)y2, 0.0F).color(g, h, k, f).next();
+
+        bufferBuilder.vertex(matrix, (float)x, (float)y2, 0.0F).color(g, h, k, f).next();
+        bufferBuilder.vertex(matrix, (float)x2, (float)y2, 0.0F).color(g, h, k, f).next();
+
+        bufferBuilder.vertex(matrix, (float)x2, (float)y2, 0.0F).color(g, h, k, f).next();
+        bufferBuilder.vertex(matrix, (float)x2, (float)y, 0.0F).color(g, h, k, f).next();
+        
         bufferBuilder.vertex(matrix, (float)x2, (float)y, 0.0F).color(g, h, k, f).next();
         bufferBuilder.vertex(matrix, (float)x, (float)y, 0.0F).color(g, h, k, f).next();
     }
