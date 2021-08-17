8db60288992a66a265c1359c5110d6850ba88c53
DustinRepo <tinmann4300@gmail.com>
2021-08-17 16:31:33 -0700

    Sanity Check for disabling LogOutSpot

--- src/main/java/me/dustin/jex/feature/mod/impl/world/LogOutSpot.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/world/LogOutSpot.java
@@ -77,10 +77,12 @@ public class LogOutSpot extends Feature {
 	
 	@Override
 	public void onDisable() {
-		fakePlayers.forEach(fakePlayer -> {
-			fakePlayer.setPos(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
-			Wrapper.INSTANCE.getWorld().removeEntity(fakePlayer.getId(), RemovalReason.DISCARDED);
-		});
+		if (Wrapper.INSTANCE.getWorld() != null) {
+			fakePlayers.forEach(fakePlayer -> {
+				fakePlayer.setPos(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+				Wrapper.INSTANCE.getWorld().removeEntity(fakePlayer.getId(), RemovalReason.DISCARDED);
+			});
+		}
 		fakePlayers.clear();
 		logOutList.clear();
 		super.onDisable();
