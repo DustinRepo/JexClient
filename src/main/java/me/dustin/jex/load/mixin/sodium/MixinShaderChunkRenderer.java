a6b41b4b9ac9da56354f1aa50575d3e1a4cef7f4
DustinRepo <tinmann4300@gmail.com>
2021-08-28 18:04:05 -0700

    Updated sodium support to 0.3.1

--- src/main/java/me/dustin/jex/load/mixin/sodium/MixinShaderChunkRenderer.java
+++ src/main/java/me/dustin/jex/load/mixin/sodium/MixinShaderChunkRenderer.java
@@ -1,60 +1,63 @@
 package me.dustin.jex.load.mixin.sodium;
 
-import me.dustin.jex.JexClient;
 import me.dustin.jex.event.render.EventSodiumBeginShader;
-import me.dustin.jex.feature.mod.impl.world.xray.impl.OpacityXray;
-import me.jellysquid.mods.sodium.client.gl.device.RenderDevice;
-import me.jellysquid.mods.sodium.client.gl.shader.*;
 import me.jellysquid.mods.sodium.client.render.chunk.ShaderChunkRenderer;
 import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPass;
-import me.jellysquid.mods.sodium.client.render.chunk.shader.ChunkProgram;
-import me.jellysquid.mods.sodium.client.render.chunk.shader.ChunkShaderBindingPoints;
-import me.jellysquid.mods.sodium.client.render.chunk.shader.ChunkShaderOptions;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Pseudo
 @Mixin(value = ShaderChunkRenderer.class, remap = false)
 public class MixinShaderChunkRenderer {
 
-    @Shadow protected ChunkProgram activeProgram;
-
-    @Inject(method = "createShader", at = @At("HEAD"), cancellable = true)
-    public void createShaderOverride(RenderDevice device, String path, ChunkShaderOptions options, CallbackInfoReturnable<ChunkProgram> cir) {
+    /*@Inject(method = "createShader", at = @At("HEAD"), cancellable = true)
+    public void createShaderOverride(String path, ChunkShaderOptions options, CallbackInfoReturnable<GlProgram<ChunkShaderInterface>> cir) {
         if ("blocks/block_layer_translucent".equalsIgnoreCase(path)) {
             ShaderConstants constants = options.constants();
             GlShader vertShader = ShaderLoader.loadShader(ShaderType.VERTEX, new Identifier("sodium", path + ".vsh"), constants);
             GlShader fragShader = ShaderLoader.loadShader(ShaderType.FRAGMENT, new Identifier("jex", path + ".fsh"), constants);
 
-            ChunkProgram var7;
-            GlProgram builder;
+            GlProgram<ChunkShaderInterface> var7;
+            GlProgram<ChunkShaderInterface> builder;
             try {
-                builder = GlProgram.builder(new Identifier("jex", "xray_shader")).attachShader(vertShader).attachShader(fragShader).bindAttribute("a_Pos", ChunkShaderBindingPoints.ATTRIBUTE_POSITION_ID).bindAttribute("a_Color", ChunkShaderBindingPoints.ATTRIBUTE_COLOR).bindAttribute("a_TexCoord", ChunkShaderBindingPoints.ATTRIBUTE_BLOCK_TEXTURE).bindAttribute("a_LightCoord", ChunkShaderBindingPoints.ATTRIBUTE_LIGHT_TEXTURE).bindFragmentData("fragColor", ChunkShaderBindingPoints.FRAG_COLOR).build((name) -> {
-                    return new ChunkProgram(device, name, options);
+                builder = GlProgram.builder(new Identifier("jex", "xray_shader")).attachShader(vertShader).attachShader(fragShader).bindAttribute("a_Pos", ChunkShaderBindingPoints.ATTRIBUTE_POSITION_ID).bindAttribute("a_Color", ChunkShaderBindingPoints.ATTRIBUTE_COLOR).bindAttribute("a_TexCoord", ChunkShaderBindingPoints.ATTRIBUTE_BLOCK_TEXTURE).bindAttribute("a_LightCoord", ChunkShaderBindingPoints.ATTRIBUTE_LIGHT_TEXTURE).bindFragmentData("fragColor", ChunkShaderBindingPoints.FRAG_COLOR).link((shader) -> {
+                    return new ChunkShaderInterface(shader, options);
                 });
-                var7 = (ChunkProgram) builder;
+                var7 = (GlProgram<ChunkShaderInterface>) builder;
             } finally {
                 vertShader.delete();
                 fragShader.delete();
             }
 
-            OpacityXray.alphaLocation = builder.getUniformLocation("Alpha");
+            OpacityXray.alphaLocation = builder.getInterface("Alpha");
             if (OpacityXray.alphaLocation < 0) {
                 JexClient.INSTANCE.getLogger().error("Couldn't find Alpha uniform! " + OpacityXray.alphaLocation);
             }
             cir.setReturnValue(var7);
+
+            ShaderConstants constants = options.constants();
+            GlShader vertShader = ShaderLoader.loadShader(ShaderType.VERTEX, new class_2960("sodium", path + ".vsh"), constants);
+            GlShader fragShader = ShaderLoader.loadShader(ShaderType.FRAGMENT, new class_2960("sodium", path + ".fsh"), constants);
+
+            GlProgram var6;
+            try {
+                var6 = GlProgram.builder(new class_2960("sodium", "chunk_shader")).attachShader(vertShader).attachShader(fragShader).bindAttribute("a_Pos", ChunkShaderBindingPoints.ATTRIBUTE_POSITION_ID).bindAttribute("a_Color", ChunkShaderBindingPoints.ATTRIBUTE_COLOR).bindAttribute("a_TexCoord", ChunkShaderBindingPoints.ATTRIBUTE_BLOCK_TEXTURE).bindAttribute("a_LightCoord", ChunkShaderBindingPoints.ATTRIBUTE_LIGHT_TEXTURE).bindFragmentData("fragColor", ChunkShaderBindingPoints.FRAG_COLOR).link((shader) -> {
+                    return new ChunkShaderInterface(shader, options);
+                });
+            } finally {
+                vertShader.delete();
+                fragShader.delete();
+            }
+
+            return var6;
         }
-    }
+    }*/
 
     @Inject(method = "begin", at = @At("RETURN"))
-    public void beginEnd(BlockRenderPass pass, MatrixStack matrixStack, CallbackInfo ci) {
+    public void beginEnd(BlockRenderPass pass, CallbackInfo ci) {
         new EventSodiumBeginShader().run();
     }
 }
