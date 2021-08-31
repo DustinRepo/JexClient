38e4c6e5e3b121b91ce3cfede7ce23ae411924bc
DustinRepo <tinmann4300@gmail.com>
2021-08-31 07:54:35 -0700

    Added modes to Derp

--- src/main/java/me/dustin/jex/feature/mod/impl/misc/Derp.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/misc/Derp.java
@@ -15,20 +15,48 @@ import java.util.Random;
 @Feature.Manifest(name = "Derp", category = Feature.Category.MISC, description = "Randomly look around for other players")
 public class Derp extends Feature {
 
+    @Op(name = "Mode", all = {"Random", "Pitch Roll", "Yaw Roll", "Both Roll"})
+    public String mode = "Random";
+    @Op(name = "Normalize Angles")
+    public boolean normalize = false;
     @Op(name = "Swing")
     public boolean swing = true;
     @OpChild(name = "Show Swing", parent = "Swing")
     public boolean showSwing = true;
 
+    private int yaw, pitch;
+
     @EventListener(events = {EventPlayerPackets.class})
     private void runMethod(EventPlayerPackets eventPlayerPackets) {
         Random random = new Random();
-        eventPlayerPackets.setYaw(random.nextFloat() * 180);
-        if (random.nextBoolean())
-            eventPlayerPackets.setYaw(-eventPlayerPackets.getYaw());
-        eventPlayerPackets.setPitch(random.nextFloat() * 90);
-        if (random.nextBoolean())
-            eventPlayerPackets.setPitch(-eventPlayerPackets.getPitch());
+        switch (mode) {
+            case "Random" -> {
+                eventPlayerPackets.setYaw(random.nextFloat() * 180);
+                if (random.nextBoolean())
+                    eventPlayerPackets.setYaw(-eventPlayerPackets.getYaw());
+                eventPlayerPackets.setPitch(random.nextFloat() * 90);
+                if (random.nextBoolean())
+                    eventPlayerPackets.setPitch(-eventPlayerPackets.getPitch());
+            }
+            case "Pitch Roll" -> {
+                pitch++;
+                eventPlayerPackets.setPitch(pitch);
+                if (pitch > 90)
+                    pitch = -90;
+            }
+            case "Yaw Roll" -> {
+                yaw++;
+                eventPlayerPackets.setYaw(yaw);
+            }
+            case "Both Roll" -> {
+                pitch++;
+                yaw++;
+                eventPlayerPackets.setYaw(yaw);
+                eventPlayerPackets.setPitch(pitch);
+                if (pitch > 90)
+                    pitch = -90;
+            }
+        }
         if (swing) {
             Hand hand = random.nextBoolean() ? Hand.MAIN_HAND : Hand.OFF_HAND;
             if (showSwing) {
