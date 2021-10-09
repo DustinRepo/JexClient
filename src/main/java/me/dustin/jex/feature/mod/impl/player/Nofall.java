53d4e3d4d4c6616d0d1b9843e2e2af6cdac68bf3
DustinRepo <tinmann4300@gmail.com>
2021-10-09 01:26:36 -0400

    Fixed NoFall causing issues

--- src/main/java/me/dustin/jex/feature/mod/impl/player/Nofall.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/player/Nofall.java
@@ -4,6 +4,7 @@ import me.dustin.events.core.annotate.EventListener;
 import me.dustin.jex.event.packet.EventPacketSent;
 import me.dustin.jex.event.player.EventPlayerPackets;
 import me.dustin.jex.feature.mod.core.Feature;
+import me.dustin.jex.feature.mod.impl.movement.Fly;
 import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.helper.network.NetworkHelper;
 import me.dustin.jex.helper.player.PlayerHelper;
@@ -15,6 +16,7 @@ public class Nofall extends Feature {
     @EventListener(events = {EventPlayerPackets.class})
     private void runEvent(EventPlayerPackets eventPlayerPackets) {
         if (eventPlayerPackets.getMode() == EventPlayerPackets.Mode.PRE) {
+            if (Feature.get(Fly.class).getState() && Wrapper.INSTANCE.getLocalPlayer().isSneaking()) return;
             if (Wrapper.INSTANCE.getLocalPlayer().isFallFlying()) return;
             if (Wrapper.INSTANCE.getLocalPlayer().fallDistance > 2.5f) {
                 NetworkHelper.INSTANCE.sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true));
@@ -24,8 +26,9 @@ public class Nofall extends Feature {
 
     @EventListener(events = {EventPacketSent.class})
     private void runEvent(EventPacketSent eventPacketSent) {
-        if (eventPacketSent.getMode() != EventPacketSent.Mode.PRE || Wrapper.INSTANCE.getLocalPlayer().isFallFlying()) return;
+        if (eventPacketSent.getMode() != EventPacketSent.Mode.PRE) return;
         if (eventPacketSent.getPacket() instanceof PlayerMoveC2SPacket origPacket && Wrapper.INSTANCE.getLocalPlayer().fallDistance > 2.5f) {
+            if ((Feature.get(Fly.class).getState() && Wrapper.INSTANCE.getLocalPlayer().isSneaking()) || Wrapper.INSTANCE.getLocalPlayer().isFallFlying()) return;
             PlayerMoveC2SPacket playerMoveC2SPacket = new PlayerMoveC2SPacket.Full(origPacket.getX(Wrapper.INSTANCE.getLocalPlayer().getX()), origPacket.getY(Wrapper.INSTANCE.getLocalPlayer().getY()), origPacket.getZ(Wrapper.INSTANCE.getLocalPlayer().getZ()), origPacket.getYaw(PlayerHelper.INSTANCE.getYaw()), origPacket.getPitch(PlayerHelper.INSTANCE.getPitch()), true);
             eventPacketSent.setPacket(playerMoveC2SPacket);
         }
