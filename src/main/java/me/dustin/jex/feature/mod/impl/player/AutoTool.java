514fe28f5b213bc1c838e7fe74c8967f6155366c
DustinRepo <tinmann4300@gmail.com>
2021-10-09 11:41:51 -0400

    Added Return to Original Slot option to AutoTool

--- src/main/java/me/dustin/jex/feature/mod/impl/player/AutoTool.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/player/AutoTool.java
@@ -1,7 +1,11 @@
 package me.dustin.jex.feature.mod.impl.player;
 
+import me.dustin.events.core.Event;
 import me.dustin.events.core.annotate.EventListener;
+import me.dustin.jex.JexClient;
+import me.dustin.jex.event.player.EventPlayerPackets;
 import me.dustin.jex.event.world.EventClickBlock;
+import me.dustin.jex.feature.option.annotate.Op;
 import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.helper.network.NetworkHelper;
 import me.dustin.jex.helper.player.InventoryHelper;
@@ -15,39 +19,59 @@ import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
 @Feature.Manifest(name = "AutoTool", category = Feature.Category.PLAYER, description = "Switch to the best tool for your needs.")
 public class AutoTool extends Feature {
 
-    @EventListener(events = {EventClickBlock.class})
-    public void run(EventClickBlock eventClickBlock) {
-        if (AutoEat.isEating)
-            return;
-        if (!Wrapper.INSTANCE.getLocalPlayer().isCreative()) {
-            int slot = InventoryHelper.INSTANCE.getInventory().selectedSlot;
-            BlockState blockState = Wrapper.INSTANCE.getWorld().getBlockState(eventClickBlock.getBlockPos());
-            float best = 1;
-            boolean found = false;
-            for (int index = 0; index < 9; index++) {
-                ItemStack itemStack = InventoryHelper.INSTANCE.getInventory().getStack(index);
-                if (blockState.getBlock() != Blocks.AIR) {
-                    if (itemStack.getMiningSpeedMultiplier(blockState) > best) {
-                        best = itemStack.getMiningSpeedMultiplier(blockState);
-                        slot = index;
-                        found = true;
-                    } else if (itemStack.getMiningSpeedMultiplier(blockState) == best && best > 1) {
-                        if (InventoryHelper.INSTANCE.compareEnchants(InventoryHelper.INSTANCE.getInventory().getStack(slot), itemStack, Enchantments.EFFICIENCY)) {
+    @Op(name = "Return to Original Slot")
+    public boolean returnToSlot = true;
+
+    private boolean attackingBlock;
+    private int savedSlot;
+
+    @EventListener(events = {EventClickBlock.class, EventPlayerPackets.class})
+    public void run(Event event) {
+        if (event instanceof EventClickBlock eventClickBlock) {
+            if (AutoEat.isEating)
+                return;
+            if (!Wrapper.INSTANCE.getLocalPlayer().isCreative()) {
+                int slot = InventoryHelper.INSTANCE.getInventory().selectedSlot;
+                BlockState blockState = Wrapper.INSTANCE.getWorld().getBlockState(eventClickBlock.getBlockPos());
+                float best = 1;
+                boolean found = false;
+                for (int index = 0; index < 9; index++) {
+                    ItemStack itemStack = InventoryHelper.INSTANCE.getInventory().getStack(index);
+                    if (blockState.getBlock() != Blocks.AIR) {
+                        if (itemStack.getMiningSpeedMultiplier(blockState) > best) {
                             best = itemStack.getMiningSpeedMultiplier(blockState);
                             slot = index;
                             found = true;
+                        } else if (itemStack.getMiningSpeedMultiplier(blockState) == best && best > 1) {
+                            if (InventoryHelper.INSTANCE.compareEnchants(InventoryHelper.INSTANCE.getInventory().getStack(slot), itemStack, Enchantments.EFFICIENCY)) {
+                                best = itemStack.getMiningSpeedMultiplier(blockState);
+                                slot = index;
+                                found = true;
+                            }
                         }
                     }
                 }
-            }
-            if (slot == InventoryHelper.INSTANCE.getInventory().selectedSlot && !found) {
-                if (InventoryHelper.INSTANCE.getInventory().getStack(InventoryHelper.INSTANCE.getInventory().selectedSlot).isDamageable()) {
-                    slot = getNonDamageSlot();
+                if (slot == InventoryHelper.INSTANCE.getInventory().selectedSlot && !found) {
+                    if (InventoryHelper.INSTANCE.getInventory().getStack(InventoryHelper.INSTANCE.getInventory().selectedSlot).isDamageable()) {
+                        slot = getNonDamageSlot();
+                    }
                 }
+                if (!attackingBlock && slot != -1) {
+                    savedSlot = InventoryHelper.INSTANCE.getInventory().selectedSlot;
+                }
+                if (slot != -1 && slot != InventoryHelper.INSTANCE.getInventory().selectedSlot) {
+                    NetworkHelper.INSTANCE.sendPacket(new UpdateSelectedSlotC2SPacket(slot));
+                    InventoryHelper.INSTANCE.getInventory().selectedSlot = slot;
+                }
+                attackingBlock = true;
             }
-            if (slot != -1 && slot != InventoryHelper.INSTANCE.getInventory().selectedSlot) {
-                NetworkHelper.INSTANCE.sendPacket(new UpdateSelectedSlotC2SPacket(slot));
-                InventoryHelper.INSTANCE.getInventory().selectedSlot = slot;
+        } else if (event instanceof EventPlayerPackets eventPlayerPackets && eventPlayerPackets.getMode() == EventPlayerPackets.Mode.PRE) {
+            if (!Wrapper.INSTANCE.getInteractionManager().isBreakingBlock() && attackingBlock) {
+                if (returnToSlot) {
+                    InventoryHelper.INSTANCE.getInventory().selectedSlot = savedSlot;
+                    NetworkHelper.INSTANCE.sendPacket(new UpdateSelectedSlotC2SPacket(savedSlot));
+                }
+                attackingBlock = false;
             }
         }
     }
