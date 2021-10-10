e93b344de6884d9291edf0bdd45d2c02275e9fd8
DustinRepo <tinmann4300@gmail.com>
2021-10-10 09:24:13 -0400

    Added more options to Trail mod

--- src/main/java/me/dustin/jex/feature/mod/impl/render/Trail.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/render/Trail.java
@@ -4,10 +4,13 @@ import com.google.gson.JsonArray;
 import me.dustin.events.core.annotate.EventListener;
 import me.dustin.jex.event.player.EventPlayerPackets;
 import me.dustin.jex.feature.mod.core.Feature;
+import me.dustin.jex.feature.option.annotate.Op;
 import me.dustin.jex.helper.file.JsonHelper;
 import me.dustin.jex.helper.file.ModFileHelper;
 import me.dustin.jex.helper.misc.Wrapper;
+import me.dustin.jex.helper.player.FriendHelper;
 import me.dustin.jex.helper.player.PlayerHelper;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleType;
 import net.minecraft.particle.ParticleTypes;
@@ -22,8 +25,12 @@ import java.util.Random;
 @Feature.Manifest(name = "Trail", category = Feature.Category.VISUAL, description = "Render a configurable list of particles as a trail behind you. Use command .trail to configure")
 public class Trail extends Feature {
 
+    @Op(name = "Show on Friends")
+    public boolean showOnFriends = true;
+
     public ArrayList<ParticleType<?>> particles = new ArrayList<>();
     private Random r = new Random();
+
     @EventListener(events = {EventPlayerPackets.class})
     private void runMethod(EventPlayerPackets eventPlayerPackets) {
         if (eventPlayerPackets.getMode() == EventPlayerPackets.Mode.PRE) {
@@ -31,6 +38,13 @@ public class Trail extends Feature {
             particles.forEach(particleType -> {
                 if (PlayerHelper.INSTANCE.isMoving())
                     Wrapper.INSTANCE.getMinecraft().particleManager.addParticle((ParticleEffect) particleType, Wrapper.INSTANCE.getLocalPlayer().getX(), Wrapper.INSTANCE.getLocalPlayer().getY(), Wrapper.INSTANCE.getLocalPlayer().getZ(), 0, 0, 0);
+                if (showOnFriends)
+                    Wrapper.INSTANCE.getWorld().getEntities().forEach(entity -> {
+                        if (entity != Wrapper.INSTANCE.getLocalPlayer() && entity instanceof PlayerEntity playerEntity) {
+                            if (FriendHelper.INSTANCE.isFriend(playerEntity.getGameProfile().getName()) && (playerEntity.getVelocity().getX() != 0 || playerEntity.getVelocity().getZ() != 0))
+                                Wrapper.INSTANCE.getMinecraft().particleManager.addParticle((ParticleEffect) particleType, Wrapper.INSTANCE.getLocalPlayer().getX(), Wrapper.INSTANCE.getLocalPlayer().getY(), Wrapper.INSTANCE.getLocalPlayer().getZ(), 0, 0, 0);
+                        }
+                    });
             });
         }
     }
