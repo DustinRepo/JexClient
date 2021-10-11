2f5f236923d2c9a0c31b8268b66daf03d6371536
DustinRepo <tinmann4300@gmail.com>
2021-10-10 21:40:27 -0400

    Added ThrowPearl

---
+++ src/main/java/me/dustin/jex/feature/mod/impl/player/ThrowPearl.java
@@ -0,0 +1,44 @@
+package me.dustin.jex.feature.mod.impl.player;
+
+import me.dustin.events.core.annotate.EventListener;
+import me.dustin.jex.event.misc.EventKeyPressed;
+import me.dustin.jex.event.misc.EventMouseButton;
+import me.dustin.jex.feature.mod.core.Feature;
+import me.dustin.jex.feature.option.annotate.Op;
+import me.dustin.jex.helper.misc.ChatHelper;
+import me.dustin.jex.helper.network.NetworkHelper;
+import me.dustin.jex.helper.player.InventoryHelper;
+import net.minecraft.item.Items;
+import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket;
+import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
+import net.minecraft.util.Hand;
+import org.lwjgl.glfw.GLFW;
+
+@Feature.Manifest(name = "ThrowPearl", category = Feature.Category.PLAYER, description = "Automatically throw ender pearl from hotbar on button press.")
+public class ThrowPearl extends Feature {
+
+    @Op(name = "Throw Key", isKeybind = true)
+    public int throwKey = GLFW.GLFW_KEY_Y;
+
+    @EventListener(events = {EventKeyPressed.class})
+    private void runMethod(EventKeyPressed eventKeyPressed) {
+        if (eventKeyPressed.getKey() == throwKey && eventKeyPressed.getType() == EventKeyPressed.PressType.IN_GAME) {
+            int slot = InventoryHelper.INSTANCE.getFromHotbar(Items.ENDER_PEARL);
+            boolean offhand = InventoryHelper.INSTANCE.getInventory().getStack(45).getItem() == Items.ENDER_PEARL;
+            if (slot == -1 && !offhand) {
+                ChatHelper.INSTANCE.addClientMessage("You have no ender pearls in your hotbar");
+            } else {
+                int savedSlot = InventoryHelper.INSTANCE.getInventory().selectedSlot;
+                if (slot != -1) {
+                    NetworkHelper.INSTANCE.sendPacket(new UpdateSelectedSlotC2SPacket(slot));
+                    InventoryHelper.INSTANCE.getInventory().selectedSlot = slot;
+                }
+                NetworkHelper.INSTANCE.sendPacket(new PlayerInteractItemC2SPacket(offhand ? Hand.OFF_HAND : Hand.MAIN_HAND));
+                if (slot != -1) {
+                    NetworkHelper.INSTANCE.sendPacket(new UpdateSelectedSlotC2SPacket(savedSlot));
+                    InventoryHelper.INSTANCE.getInventory().selectedSlot = savedSlot;
+                }
+            }
+        }
+    }
+}
