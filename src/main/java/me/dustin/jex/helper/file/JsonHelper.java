7471ada2335334a3ec690fee47ecc90abcd55cd6
DustinRepo <tinmann4300@gmail.com>
2021-10-11 00:19:14 -0400

    Fixed Json issue

--- src/main/java/me/dustin/jex/helper/file/JsonHelper.java
+++ src/main/java/me/dustin/jex/helper/file/JsonHelper.java
@@ -5,7 +5,7 @@ import com.google.gson.GsonBuilder;
 
 public enum JsonHelper {
     INSTANCE;
-    public final Gson gson = new Gson();
-    public final Gson prettyGson = new GsonBuilder().setPrettyPrinting().create();
+    public final Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+    public final Gson prettyGson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
 
 }
