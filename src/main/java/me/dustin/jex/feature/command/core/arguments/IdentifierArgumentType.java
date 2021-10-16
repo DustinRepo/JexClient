04e02788f0261e8f60e3832136fe1826a06aafa2
DustinRepo <tinmann4300@gmail.com>
2021-10-16 19:20:18 -0400

    Added CommandFindBiome

---
+++ src/main/java/me/dustin/jex/feature/command/core/arguments/IdentifierArgumentType.java
@@ -0,0 +1,38 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package me.dustin.jex.feature.command.core.arguments;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class IdentifierArgumentType implements ArgumentType<Identifier> {
+    private static final Collection<String> EXAMPLES = Arrays.asList("foo", "foo:bar", "012");
+
+    public static IdentifierArgumentType identifier() {
+        return new IdentifierArgumentType();
+    }
+
+    public static Identifier getIdentifier(CommandContext<FabricClientCommandSource> context, String name) {
+        return context.getArgument(name, Identifier.class);
+    }
+
+    public Identifier parse(StringReader stringReader) throws CommandSyntaxException {
+        return Identifier.fromCommandInput(stringReader);
+    }
+
+    public Collection<String> getExamples() {
+        return EXAMPLES;
+    }
+}
--- src/main/java/me/dustin/jex/feature/command/impl/CommandFindBiome.java
+++ src/main/java/me/dustin/jex/feature/command/impl/CommandFindBiome.java
@@ -1,11 +1,12 @@
 package me.dustin.jex.feature.command.impl;
 
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
 import me.dustin.jex.JexClient;
 import me.dustin.jex.feature.command.core.Command;
 import me.dustin.jex.feature.command.core.annotate.Cmd;
+import me.dustin.jex.feature.command.core.arguments.IdentifierArgumentType;
 import me.dustin.jex.feature.command.core.arguments.MessageArgumentType;
 import me.dustin.jex.feature.command.core.arguments.Vec3ArgumentType;
 import me.dustin.jex.helper.misc.ChatHelper;
@@ -14,39 +15,37 @@ import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.helper.world.WorldHelper;
 import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
 import net.minecraft.client.world.GeneratorType;
+import net.minecraft.command.CommandSource;
+import net.minecraft.command.suggestion.SuggestionProviders;
 import net.minecraft.server.integrated.IntegratedServer;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
-import net.minecraft.world.gen.feature.StructureFeature;
-import org.apache.commons.lang3.StringUtils;
+import net.minecraft.world.biome.Biome;
 
-@Cmd(name = "find", description = "Find structures with a given seed, like the /locate command", syntax = {".find <structure> <seed>", ".find <structure> <searchPos> <seed>"})
-public class CommandFind extends Command {
-    private StructureFeature<?> structureFeature;
+@Cmd(name = "findbiome", description = "Find Biomes with a given seed, like the /locatebiome command", syntax = {".find <biome> <seed>", ".find <biome> <searchPos> <seed>"})
+public class CommandFindBiome extends Command {
     private BlockPos startPos;
+
+    SuggestionProvider<FabricClientCommandSource> ALL_BIOMES = SuggestionProviders.register(new Identifier("jex", "available_biomes"), (context, builder) -> CommandSource.suggestIdentifiers((context.getSource()).getRegistryManager().get(Registry.BIOME_KEY).getIds(), builder));
+
     @Override
     public void registerCommand() {
-        LiteralArgumentBuilder<FabricClientCommandSource> argumentBuilder = literal(this.name);
-        StructureFeature.STRUCTURES.forEach((s, structureFeature) -> {
-            argumentBuilder.then(literal(s).then(argument("pos", Vec3ArgumentType.vec3()).then(argument("seed", MessageArgumentType.message()).executes(context -> {
-                this.structureFeature = structureFeature;
-                this.startPos = new BlockPos(Vec3ArgumentType.getVec3(context, "pos"));
-                return run(context);
-            }))).then(argument("seed", MessageArgumentType.message()).executes(context -> {
-                this.structureFeature = structureFeature;
-                return run(context);
-            })));
-        });
-        dispatcher.register(argumentBuilder);
+        dispatcher.register(literal(this.name).then(argument("biome", IdentifierArgumentType.identifier()).suggests(ALL_BIOMES).then(argument("pos", Vec3ArgumentType.vec3()).then(argument("seed", MessageArgumentType.message()).executes(context -> {
+            this.startPos = new BlockPos(Vec3ArgumentType.getVec3(context, "pos"));
+            return run(context);
+        }))).then(argument("seed", MessageArgumentType.message()).executes(this))));
     }
 
     @Override
     public int run(CommandContext<FabricClientCommandSource> context) throws CommandSyntaxException {
         long seed = SeedHelper.INSTANCE.getSeed(MessageArgumentType.getMessage(context, "seed").asString()).getAsLong();
+        Identifier biomeId = IdentifierArgumentType.getIdentifier(context, "biome");
         if (startPos == null)
             startPos = Wrapper.INSTANCE.getLocalPlayer().getBlockPos();
-        new Thread("structure_finder") {
+        new Thread("biome_finder") {
             @Override
             public void run() {
                 ChatHelper.INSTANCE.addClientMessage("Creating fake server for world gen... (This may take a moment)");
@@ -55,18 +54,25 @@ public class CommandFind extends Command {
                     ChatHelper.INSTANCE.addClientMessage("Error: Could not create integrated server");
                     return;
                 }
+
+                Biome biome = integratedServer.getRegistryManager().get(Registry.BIOME_KEY).getOrEmpty(biomeId).orElseThrow(() -> null);
+
+                if (biome == null) {
+                    ChatHelper.INSTANCE.addClientMessage("Error: Biome request returned null");
+                }
+
                 ServerWorld serverWorld = switch (WorldHelper.INSTANCE.getDimensionID().toString()) {
                     case "minecraft:the_nether" -> integratedServer.getWorld(World.NETHER);
                     case "minecraft:the_end" -> integratedServer.getWorld(World.END);
                     default -> integratedServer.getOverworld();
                 };
                 if (serverWorld != null) {
-                    BlockPos pos = serverWorld.locateStructure(structureFeature, startPos, 100, false);
+                    BlockPos pos = serverWorld.locateBiome(biome, startPos, 6400, 8);
                     if (pos != null) {
                         String posString = "BlockPos: X: \247b" + pos.getX() + (pos.getY() == 0 ? "" : " \2477Y: \247b" + pos.getY()) + " \2477Z: \247b" + pos.getZ();
-                        ChatHelper.INSTANCE.addClientMessage(StringUtils.capitalize(structureFeature.getName()) + " found at " + posString);
+                        ChatHelper.INSTANCE.addClientMessage(biomeId.toString() + " found at " + posString);
                     } else {
-                        ChatHelper.INSTANCE.addClientMessage("Could not find structure");
+                        ChatHelper.INSTANCE.addClientMessage("Could not find biome");
                     }
                 } else {
                     ChatHelper.INSTANCE.addClientMessage("ServerWorld null");
