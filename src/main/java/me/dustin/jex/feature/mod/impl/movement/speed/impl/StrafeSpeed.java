368b269ad0ae5c4d20589d629bade94717632c71
DustinRepo <tinmann4300@gmail.com>
2021-10-17 11:09:05 -0400

    Moved the AutoHop option in Vanilla Speed to be it's own Speed mode called Strafe with customizable hops

--- src/main/java/me/dustin/jex/feature/mod/impl/movement/speed/impl/StrafeSpeed.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/movement/speed/impl/StrafeSpeed.java
@@ -4,16 +4,16 @@ import me.dustin.events.core.Event;
 import me.dustin.jex.event.player.EventMove;
 import me.dustin.jex.event.player.EventPlayerPackets;
 import me.dustin.jex.feature.extension.FeatureExtension;
+import me.dustin.jex.feature.mod.core.Feature;
+import me.dustin.jex.feature.mod.impl.movement.speed.Speed;
 import me.dustin.jex.helper.baritone.BaritoneHelper;
 import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.helper.player.PlayerHelper;
-import me.dustin.jex.feature.mod.core.Feature;
-import me.dustin.jex.feature.mod.impl.movement.speed.Speed;
 
-public class VanillaSpeed extends FeatureExtension {
+public class StrafeSpeed extends FeatureExtension {
 
-    public VanillaSpeed() {
-        super("Vanilla", Speed.class);
+    public StrafeSpeed() {
+        super("Strafe", Speed.class);
     }
 
     @Override
@@ -22,7 +22,13 @@ public class VanillaSpeed extends FeatureExtension {
             Speed speed = ((Speed) Feature.get(Speed.class));
             if (BaritoneHelper.INSTANCE.isBaritoneRunning() && !Wrapper.INSTANCE.getLocalPlayer().isOnGround())
                 return;
-            PlayerHelper.INSTANCE.setMoveSpeed(eventMove, speed.vanillaSpeed);
+            PlayerHelper.INSTANCE.setMoveSpeed(eventMove, speed.strafeSpeed);
+        } else if (event instanceof EventPlayerPackets eventPlayerPackets) {
+            Speed speed = ((Speed) Feature.get(Speed.class));
+            if (eventPlayerPackets.getMode() == EventPlayerPackets.Mode.PRE) {
+                if (Wrapper.INSTANCE.getLocalPlayer().isOnGround() && PlayerHelper.INSTANCE.isMoving())
+                    PlayerHelper.INSTANCE.setVelocityY(speed.hopAmount);
+            }
         }
     }
 
