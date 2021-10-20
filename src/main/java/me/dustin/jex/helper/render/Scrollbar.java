70454ae231d5b031a97cb74867561994b3f693ee
DustinRepo <tinmann4300@gmail.com>
2021-10-20 15:07:01 -0400

    Added ChangelogScreen

--- src/main/java/me/dustin/jex/helper/render/Scrollbar.java
+++ src/main/java/me/dustin/jex/helper/render/Scrollbar.java
@@ -1,9 +1,10 @@
 package me.dustin.jex.helper.render;
 
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.MathHelper;
 
 public class Scrollbar {
-    
+
     private float x,y,width,height;
     private float viewportY, viewportHeight;
     private float contentHeight;
@@ -24,12 +25,13 @@ public class Scrollbar {
         updateHeight();
         if (contentHeight > viewportHeight) {
             Render2DHelper.INSTANCE.fill(matrixStack, x, viewportY, x + width, viewportY + viewportHeight, 0xff353535);
+            y = MathHelper.clamp(y, viewportY, viewportY + viewportHeight - height);
             Render2DHelper.INSTANCE.fill(matrixStack, x, y, x + width, y + height, color);
         }
     }
 
     public boolean isHovered() {
-        return Render2DHelper.INSTANCE.isHovered(x,y,width,height);
+        return Render2DHelper.INSTANCE.isHovered(x,viewportY,width,viewportHeight);
     }
 
     public float getX() {
