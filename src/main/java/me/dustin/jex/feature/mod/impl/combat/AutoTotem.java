a4a1d831388c0e6affb8e4e67007f314e1a9501d
DustinRepo <tinmann4300@gmail.com>
2021-10-30 16:14:33 -0400

    Improved AutoTotem

--- src/main/java/me/dustin/jex/feature/mod/impl/combat/AutoTotem.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/combat/AutoTotem.java
@@ -2,6 +2,7 @@ package me.dustin.jex.feature.mod.impl.combat;
 
 import me.dustin.events.core.annotate.EventListener;
 import me.dustin.jex.JexClient;
+import me.dustin.jex.event.misc.EventTick;
 import me.dustin.jex.event.packet.EventPacketSent;
 import me.dustin.jex.event.player.EventPlayerPackets;
 import me.dustin.jex.feature.mod.core.Feature;
@@ -24,41 +25,66 @@ import net.minecraft.util.math.Direction;
 @Feature.Manifest(category = Feature.Category.COMBAT, description = "Keep a Totem in your offhand at all times.")
 public class AutoTotem extends Feature {
 
-    @Op(name = "Check Health")
-    public boolean checkHealth;
-    @OpChild(name = "Health", min = 5, max = 17, parent = "Check Health")
+    @Op(name = "When", all = {"Always", "Low Health"})
+    public String activateWhen = "Always";
+    @OpChild(name = "Replace Current Item", parent = "When", dependency = "Always")
+    public boolean replaceOffhand = false;
+    @OpChild(name = "Health", min = 5, max = 17, parent = "When", dependency = "Low Health")
     public int health = 10;
     @Op(name = "Open Inventory")
     public boolean openInventory;
 
+    private int swappedSlot;
+
     @EventListener(events = {EventPlayerPackets.class})
     public void runEvent(EventPlayerPackets event) {
         if (event.getMode() == EventPlayerPackets.Mode.PRE) {
+            AutoGapple autoGapple = (AutoGapple)Feature.get(AutoGapple.class);
+            if (autoGapple.takeFromInv && autoGapple.putInto.equalsIgnoreCase("offhand") && autoGapple.isEating())
+                return;
             int totemCount = InventoryHelper.INSTANCE.countItems(Items.TOTEM_OF_UNDYING);
             int firstTotem = InventoryHelper.INSTANCE.getFromHotbar(Items.TOTEM_OF_UNDYING);
             if (firstTotem == -1)
                 firstTotem = InventoryHelper.INSTANCE.getFromInv(Items.TOTEM_OF_UNDYING);
-            if (!(Wrapper.INSTANCE.getLocalPlayer().currentScreenHandler instanceof PlayerScreenHandler) || Wrapper.INSTANCE.getMinecraft().currentScreen instanceof InventoryScreen)
-                return;
-            if (Wrapper.INSTANCE.getLocalPlayer().getOffHandStack() == null || Wrapper.INSTANCE.getLocalPlayer().getOffHandStack().getItem() == Items.AIR && firstTotem != -1) {
-                moveTotem(firstTotem);
+
+            if (needsOffhandTotem()) {
+                if (Wrapper.INSTANCE.getLocalPlayer().getOffHandStack().getItem() != Items.TOTEM_OF_UNDYING) {
+                    if (Wrapper.INSTANCE.getLocalPlayer().getOffHandStack().getItem() != Items.AIR)
+                        swappedSlot = firstTotem;
+                    moveTotem(firstTotem);
+                }
+            } else if (swappedSlot != -1) {
+                moveTotem(swappedSlot);
+                swappedSlot = -1;
             }
             this.setSuffix(totemCount + "");
         }
     }
 
+    @EventListener(events = {EventTick.class})
+    private void tick(EventTick eventTick) {
+        if (Wrapper.INSTANCE.getLocalPlayer() == null)
+            swappedSlot = -1;
+    }
+
+    public boolean needsOffhandTotem() {
+        if (activateWhen.equalsIgnoreCase("Always"))
+            return (replaceOffhand && Wrapper.INSTANCE.getLocalPlayer().getOffHandStack().getItem() != Items.TOTEM_OF_UNDYING) || Wrapper.INSTANCE.getLocalPlayer().getOffHandStack().getItem() == Items.AIR;
+        return health >= Wrapper.INSTANCE.getLocalPlayer().getHealth();
+    }
+
     public void moveTotem(int slot) {
-        if (checkHealth && health < Wrapper.INSTANCE.getLocalPlayer().getHealth())
-            return;
         if (openInventory)
             Wrapper.INSTANCE.getMinecraft().openScreen(new InventoryScreen(Wrapper.INSTANCE.getLocalPlayer()));
-
-        InventoryHelper.INSTANCE.windowClick(Wrapper.INSTANCE.getLocalPlayer().currentScreenHandler, slot < 9 ? slot + 36 : slot, SlotActionType.PICKUP);
-        InventoryHelper.INSTANCE.windowClick(Wrapper.INSTANCE.getLocalPlayer().currentScreenHandler, 45, SlotActionType.PICKUP);
-
+        InventoryHelper.INSTANCE.moveToOffhand(slot);
         if (openInventory) {
             NetworkHelper.INSTANCE.sendPacket(new CloseHandledScreenC2SPacket(Wrapper.INSTANCE.getLocalPlayer().currentScreenHandler.syncId));
             Wrapper.INSTANCE.getMinecraft().openScreen(null);
         }
+    }
+
+    public void putTotemBack() {
+        InventoryHelper.INSTANCE.windowClick(Wrapper.INSTANCE.getLocalPlayer().currentScreenHandler, 45, SlotActionType.PICKUP);
+        InventoryHelper.INSTANCE.windowClick(Wrapper.INSTANCE.getLocalPlayer().currentScreenHandler, swappedSlot < 9 ? swappedSlot + 36 : swappedSlot, SlotActionType.PICKUP);
     }
 }
