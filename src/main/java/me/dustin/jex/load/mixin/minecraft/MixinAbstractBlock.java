211ae72b64d97f2875eb6d1d456cbe620e838e4d
DustinRepo <tinmann4300@gmail.com>
2021-11-01 12:27:04 -0400

    Overhauled jesus

--- src/main/java/me/dustin/jex/load/mixin/minecraft/MixinAbstractBlock.java
+++ src/main/java/me/dustin/jex/load/mixin/minecraft/MixinAbstractBlock.java
@@ -1,11 +1,9 @@
 package me.dustin.jex.load.mixin.minecraft;
 
 import me.dustin.jex.event.world.EventBlockCollisionShape;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.FluidBlock;
 import net.minecraft.block.ShapeContext;
-import net.minecraft.fluid.FlowableFluid;
-import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
@@ -17,24 +15,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(FluidBlock.class)
-public class MixinFluidBlock {
+@Mixin(AbstractBlock.class)
+public class MixinAbstractBlock {
 
-    @Shadow
-    @Final
-    public static VoxelShape COLLISION_SHAPE;
-
-    @Shadow
-    @Final
-    public static IntProperty LEVEL;
-
-    @Shadow
-    @Final
-    protected FlowableFluid fluid;
+    @Shadow @Final protected boolean collidable;
 
     @Inject(method = "getCollisionShape", at = @At("HEAD"), cancellable = true)
     public void getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> ci) {
-        EventBlockCollisionShape eventBlockCollisionShape = new EventBlockCollisionShape(pos, state.getBlock(), context.isAbove(this.COLLISION_SHAPE, pos, true) && (Integer) state.get(this.LEVEL) == 0 && context.canWalkOnFluid(world.getFluidState(pos.up()), this.fluid) ? COLLISION_SHAPE : VoxelShapes.empty()).run();
+        EventBlockCollisionShape eventBlockCollisionShape = new EventBlockCollisionShape(pos, state.getBlock(), this.collidable ? state.getOutlineShape(world, pos) : VoxelShapes.empty()).run();
         if (eventBlockCollisionShape.isCancelled())
             ci.setReturnValue(eventBlockCollisionShape.getVoxelShape());
     }
