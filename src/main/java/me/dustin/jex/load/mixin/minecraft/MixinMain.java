08a3f96be3d3f14ae30ac364cc07ac135c0f83b1
DustinRepo <tinmann4300@gmail.com>
2021-11-01 16:55:50 -0400

    Added new dev login method

--- src/main/java/me/dustin/jex/load/mixin/minecraft/MixinMain.java
+++ src/main/java/me/dustin/jex/load/mixin/minecraft/MixinMain.java
@@ -1,10 +1,9 @@
 package me.dustin.jex.load.mixin.minecraft;
 
 import me.dustin.jex.JexClient;
-import me.dustin.jex.file.core.ConfigManager;
-import me.dustin.jex.file.impl.AltFile;
 import me.dustin.jex.gui.account.account.MinecraftAccount;
-import me.dustin.jex.helper.network.login.minecraft.MinecraftAccountManager;
+import me.dustin.jex.helper.file.ModFileHelper;
+import me.dustin.jex.helper.file.YamlHelper;
 import me.dustin.jex.helper.network.login.minecraft.MicrosoftLogin;
 import me.dustin.jex.helper.network.login.minecraft.MojangLogin;
 import net.fabricmc.loader.api.FabricLoader;
@@ -15,42 +14,49 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 @Mixin(Main.class)
 public class MixinMain {
+
+    private static MinecraftAccount mcAccount;
 
     @ModifyArgs(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Session;<init>(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V"), method = "main")
     private static void modifySession(Args args) {
         if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
-            new AltFile().read();
-            if (!MinecraftAccountManager.INSTANCE.getAccounts().isEmpty()) {
-                MinecraftAccount mcAccount = MinecraftAccountManager.INSTANCE.getAccounts().get(0);
-                if (mcAccount instanceof MinecraftAccount.MicrosoftAccount microsoftAccount) {
-                    String uuid = microsoftAccount.uuid;
-                    if (uuid == null || uuid.equalsIgnoreCase("null")) {
-                        JexClient.INSTANCE.getLogger().info("UUID null, can not log in");
-                        return;
-                    }
-
-                    JexClient.INSTANCE.getLogger().info("Logging in to Microsoft account with name " + microsoftAccount.username);
-                    Session session = new MicrosoftLogin(microsoftAccount, session1 -> {}).loginNoThread();
-                    if (session != null)
-                        args.setAll(session.getUsername(), session.getUuid(), session.getAccessToken(), "msa");
-                    else
-                        JexClient.INSTANCE.getLogger().info("Unable to login");
-                } else if (mcAccount instanceof MinecraftAccount.MojangAccount mojangAccount) {
-                    Session session = MojangLogin.login(mojangAccount.getEmail(), mojangAccount.getPassword());
-                    if (session != null) {
-                        JexClient.INSTANCE.getLogger().info("Logging in to Mojang account with name " + session.getUsername());
-                        args.setAll(session.getUsername(), session.getUuid(), session.getAccessToken(), "mojang");
-                    } else
-                        JexClient.INSTANCE.getLogger().info("Unable to login");
-                } else {
-                    JexClient.INSTANCE.getLogger().info("Account not recognized, can not log in.");
-                }
+            Map<String, Object> parsedYaml = YamlHelper.INSTANCE.readFile(new File(ModFileHelper.INSTANCE.getJexDirectory(), "dev-login.yml"));
+            if (parsedYaml == null || parsedYaml.isEmpty())
+                return;
+            String username = (String)parsedYaml.get("username");
+            String email = (String)parsedYaml.get("email");
+            String password = (String)parsedYaml.get("password");
+            String accountType = (String)parsedYaml.get("account-type");
+            if (accountType.equalsIgnoreCase("msa")) {
+                mcAccount = new MinecraftAccount.MicrosoftAccount(username, email, password, "", "", "");
             } else {
-                JexClient.INSTANCE.getLogger().info("Accounts empty, can not log in.");
+                mcAccount = new MinecraftAccount.MojangAccount(username, email, password);
             }
+            if (mcAccount instanceof MinecraftAccount.MicrosoftAccount microsoftAccount) {
+                Session session = new MicrosoftLogin(microsoftAccount, session1 -> {}).loginNoThread();
+                if (session != null) {
+                    JexClient.INSTANCE.getLogger().info("Logging in to Microsoft account with name " + microsoftAccount.username);
+                    args.setAll(session.getUsername(), session.getUuid(), session.getAccessToken(), "msa");
+                } else
+                    JexClient.INSTANCE.getLogger().info("Unable to login");
+            } else if (mcAccount instanceof MinecraftAccount.MojangAccount mojangAccount) {
+                Session session = MojangLogin.login(mojangAccount.getEmail(), mojangAccount.getPassword());
+                if (session != null) {
+                    JexClient.INSTANCE.getLogger().info("Logging in to Mojang account with name " + session.getUsername());
+                    args.setAll(session.getUsername(), session.getUuid(), session.getAccessToken(), "mojang");
+                } else
+                    JexClient.INSTANCE.getLogger().info("Unable to login");
+            } else {
+                JexClient.INSTANCE.getLogger().info("Account not recognized, can not log in.");
+            }
+        } else {
+            JexClient.INSTANCE.getLogger().info("Accounts empty, can not log in.");
         }
     }
-
 }
