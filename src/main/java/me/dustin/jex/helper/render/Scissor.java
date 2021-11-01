7a6ef42275745835c6b215f1ad7c394d1f6cc9f0
DustinRepo <tinmann4300@gmail.com>
2021-10-31 21:06:51 -0400

    Fixed Mac rendering bug

--- src/main/java/me/dustin/jex/helper/render/Scissor.java
+++ src/main/java/me/dustin/jex/helper/render/Scissor.java
@@ -1,7 +1,9 @@
 package me.dustin.jex.helper.render;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import me.dustin.jex.gui.click.window.impl.Window;
 import me.dustin.jex.helper.misc.Wrapper;
+import net.minecraft.util.Util;
 import org.lwjgl.opengl.GL11;
 
 public enum Scissor {
@@ -10,12 +12,11 @@ public enum Scissor {
 
     public void cut(int x, int y, int width, int height) {
         double factor = Render2DHelper.INSTANCE.getScaleFactor();
-        GL11.glScissor((int) (x * factor), (int) ((Wrapper.INSTANCE.getWindow().getHeight() - (y * factor) - height * factor)), (int) (width * factor), (int) (height * factor));
+        GL11.glScissor((int)(x * factor), (int)((Wrapper.INSTANCE.getWindow().getScaledHeight() - (y + height)) * factor), (int)(width * factor), (int)(height * factor));
         GL11.glEnable(GL11.GL_SCISSOR_TEST);
     }
 
     public void seal() {
         GL11.glDisable(GL11.GL_SCISSOR_TEST);
     }
-
 }
