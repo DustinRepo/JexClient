6b87378314d81095a80e9903fb3789cdb16eecbc
DustinRepo <tinmann4300@gmail.com>
2021-11-11 16:05:11 -0500

    Fixed crash on Raspberry Pi

--- src/main/java/me/dustin/jex/feature/mod/impl/misc/Discord.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/misc/Discord.java
@@ -1,14 +1,14 @@
 package me.dustin.jex.feature.mod.impl.misc;
 
-import club.minnced.discord.rpc.DiscordEventHandlers;
-import club.minnced.discord.rpc.DiscordRPC;
-import club.minnced.discord.rpc.DiscordRichPresence;
-import me.dustin.jex.JexClient;
-import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.feature.mod.core.Feature;
 import me.dustin.jex.feature.option.annotate.Op;
+import me.dustin.jex.helper.misc.DiscordPresenceHelper;
+import net.minecraft.util.Util;
 
-import java.util.Objects;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 @Feature.Manifest(category = Feature.Category.MISC, description = "Show on Discord that you are using Jex Client", enabled = true, visible = false)
 public class Discord extends Feature {
@@ -16,62 +16,42 @@ public class Discord extends Feature {
     @Op(name = "Show Server IP")
     public boolean showServerIP = true;
 
-    private DiscordRPC lib;
-    private Thread discordThread;
-    private DiscordRichPresence presence;
-    private final String APPID = "812897399550246914";
-
     @Override
     public void onEnable() {
-        lib = DiscordRPC.INSTANCE;
-        DiscordEventHandlers handlers = new DiscordEventHandlers();
-        handlers.ready = (user) -> JexClient.INSTANCE.getLogger().info("Ready!");
-        lib.Discord_Initialize(APPID, handlers, true, "");
-        presence = new DiscordRichPresence();
-        presence.startTimestamp = System.currentTimeMillis() / 1000; // epoch second
-        presence.largeImageKey = "jex";
-        presence.largeImageText = "Jex Client " + JexClient.INSTANCE.getVersion().version();
-        presence.details = "Jex Client " + JexClient.INSTANCE.getVersion().version();
-        presence.state = getDetails();
-        lib.Discord_UpdatePresence(presence);
-        // in a worker thread
-        (discordThread = new Thread(() -> {
-            while (!Thread.currentThread().isInterrupted()) {
-                try {
-                    if (presence == null)return;
-                    presence.details = "Jex Client " + JexClient.INSTANCE.getVersion().version();
-                    presence.state = getDetails();
-                    lib.Discord_UpdatePresence(presence);
-                    Thread.sleep(2000);
-                } catch (Exception ignored) {}
-            }
-        }, "RPC-Callback-Handler")).start();
+        if (isOnRaspberryPi()) {
+            setState(false);
+            return;
+        }
+        DiscordPresenceHelper.INSTANCE.init();
     }
 
     @Override
     public void onDisable() {
-        if (discordThread != null && !discordThread.isInterrupted()) {
-            discordThread.interrupt();
+        if (isOnRaspberryPi()) {
+            return;
         }
-        if (lib != null) {
-            lib.Discord_Shutdown();
-            lib = null;
-            presence = null;
-        }
+        DiscordPresenceHelper.INSTANCE.destroy();
     }
 
-    private String getDetails() {
-        StringBuilder sb = new StringBuilder();
-        if (Wrapper.INSTANCE.getLocalPlayer() == null)
-            sb.append("In the menus");
-        else {
-            sb.append("Playing ");
-            if (showServerIP) {
-                sb.append(Wrapper.INSTANCE.getMinecraft().isIntegratedServerRunning() ? "SinglePlayer" : Objects.requireNonNull(Wrapper.INSTANCE.getMinecraft().getCurrentServerEntry()).address + " " + Wrapper.INSTANCE.getMinecraft().getCurrentServerEntry().version.getString());
-            } else {
-                sb.append(Wrapper.INSTANCE.getMinecraft().isIntegratedServerRunning() ? "SinglePlayer" : "Mineman Multiplayer");
+    public boolean isOnRaspberryPi() {
+        //might as well just catch all ARM devices since discord does not support ARM and by extension, neither does the RPC.
+        if (System.getProperty("os.arch").toLowerCase().contains("arm"))
+            return true;
+        if (Util.getOperatingSystem() == Util.OperatingSystem.LINUX) {
+            final File file = new File("/etc", "os-release");
+            try (FileInputStream fis = new FileInputStream(file); BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fis))) {
+                String string;
+                while ((string = bufferedReader.readLine()) != null) {
+                    if (string.toLowerCase().contains("raspbian")) {
+                        if (string.toLowerCase().contains("name")) {
+                            return true;
+                        }
+                    }
+                }
+            } catch (final Exception e) {
+                e.printStackTrace();
             }
         }
-        return sb.toString();
+        return false;
     }
 }
