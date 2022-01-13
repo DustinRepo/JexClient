8ed674fcd175fa8b4f7b4ade97fa8a02409e39e7
DustinRepo <tinmann4300@gmail.com>
2022-01-13 12:34:41 -0500

    Fixed Optifine crash

--- src/main/java/me/dustin/jex/load/JexMixinConfigPlugin.java
+++ src/main/java/me/dustin/jex/load/JexMixinConfigPlugin.java
@@ -24,15 +24,6 @@ public class JexMixinConfigPlugin implements IMixinConfigPlugin {
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        if (FabricLoader.getInstance().isModLoaded("optifabric")) {
-            if (mixinClassName.equalsIgnoreCase("me.dustin.jex.load.mixin.minecraft.MixinShader")) {
-                LOGGER.info("Optifabric loaded. Ignoring " + mixinClassName + " that injects into " + targetClassName);
-                return false;
-            }
-        } else if (mixinClassName.toLowerCase().contains("optifine")) {
-            LOGGER.info("Optifabric not loaded. Ignoring " + mixinClassName + " that injects into " + targetClassName);
-            return false;
-        }
         //I use @Psuedo for sodium classes but someone experienced a weird crash so this is an extra fail-safe
         if (!FabricLoader.getInstance().isModLoaded("sodium")) {
             if (mixinClassName.contains("sodium.")) {
