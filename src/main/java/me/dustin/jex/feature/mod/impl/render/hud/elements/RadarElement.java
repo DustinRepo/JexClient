6442c7a735b4c74c99dd8c352ddac2c47c52dc00
DustinRepo <tinmann4300@gmail.com>
2022-01-13 12:35:43 -0500

    testing

--- src/main/java/me/dustin/jex/feature/mod/impl/render/hud/elements/RadarElement.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/render/hud/elements/RadarElement.java
@@ -4,18 +4,29 @@ import me.dustin.jex.feature.mod.core.Feature;
 import me.dustin.jex.feature.mod.impl.render.esp.ESP;
 import me.dustin.jex.feature.mod.impl.world.Radar;
 import me.dustin.jex.feature.mod.impl.world.Waypoints;
+import me.dustin.jex.helper.file.FileHelper;
 import me.dustin.jex.helper.math.ColorHelper;
 import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.helper.player.PlayerHelper;
 import me.dustin.jex.helper.render.Render2DHelper;
 import me.dustin.jex.helper.render.font.FontHelper;
+import me.dustin.jex.helper.world.WorldHelper;
+import net.minecraft.block.Block;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.render.*;
+import net.minecraft.client.texture.NativeImage;
+import net.minecraft.client.texture.NativeImageBackedTexture;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.math.Matrix4f;
-import net.minecraft.util.math.Quaternion;
-import net.minecraft.util.math.Vec3f;
+import net.minecraft.item.FilledMapItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.map.MapState;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.*;
+import net.minecraft.world.Heightmap;
 
 import java.awt.*;
 
@@ -23,6 +34,8 @@ public class RadarElement extends HudElement{
     public RadarElement(float x, float y, float minWidth, float minHeight) {
         super("Radar", x, y, minWidth, minHeight);
     }
+
+    NativeImage radarImage = null;
 
     @Override
     public void render(MatrixStack matrixStack) {
@@ -50,11 +63,13 @@ public class RadarElement extends HudElement{
             float scale = 0.75f;
             matrixStack.scale(scale, scale, 1);
             Waypoints.waypoints.forEach(waypoint -> {
-                float xPos = (float) (waypoint.getX() - Wrapper.INSTANCE.getLocalPlayer().getX()) + midPos + this.getX();
-                float yPos = (float) (waypoint.getZ() - Wrapper.INSTANCE.getLocalPlayer().getZ()) + midPos + this.getY();
-                String letter = waypoint.getName().substring(0, 1);
-                if (xPos < this.getX() + this.getWidth() - 2 && yPos < this.getY() + this.getHeight() - 2 && yPos > this.getY() + 2 && xPos > this.getX() + 2) {
-                    FontHelper.INSTANCE.drawCenteredString(matrixStack, letter, xPos / scale, yPos / scale, waypoint.getColor());
+                if (waypoint.getDimension().equalsIgnoreCase(WorldHelper.INSTANCE.getDimensionID().toString())) {
+                    float xPos = (float) (waypoint.getX() - Wrapper.INSTANCE.getLocalPlayer().getX()) + midPos + this.getX();
+                    float yPos = (float) (waypoint.getZ() - Wrapper.INSTANCE.getLocalPlayer().getZ()) + midPos + this.getY();
+                    String letter = waypoint.getName().substring(0, 1);
+                    if (xPos < this.getX() + this.getWidth() - 2 && yPos < this.getY() + this.getHeight() - 2 && yPos > this.getY() + 2 && xPos > this.getX() + 2) {
+                        FontHelper.INSTANCE.drawCenteredString(matrixStack, letter, xPos / scale, yPos / scale, waypoint.getColor());
+                    }
                 }
             });
             matrixStack.pop();
@@ -66,11 +81,36 @@ public class RadarElement extends HudElement{
             matrixStack.multiply(new Quaternion(new Vec3f(0.0F, 0.0F, 1.0F), PlayerHelper.INSTANCE.getYaw() + 180, true));
             drawPointer(matrixStack);
             matrixStack.pop();
+
+            /*Identifier id = new Identifier("jex", "radar/map.png");
+            if (radarImage == null) {
+                if (Wrapper.INSTANCE.getWorld() != null) {
+                    radarImage = createNativeImage();
+                    MinecraftClient.getInstance().getTextureManager().registerTexture(id, new NativeImageBackedTexture(radarImage));
+                }
+            } else {
+                Render2DHelper.INSTANCE.bindTexture(id);
+                DrawableHelper.drawTexture(matrixStack, (int) getX(), (int) getY(), 0, 0, (int)getWidth(), (int)getHeight(), (int)getWidth(), (int)getHeight());
+
+            }*/
         }catch (Exception e) { matrixStack.pop(); }
         FontHelper.INSTANCE.drawCenteredString(matrixStack, "N", this.getX() + midPos + 1, this.getY() + 2, -1);
         FontHelper.INSTANCE.drawCenteredString(matrixStack, "S", this.getX() + midPos + 1, this.getY() + this.getHeight() - 11, -1);
         FontHelper.INSTANCE.drawWithShadow(matrixStack, "W", this.getX() + 3, this.getY() + (this.getWidth() / 2) - 5, -1);
         FontHelper.INSTANCE.drawWithShadow(matrixStack, "E", this.getX() + this.getWidth() - 1 - FontHelper.INSTANCE.getStringWidth("E"), this.getY() + (this.getWidth() / 2) - 5, -1);
+    }
+
+    public NativeImage createNativeImage() {
+        NativeImage nativeImage = new NativeImage((int)getWidth(), (int)getHeight(), true);
+        for (int x = -nativeImage.getWidth() / 2; x < nativeImage.getWidth() / 2; x++)
+            for (int z = -nativeImage.getHeight() / 2; z < nativeImage.getHeight() / 2; z++) {
+                BlockPos blockPos = Wrapper.INSTANCE.getLocalPlayer().getBlockPos().add(x, 0, z);
+                int y = Wrapper.INSTANCE.getWorld().getChunk(blockPos.getX() / 16, blockPos.getZ() / 16).sampleHeightmap(Heightmap.Type.WORLD_SURFACE, x, z);
+                Block block = WorldHelper.INSTANCE.getBlock(new BlockPos(blockPos.getX(), y, blockPos.getZ()));
+                nativeImage.setColor(x, z, block.getDefaultMapColor().color);
+            }
+        nativeImage.close();
+        return nativeImage;
     }
 
     @Override
