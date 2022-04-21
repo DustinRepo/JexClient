2d29a9379a84e849582f236d095e4f41a01a5c6a
DustinRepo <tinmann4300@gmail.com>
2022-04-21 19:06:44 -0400

    Cleaning of ServerPinger and ServerScrubber

--- src/main/java/me/dustin/jex/helper/network/ServerPinger.java
+++ src/main/java/me/dustin/jex/helper/network/ServerPinger.java
@@ -1,24 +1,14 @@
 package me.dustin.jex.helper.network;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.mojang.authlib.GameProfile;
-import io.netty.buffer.Unpooled;
-import me.dustin.jex.JexClient;
-import me.dustin.jex.helper.file.JsonHelper;
 import me.dustin.jex.helper.misc.ChatHelper;
-import me.dustin.jex.helper.misc.Wrapper;
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.network.Address;
 import net.minecraft.client.network.AllowedAddressResolver;
 import net.minecraft.client.network.ServerAddress;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.NetworkState;
-import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.listener.ClientQueryPacketListener;
 import net.minecraft.network.packet.c2s.handshake.HandshakeC2SPacket;
-import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
 import net.minecraft.network.packet.c2s.query.QueryPingC2SPacket;
 import net.minecraft.network.packet.c2s.query.QueryRequestC2SPacket;
 import net.minecraft.network.packet.s2c.query.QueryPongS2CPacket;
@@ -27,25 +17,11 @@ import net.minecraft.server.ServerMetadata;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Optional;
 import java.util.UUID;
 
 public class ServerPinger {
-
-    public static byte PACKET_HANDSHAKE = 0x00, PACKET_STATUSREQUEST = 0x00, PACKET_PING = 0x01;
-    public static int PROTOCOL_VERSION = 758;
-    public static int STATUS_PING = 1;
 
     private static final UUID emptyUUID = UUID.fromString("00000000-0000-0000-0000-000000000000");
     private final String ip;
@@ -58,162 +34,8 @@ public class ServerPinger {
         new Thread(() -> {
             ServerAddress serverAddress = ServerAddress.parse(ip);
             Optional<InetSocketAddress> optional = AllowedAddressResolver.DEFAULT.resolve(serverAddress).map(Address::getInetSocketAddress);
-            if (optional.isPresent()) {
-                // \/ this one uses code much more intertwined with Minecraft, I just thought it would be fun to re-create the packet sending and receiving myself
-                //ping(optional.get());
-                try {
-                    InetAddress inetAddress = InetAddress.getByName(serverAddress.getAddress());
-                    ChatHelper.INSTANCE.addClientMessage("Resolved IP: " + inetAddress.getHostAddress());
-                    Socket socket = new Socket(inetAddress.getHostAddress(), serverAddress.getPort());
-                    DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-                    DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-
-                    sendHandshake(dataOutputStream, serverAddress, NetworkState.STATUS);
-                    sendQueryRequest(dataOutputStream);
-
-                    int size = readVarInt(dataInputStream);//even tho we don't use it currently you MUST read this in order to not mess up the order of others being read
-                    int packetID = readVarInt(dataInputStream);
-
-                    if (packetID == 0x00) {
-                        String resp = receiveQueryRequest(dataInputStream);
-                        JsonObject mainObject = JsonHelper.INSTANCE.gson.fromJson(resp, JsonObject.class);
-                        JexClient.INSTANCE.getLogger().info(resp);
-
-                        //server desc
-                        JsonObject description = mainObject.getAsJsonObject("description");
-                        Text text = Text.Serializer.fromJson(description);
-                        ChatHelper.INSTANCE.addRawMessage(Formatting.GRAY + "----------------");
-                        ChatHelper.INSTANCE.addRawMessage(text);
-                        ChatHelper.INSTANCE.addRawMessage(Formatting.GRAY + "----------------");
-
-                        //server info
-                        JsonObject version = mainObject.getAsJsonObject("version");
-                        String verName = version.get("name").getAsString();
-                        int verProtocol = version.get("protocol").getAsInt();
-                        ChatHelper.INSTANCE.addRawMessage("Version: " + Formatting.AQUA + verName + Formatting.WHITE + " (Protocol ver: " + Formatting.AQUA + verProtocol + Formatting.WHITE + ")");
-
-                        //player list
-                        JsonObject playersObject = mainObject.getAsJsonObject("players");
-                        JsonArray jsonArray = playersObject.getAsJsonArray("sample");
-                        int maxPlayers = playersObject.get("max").getAsInt();
-                        int onlinePlayers = playersObject.get("online").getAsInt();
-
-                        ChatHelper.INSTANCE.addRawMessage("Players online: " + onlinePlayers + "/" + maxPlayers);
-                        if (jsonArray != null)
-                            for (int i = 0; i < jsonArray.size(); i++) {
-                                JsonObject playerObj = jsonArray.get(i).getAsJsonObject();
-                                String name = playerObj.get("name").getAsString();
-                                String uuid = playerObj.get("id").getAsString();
-                                ChatHelper.INSTANCE.addRawMessage(Formatting.GREEN + name);
-                            }
-                    } else
-                        ChatHelper.INSTANCE.addClientMessage("Server sent bad packet");
-
-                    sendPing(dataOutputStream);
-                    size = readVarInt(dataInputStream);
-                    packetID = readVarInt(dataInputStream);
-
-                    if (packetID == 0x01)
-                        receivePing(dataInputStream);
-                    else
-                        ChatHelper.INSTANCE.addClientMessage("Server sent bad packet");
-
-                    socket.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
+            optional.ifPresent(this::ping);
         }).start();
-    }
-
-    private void sendHandshake(DataOutputStream dataOutputStream, ServerAddress serverAddress, NetworkState networkState) {
-        try {
-            ByteArrayOutputStream handshakeBytes = new ByteArrayOutputStream();
-            DataOutputStream handshakePacket = new DataOutputStream(handshakeBytes);
-            handshakePacket.writeByte(PACKET_HANDSHAKE);//packet id
-            writeVarInt(handshakePacket, PROTOCOL_VERSION);//protocol version
-            writeVarInt(handshakePacket, serverAddress.getAddress().length());//length of address
-            handshakePacket.writeBytes(serverAddress.getAddress());//address
-            handshakePacket.writeShort(serverAddress.getPort());//port
-            writeVarInt(handshakePacket, networkState.getId());//status id
-
-            writeVarInt(dataOutputStream, handshakeBytes.size());//size of data
-            dataOutputStream.write(handshakeBytes.toByteArray());//data
-            handshakeBytes.close();
-            handshakePacket.close();
-        } catch (Exception e) {e.printStackTrace();}
-    }
-
-    private void sendQueryRequest(DataOutputStream dataOutputStream) {
-        try {
-            dataOutputStream.writeByte(0x01);//size of data
-            dataOutputStream.writeByte(PACKET_STATUSREQUEST);//packet id
-        } catch (Exception e) {e.printStackTrace();}
-    }
-
-    private String receiveQueryRequest(DataInputStream dataInputStream) {
-        try {
-            int strLength = readVarInt(dataInputStream);
-            byte[] strBytes = new byte[strLength];
-            dataInputStream.readFully(strBytes);
-            return new String(strBytes);
-        } catch (Exception e) {e.printStackTrace();}
-        return "null";
-    }
-
-    private void sendPing(DataOutputStream dataOutputStream) {
-        try {
-            dataOutputStream.writeByte(0x09);//size of packet id + data - 1 byte for packet id and 8 bytes for a long
-            dataOutputStream.writeByte(PACKET_PING);//packet id
-            dataOutputStream.writeLong(System.currentTimeMillis());
-        } catch (Exception e) {e.printStackTrace();}
-    }
-
-    private void receivePing(DataInputStream dataInputStream) {
-        try {
-            byte[] bytes = new byte[8];//size of data - 8 bytes for a long
-            dataInputStream.readFully(bytes);
-
-            ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
-            buffer.put(bytes);
-            buffer.flip();//need flip
-            long startTime = buffer.getLong();
-            long pingTime = System.currentTimeMillis() - startTime;
-            ChatHelper.INSTANCE.addRawMessage("Ping: " + pingTime);
-        } catch (Exception e) {e.printStackTrace();}
-    }
-
-    public static int readVarInt(DataInputStream in) throws IOException {
-        int i = 0;
-        int j = 0;
-        while (true) {
-            int k = in.readByte();
-
-            i |= (k & 0x7F) << j++ * 7;
-
-            if (j > 5) {
-                throw new RuntimeException("VarInt too big");
-            }
-
-            if ((k & 0x80) != 128) {
-                break;
-            }
-        }
-
-        return i;
-    }
-
-    public static void writeVarInt(DataOutputStream out, int paramInt) throws IOException {
-        while (true) {
-            if ((paramInt & 0xFFFFFF80) == 0) {
-                out.write(paramInt);
-                return;
-            }
-
-            out.write(paramInt & 0x7F | 0x80);
-            paramInt >>>= 7;
-        }
     }
 
     private void ping(InetSocketAddress inetAddress) {
@@ -222,12 +44,21 @@ public class ServerPinger {
             @Override
             public void onResponse(QueryResponseS2CPacket packet) {
                 ServerMetadata serverMetadata = packet.getServerMetadata();
-                ChatHelper.INSTANCE.addRawMessage(serverMetadata.getDescription().getString());
+                //description
+                ChatHelper.INSTANCE.addRawMessage(Formatting.GRAY + "----------------");
+                ChatHelper.INSTANCE.addRawMessage(serverMetadata.getDescription());
+                ChatHelper.INSTANCE.addRawMessage(Formatting.GRAY + "----------------");
+
+                //version info
+                if (serverMetadata.getVersion() != null)
+                    ChatHelper.INSTANCE.addRawMessage("Version: " + Formatting.AQUA + serverMetadata.getVersion().getGameVersion() + Formatting.WHITE + " (Protocol ver: " + Formatting.AQUA + serverMetadata.getVersion().getProtocolVersion() + Formatting.WHITE + ")");
+                //players
                 if (serverMetadata.getPlayers() != null) {
-                    ChatHelper.INSTANCE.addRawMessage("Players: ");
+                    ChatHelper.INSTANCE.addRawMessage("Players online: " + serverMetadata.getPlayers().getOnlinePlayerCount() + "/" + serverMetadata.getPlayers().getPlayerLimit());
+
                     for (GameProfile gameProfile : serverMetadata.getPlayers().getSample()) {
                         if (gameProfile.getId().compareTo(emptyUUID) != 0)
-                            ChatHelper.INSTANCE.addRawMessage(Formatting.DARK_GRAY + gameProfile.getName());
+                            ChatHelper.INSTANCE.addRawMessage(Formatting.GREEN + gameProfile.getName());
                     }
                 }
             }
--- src/main/java/me/dustin/jex/helper/network/ServerScrubber.java
+++ src/main/java/me/dustin/jex/helper/network/ServerScrubber.java
@@ -10,6 +10,7 @@ import me.dustin.jex.event.filters.TickFilter;
 import me.dustin.jex.event.misc.EventTick;
 import me.dustin.jex.helper.file.JsonHelper;
 import me.dustin.jex.helper.misc.ChatHelper;
+import net.minecraft.SharedConstants;
 import net.minecraft.client.network.Address;
 import net.minecraft.client.network.AllowedAddressResolver;
 import net.minecraft.client.network.ServerAddress;
@@ -19,6 +20,7 @@ import net.minecraft.util.Formatting;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -40,49 +42,56 @@ public enum ServerScrubber {
             return;
         }
         EventManager.register(this);
-        servers.forEach(ip -> {
+        servers.forEach(ip1 -> {
             Thread thread = new Thread(() -> {
+                int port = 25565;
+                String ip = ip1;
+                if (ip.contains(":")) {
+                    port = Integer.parseInt(ip.split(":")[1]);
+                    ip = ip.split(":")[0];
+                }
+
                 JexClient.INSTANCE.getLogger().info("Searching " + ip + " for player");
-                ServerAddress serverAddress = ServerAddress.parse(ip);
-                Optional<InetSocketAddress> optional = AllowedAddressResolver.DEFAULT.resolve(serverAddress).map(Address::getInetSocketAddress);
-                if (optional.isPresent()) {
-                    try {
-                        isSearching = true;
-                        InetAddress inetAddress = InetAddress.getByName(serverAddress.getAddress());
-                        Socket socket = new Socket(inetAddress.getHostAddress(), serverAddress.getPort());
-                        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-                        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-
-                        sendHandshake(dataOutputStream, serverAddress);
-                        sendQueryRequest(dataOutputStream);
-
-                        int size = ServerPinger.readVarInt(dataInputStream);//even tho we don't use it currently you MUST read this in order to not mess up the order of others being read
-                        int packetID = ServerPinger.readVarInt(dataInputStream);
-
-                        if (packetID != ServerPinger.PACKET_STATUSREQUEST) {
-                            JexClient.INSTANCE.getLogger().info("bad packet from" + serverAddress.getAddress() + ":" + serverAddress.getPort());
-                            return;
-                        }
-
-                        String resp = receiveQueryRequest(dataInputStream);
-                        JsonObject mainObject = JsonHelper.INSTANCE.gson.fromJson(resp, JsonObject.class);
-
-                        JsonObject playersObject = mainObject.getAsJsonObject("players");
-                        JsonArray jsonArray = playersObject.getAsJsonArray("sample");
-                        if (jsonArray != null)
-                            for (int i = 0; i < jsonArray.size(); i++) {
-                                JsonObject playerObj = jsonArray.get(i).getAsJsonObject();
-                                String playerName = playerObj.get("name").getAsString();
-                                if (playerName.equalsIgnoreCase(name)) {//we found him
-                                    ChatHelper.INSTANCE.addClientMessage(Formatting.GOLD + playerName + Formatting.GRAY + " found on server: " + Formatting.AQUA + serverAddress.getAddress() + ":" + serverAddress.getPort());
-                                    found = true;
-                                    socket.close();
-                                }
-                            }
-                        socket.close();
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                try {
+                    isSearching = true;
+                    MinecraftServerAddress minecraftServerAddress = MinecraftServerAddress.resolve(ip, port);
+                    if (minecraftServerAddress == null) {
+                        System.out.println("NULL: " + ip + ":" + port);
+                        return;
                     }
+                    Socket socket = new Socket(minecraftServerAddress.getIp(), minecraftServerAddress.getPort());
+                    DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                    DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                    sendHandshake(dataOutputStream, minecraftServerAddress);
+                    sendQueryRequest(dataOutputStream);
+
+                    int size = readVarInt(dataInputStream);//even tho we don't use it currently you MUST read this in order to not mess up the order of others being read
+                    int packetID = readVarInt(dataInputStream);
+
+                    if (packetID != 0x00) {
+                        JexClient.INSTANCE.getLogger().info("bad packet from" + minecraftServerAddress.getIp() + ":" + minecraftServerAddress.getPort());
+                        return;
+                    }
+
+                    String resp = receiveQueryRequest(dataInputStream);
+                    JsonObject mainObject = JsonHelper.INSTANCE.gson.fromJson(resp, JsonObject.class);
+
+                    JsonObject playersObject = mainObject.getAsJsonObject("players");
+                    JsonArray jsonArray = playersObject.getAsJsonArray("sample");
+                    if (jsonArray != null)
+                        for (int i = 0; i < jsonArray.size(); i++) {
+                            JsonObject playerObj = jsonArray.get(i).getAsJsonObject();
+                            String playerName = playerObj.get("name").getAsString();
+                            if (playerName.equalsIgnoreCase(name)) {//we found him
+                                ChatHelper.INSTANCE.addClientMessage(Formatting.GOLD + playerName + Formatting.GRAY + " found on server: " + Formatting.AQUA + minecraftServerAddress.getIp() + ":" + minecraftServerAddress.getPort());
+                                found = true;
+                                socket.close();
+                            }
+                        }
+                    socket.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             });
             threads.add(thread);
@@ -115,18 +124,18 @@ public enum ServerScrubber {
         }
     }, new TickFilter(EventTick.Mode.PRE));
 
-    private void sendHandshake(DataOutputStream dataOutputStream, ServerAddress serverAddress) {
+    private void sendHandshake(DataOutputStream dataOutputStream, MinecraftServerAddress serverAddress) {
         try {
             ByteArrayOutputStream handshakeBytes = new ByteArrayOutputStream();
             DataOutputStream handshakePacket = new DataOutputStream(handshakeBytes);
-            handshakePacket.writeByte(ServerPinger.PACKET_HANDSHAKE);//packet id
-            ServerPinger.writeVarInt(handshakePacket, ServerPinger.PROTOCOL_VERSION);//protocol version
-            ServerPinger.writeVarInt(handshakePacket, serverAddress.getAddress().length());//length of address
-            handshakePacket.writeBytes(serverAddress.getAddress());//address
+            handshakePacket.writeByte(0x00);//packet id
+            writeVarInt(handshakePacket, SharedConstants.getProtocolVersion());//protocol version
+            writeVarInt(handshakePacket, serverAddress.getIp().length());//length of address
+            handshakePacket.writeBytes(serverAddress.getIp());//address
             handshakePacket.writeShort(serverAddress.getPort());//port
-            ServerPinger.writeVarInt(handshakePacket, NetworkState.STATUS.getId());//status id
+            writeVarInt(handshakePacket, NetworkState.STATUS.getId());//status id
 
-            ServerPinger.writeVarInt(dataOutputStream, handshakeBytes.size());//size of data
+            writeVarInt(dataOutputStream, handshakeBytes.size());//size of data
             dataOutputStream.write(handshakeBytes.toByteArray());//data
             handshakeBytes.close();
             handshakePacket.close();
@@ -136,18 +145,52 @@ public enum ServerScrubber {
     private void sendQueryRequest(DataOutputStream dataOutputStream) {
         try {
             dataOutputStream.writeByte(0x01);//size of data
-            dataOutputStream.writeByte(ServerPinger.PACKET_STATUSREQUEST);//packet id
+            dataOutputStream.writeByte(0x00);//packet id
         } catch (Exception e) {e.printStackTrace();}
     }
 
     private String receiveQueryRequest(DataInputStream dataInputStream) {
         try {
-            int strLength = ServerPinger.readVarInt(dataInputStream);
+            int strLength = readVarInt(dataInputStream);
             byte[] strBytes = new byte[strLength];
             dataInputStream.readFully(strBytes);
             return new String(strBytes);
         } catch (Exception e) {e.printStackTrace();}
         return "null";
+    }
+
+
+
+    private void writeVarInt(DataOutputStream out, int paramInt) throws IOException {
+        while (true) {
+            if ((paramInt & 0xFFFFFF80) == 0) {
+                out.write(paramInt);
+                return;
+            }
+
+            out.write(paramInt & 0x7F | 0x80);
+            paramInt >>>= 7;
+        }
+    }
+
+    private int readVarInt(DataInputStream in) throws IOException {
+        int i = 0;
+        int j = 0;
+        while (true) {
+            int k = in.readByte();
+
+            i |= (k & 0x7F) << j++ * 7;
+
+            if (j > 5) {
+                throw new RuntimeException("VarInt too big");
+            }
+
+            if ((k & 0x80) != 128) {
+                break;
+            }
+        }
+
+        return i;
     }
 
     public void loadDefaultList() {
