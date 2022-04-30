f6a77617293994ab391d1210d6a4ffdf9bfd210e
DustinRepo <tinmann4300@gmail.com>
2022-04-29 23:03:16 -0400

    Fixed FakeLag breaking Nametags on snapshots

--- src/main/java/me/dustin/jex/feature/mod/impl/misc/Fakelag.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/misc/Fakelag.java
@@ -1,36 +1,30 @@
 package me.dustin.jex.feature.mod.impl.misc;
 
-import com.mojang.authlib.GameProfile;
 import me.dustin.events.core.EventListener;
 import me.dustin.events.core.annotate.EventPointer;
 import me.dustin.jex.event.filters.ClientPacketFilter;
+import me.dustin.jex.event.filters.TickFilter;
+import me.dustin.jex.event.misc.EventTick;
 import me.dustin.jex.event.packet.EventPacketSent;
-import me.dustin.jex.helper.entity.FakePlayerEntity;
 import me.dustin.jex.helper.misc.StopWatch;
 import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.feature.mod.core.Feature;
 import me.dustin.jex.feature.option.annotate.Op;
-import net.minecraft.entity.Entity;
 import net.minecraft.network.Packet;
 
 import java.util.ArrayList;
-import java.util.UUID;
 
 @Feature.Manifest(category = Feature.Category.MISC, description = "Pretend to lag")
 public class Fakelag extends Feature {
 
     @Op(name = "Catch when", all = {"Both", "OnGround", "In Air"})
     public String catchWhen = "Both";
-    @Op(name = "Visualize")
-    public boolean visualize = true;
     @Op(name = "Choke MS", min = 50, max = 2000, inc = 10)
     public int choke = 100;
 
     private final ArrayList<Packet<?>> packets = new ArrayList<>();
     private final StopWatch stopWatch = new StopWatch();
     private boolean sending = false;
-
-    private FakePlayerEntity fakePlayerEntity;
 
     @EventPointer
     private final EventListener<EventPacketSent> eventPacketSentEventListener = new EventListener<>(event -> {
@@ -39,7 +33,6 @@ public class Fakelag extends Feature {
         if (Wrapper.INSTANCE.getLocalPlayer() == null) {
             packets.clear();
             stopWatch.reset();
-            fakePlayerEntity = null;
         }
         if (!stopWatch.hasPassed(choke) && shouldCatchPackets()) {
             packets.add(event.getPacket());
@@ -48,17 +41,18 @@ public class Fakelag extends Feature {
             sending = true;
             packets.forEach(Wrapper.INSTANCE.getLocalPlayer().networkHandler::sendPacket);
             sending = false;
-            if (visualize) {
-                if (fakePlayerEntity == null)
-                    createFakePlayer();
-                fakePlayerEntity.copyPositionAndRotation(Wrapper.INSTANCE.getLocalPlayer());
-            } else {
-                removeFakePlayer();
-            }
             packets.clear();
             stopWatch.reset();
         }
     }, new ClientPacketFilter(EventPacketSent.Mode.PRE));
+
+    @EventPointer
+    private final EventListener<EventTick> eventTickEventListener = new EventListener<>(event -> {
+        if (Wrapper.INSTANCE.getLocalPlayer() == null) {
+            packets.clear();
+            stopWatch.reset();
+        }
+    }, new TickFilter(EventTick.Mode.PRE));
 
     private boolean shouldCatchPackets() {
         return switch (catchWhen.toLowerCase()) {
@@ -71,8 +65,6 @@ public class Fakelag extends Feature {
 
     @Override
     public void onEnable() {
-        if (Wrapper.INSTANCE.getWorld() != null)
-            createFakePlayer();
         super.onEnable();
     }
 
@@ -84,22 +76,6 @@ public class Fakelag extends Feature {
             } catch (Exception e) {}
             packets.clear();
         }
-        removeFakePlayer();
-        fakePlayerEntity = null;
         super.onDisable();
-    }
-
-    public void removeFakePlayer() {
-        if (fakePlayerEntity != null && Wrapper.INSTANCE.getWorld() != null) {
-            fakePlayerEntity.setPos(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
-            Wrapper.INSTANCE.getWorld().removeEntity(40000, Entity.RemovalReason.DISCARDED);
-        }
-    }
-
-    public void createFakePlayer() {
-        this.fakePlayerEntity = new FakePlayerEntity(Wrapper.INSTANCE.getWorld(), new GameProfile(UUID.randomUUID(), "Fakelag Player"));
-        fakePlayerEntity.copyFrom(Wrapper.INSTANCE.getLocalPlayer());
-        fakePlayerEntity.copyPositionAndRotation(Wrapper.INSTANCE.getLocalPlayer());
-        Wrapper.INSTANCE.getWorld().addEntity(40000, fakePlayerEntity);
     }
 }
