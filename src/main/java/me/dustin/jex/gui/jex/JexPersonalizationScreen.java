7e893e304900a04f9e659f57b31b3b96fd7182f4
DustinRepo <tinmann4300@gmail.com>
2022-05-14 14:24:44 -0400

    Fixed a Hat being set even if you don't have one enabled in the Personalization screen

--- src/main/java/me/dustin/jex/gui/jex/JexPersonalizationScreen.java
+++ src/main/java/me/dustin/jex/gui/jex/JexPersonalizationScreen.java
@@ -75,6 +75,7 @@ public class JexPersonalizationScreen extends Screen {
             String uuid = Wrapper.INSTANCE.getMinecraft().getSession().getUuid().replace("-", "");
             if (selectedHat == -1) {
                 Hat.clearHat(uuid);
+                setHat = "None";
             } else {
                 Hat.HatType hatType = Hat.HatType.values()[selectedHat];
                 Hat.setHat(uuid, hatType.name());
--- src/main/java/me/dustin/jex/load/JexLoad.java
+++ src/main/java/me/dustin/jex/load/JexLoad.java
@@ -8,7 +8,7 @@ public class JexLoad implements ModInitializer {
     @Override
     public void onInitialize() {
         //Place for things that require fabric to load assets before being loaded
-        JexPlugin.fabricLoad();
         new Hat().load();
+        JexPlugin.fabricLoad();
     }
 }
