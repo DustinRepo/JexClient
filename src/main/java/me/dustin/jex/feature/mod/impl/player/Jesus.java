baf0675ed75cfec5be6b7a87a3194316e37c41d8
DustinRepo <tinmann4300@gmail.com>
2022-06-24 19:13:53 -0400

    Added Swim mode to Jesus

--- src/main/java/me/dustin/jex/feature/mod/impl/player/Jesus.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/player/Jesus.java
@@ -3,12 +3,16 @@ package me.dustin.jex.feature.mod.impl.player;
 import me.dustin.events.core.EventListener;
 import me.dustin.events.core.annotate.EventPointer;
 import me.dustin.events.core.priority.Priority;
+import me.dustin.jex.JexClient;
 import me.dustin.jex.event.filters.ClientPacketFilter;
 import me.dustin.jex.event.filters.PlayerPacketsFilter;
+import me.dustin.jex.event.filters.SoundFilter;
 import me.dustin.jex.event.packet.EventPacketSent;
+import me.dustin.jex.event.player.EventGetPose;
 import me.dustin.jex.event.player.EventMove;
 import me.dustin.jex.event.player.EventPlayerPackets;
 import me.dustin.jex.event.world.EventBlockCollisionShape;
+import me.dustin.jex.event.world.EventPlaySound;
 import me.dustin.jex.feature.mod.core.Category;
 import me.dustin.jex.feature.mod.core.Feature;
 import me.dustin.jex.feature.property.Property;
@@ -16,9 +20,14 @@ import me.dustin.jex.helper.baritone.BaritoneHelper;
 import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.helper.player.PlayerHelper;
 import me.dustin.jex.helper.world.WorldHelper;
+import net.minecraft.block.Block;
+import net.minecraft.block.FluidBlock;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.entity.EntityPose;
 import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.WaterFluid;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShapes;
@@ -47,6 +56,31 @@ public class Jesus extends Feature {
     private final EventListener<EventPlayerPackets> eventPlayerPacketsEventListener = new EventListener<>(event -> {
         if (Wrapper.INSTANCE.getPlayer() == null || Wrapper.INSTANCE.getWorld() == null)
             return;
+        if (modeProperty.value() == Mode.SWIM) {
+            if (Wrapper.INSTANCE.getLocalPlayer().isSneaking())
+                return;
+            if (WorldHelper.INSTANCE.isInLiquid(Wrapper.INSTANCE.getPlayer())) {
+                Block insideBlock = WorldHelper.INSTANCE.getBlockBelowEntity(Wrapper.INSTANCE.getPlayer(), 0);
+                Block belowBlock = WorldHelper.INSTANCE.getBlockBelowEntity(Wrapper.INSTANCE.getPlayer(), 0.5f);
+                Vec3d orig = Wrapper.INSTANCE.getPlayer().getVelocity();
+                if (insideBlock instanceof FluidBlock && belowBlock instanceof FluidBlock && !Wrapper.INSTANCE.getPlayer().horizontalCollision) {
+                    FluidState fluidState = WorldHelper.INSTANCE.getFluidState(Wrapper.INSTANCE.getPlayer().getBlockPos());
+                    double swimHeight = Wrapper.INSTANCE.getLocalPlayer().getSwimHeight();
+                    if (fluidState.getHeight() < swimHeight || !(fluidState.getFluid() instanceof WaterFluid))
+                        return;
+                    if (!Wrapper.INSTANCE.getPlayer().isSwimming() && PlayerHelper.INSTANCE.isMoving()) {
+                        Wrapper.INSTANCE.getPlayer().setSprinting(true);
+                        Wrapper.INSTANCE.getPlayer().setVelocity(orig.getX(), -0.05, orig.getZ());
+                    } else {
+                        if (Wrapper.INSTANCE.getPlayer().isSubmergedInWater())
+                            Wrapper.INSTANCE.getPlayer().setVelocity(orig.getX(), 0.05, orig.getZ());
+                        else
+                            Wrapper.INSTANCE.getPlayer().setVelocity(orig.getX(), 0, orig.getZ());
+                    }
+                }
+            }
+            return;
+        }
         if (modeProperty.value() == Mode.SOLID && (WorldHelper.INSTANCE.isInLiquid(Wrapper.INSTANCE.getPlayer())) && !Wrapper.INSTANCE.getPlayer().isSneaking()) {
             Vec3d orig = Wrapper.INSTANCE.getPlayer().getVelocity();
             Wrapper.INSTANCE.getPlayer().setVelocity(orig.getX(), 0.11, orig.getZ());
@@ -78,8 +112,14 @@ public class Jesus extends Feature {
     }, Priority.SECOND_LAST, new PlayerPacketsFilter(EventPlayerPackets.Mode.PRE));
 
     @EventPointer
+    private final EventListener<EventPlaySound> eventPlaySoundEventListener = new EventListener<>(event -> {
+        if (modeProperty.value() == Mode.SWIM)
+            event.cancel();
+    }, new SoundFilter(EventPlaySound.Mode.PRE, new Identifier("ambient.underwater.enter"), new Identifier("ambient.underwater.exit"), new Identifier("ambient.underwater.loop"), new Identifier("entity.player.swim"), new Identifier("ambient.underwater.loop"), new Identifier("ambient.underwater.loop.additions")));
+
+    @EventPointer
     private final EventListener<EventBlockCollisionShape> eventBlockCollisionShapeEventListener = new EventListener<>(event -> {
-        if (Wrapper.INSTANCE.getPlayer() == null || Wrapper.INSTANCE.getWorld() == null || modeProperty.value() == Mode.DOLPHIN || event.getBlockPos() == null)
+        if (Wrapper.INSTANCE.getPlayer() == null || Wrapper.INSTANCE.getWorld() == null || modeProperty.value() != Mode.SOLID || event.getBlockPos() == null)
             return;
         if (Wrapper.INSTANCE.getPlayer().isSubmergedInWater() || Wrapper.INSTANCE.getPlayer().isInLava() || (event.getBlockPos().getY() < Wrapper.INSTANCE.getPlayer().getY() + 0.5f && WorldHelper.INSTANCE.isInLiquid(Wrapper.INSTANCE.getPlayer())) || Wrapper.INSTANCE.getPlayer().isSneaking() || Wrapper.INSTANCE.getPlayer().fallDistance > 3)
             return;
@@ -109,9 +149,9 @@ public class Jesus extends Feature {
     @EventPointer
     private final EventListener<EventPacketSent> eventPacketSentEventListener = new EventListener<>(event -> {
         if (WorldHelper.INSTANCE.isOnLiquid(Wrapper.INSTANCE.getPlayer()) || WorldHelper.INSTANCE.isTouchingLiquidBlockSpace(Wrapper.INSTANCE.getPlayer())) {
-            if (ticks >= 6) {
+            if (ticks >= 4) {
                 PlayerMoveC2SPacket origPacket = (PlayerMoveC2SPacket) event.getPacket();
-                PlayerMoveC2SPacket playerMoveC2SPacket = new PlayerMoveC2SPacket.Full(origPacket.getX(Wrapper.INSTANCE.getPlayer().getX()), origPacket.getY(Wrapper.INSTANCE.getPlayer().getY()) - 0.02, origPacket.getZ(Wrapper.INSTANCE.getPlayer().getZ()), origPacket.getYaw(PlayerHelper.INSTANCE.getYaw()), origPacket.getPitch(PlayerHelper.INSTANCE.getPitch()), origPacket.isOnGround());
+                PlayerMoveC2SPacket playerMoveC2SPacket = new PlayerMoveC2SPacket.Full(origPacket.getX(Wrapper.INSTANCE.getPlayer().getX()), origPacket.getY(Wrapper.INSTANCE.getPlayer().getY()) - 0.05, origPacket.getZ(Wrapper.INSTANCE.getPlayer().getZ()), origPacket.getYaw(PlayerHelper.INSTANCE.getYaw()), origPacket.getPitch(PlayerHelper.INSTANCE.getPitch()), origPacket.isOnGround());
                 event.setPacket(playerMoveC2SPacket);
                 ticks = 0;
             } else
@@ -128,6 +168,6 @@ public class Jesus extends Feature {
     }
 
     public enum Mode {
-        SOLID, DOLPHIN
+        SOLID, DOLPHIN, SWIM
     }
 }
