acef02c3177d4087839f90c4ce76fe6fa887c960
mdenials <46415607+mdenials@users.noreply.github.com>
2022-11-04 09:38:44 +0500

    Add HighJump
    a very rough sketch do not judge strictly

--- src/main/java/me/dustin/jex/feature/mod/impl/movement/HighJump.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/movement/HighJump.java
@@ -1,1 +1,31 @@
+package me.dustin.jex.feature.mod.impl.movement;
 
+import me.dustin.events.core.EventListener;
+import me.dustin.events.core.annotate.EventPointer;
+import me.dustin.jex.event.player.EventMove;
+import me.dustin.jex.feature.mod.core.Category;
+import me.dustin.jex.helper.misc.Wrapper;
+import me.dustin.jex.feature.mod.core.Feature;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.util.math.Vec3d;
+import me.dustin.jex.feature.property.Property;
+
+public class HighJump extends Feature {
+  	
+public Property <Integer> jumplevelProperty = new Property.PropertyBuilder<Integer>(this.getClass())
+.name ("Jump level")
+.description ("Blocks jump multipler, specified with a value of 1 to 1)
+.value(0)
+.min(2)
+.max(1000)
+.inc(1)
+.build();
+							
+public AntiKnockback() {
+super(Category.MOVEMENT, "Multiple the jump height".);
+}
+@Eventpointer
+private final EventListener<EventMove> eventMoveEventListener = new EventListener<>(event -> {
+if (Wrapper.INSTANCE.getLocalPlayer().jumplevel > jumplevelProperty.value())
+event.setY(+jumplevelProperty.value());
+}
