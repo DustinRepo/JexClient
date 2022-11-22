50776284e75c3b19ac737bc1c03f50a09677fb6c
mdenials <46415607+mdenials@users.noreply.github.com>
2022-11-22 20:20:27 +0500

    Update ProxyHelper.java

--- src/main/java/me/dustin/jex/helper/network/ProxyHelper.java
+++ src/main/java/me/dustin/jex/helper/network/ProxyHelper.java
@@ -62,7 +62,7 @@ public enum ProxyHelper {
             ProxyHelper.ClientProxy proxy = ProxyHelper.INSTANCE.getProxy();
             if (ProxyHelper.INSTANCE.isConnectedToProxy()) {
                 if (proxy.socksType() == ProxyHelper.SocksType.FIVE) {
-                    channel.pipeline().addFirst(new Socks5ProxyHandler(new InetSocketAddress(proxy.host(), proxy.port()), proxy.authName(), proxy.authPass()));
+                    channel.pipeline().addFirst(new Socks5ProxyHandler(new InetSocketAddress(proxy.host(), proxy.port()), proxy.username.isEmpty() ? null : proxy.username, proxy.password.isEmpty ? null : proxy.password));
                 } else {
                     channel.pipeline().addFirst(new Socks4ProxyHandler(new InetSocketAddress(proxy.host(), proxy.port())));
                 }
