a7c9fd8d6318fa8b595629675b8b61b2a1f5e5be
mdenials <46415607+mdenials@users.noreply.github.com>
2022-11-23 11:26:31 +0500

    Update ProxyHelper.java

--- src/main/java/me/dustin/jex/helper/network/ProxyHelper.java
+++ src/main/java/me/dustin/jex/helper/network/ProxyHelper.java
@@ -58,7 +58,7 @@ public enum ProxyHelper {
     }, new DrawScreenFilter(EventDrawScreen.Mode.POST, MultiplayerScreen.class));
 
     public ClientConnection clientConnection;
-        protected final initChannel(Channel channel) {
+        protected final Channel initChannel(Channel channel) {
             ProxyHelper.ClientProxy proxy = ProxyHelper.INSTANCE.getProxy();
             if (ProxyHelper.INSTANCE.isConnectedToProxy()) {
                 if (proxy.socksType() == ProxyHelper.SocksType.FIVE) {
