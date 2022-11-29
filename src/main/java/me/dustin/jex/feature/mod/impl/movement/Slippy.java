7397f5995b19c2e3a5e6d2bb6e83280139f9877b
mdenials <46415607+mdenials@users.noreply.github.com>
2022-11-29 16:20:32 +0500

    Update Slippy.java

--- src/main/java/me/dustin/jex/feature/mod/impl/movement/Slippy.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/movement/Slippy.java
@@ -1,14 +1,17 @@
 package me.dustin.jex.feature.mod.impl.movement;
 
-import net.minecraft.block.Block;
-import me.dustin.jex.feature.mod.core.Feature;
-import me.dustin.jex.feature.property.Property;
-import me.dustin.jex.feature.mod.core.Category;
 import me.dustin.events.core.EventListener;
 import me.dustin.events.core.annotate.EventPointer;
-import me.dustin.jex.event.player.EventMove;
+import me.dustin.jex.event.filters.PlayerPacketsFilter;
+import me.dustin.jex.event.player.EventPlayerPackets;
+import me.dustin.jex.feature.mod.core.Category;
+import me.dustin.jex.feature.property.Property;
 import me.dustin.jex.helper.misc.Wrapper;
-import net.minecraft.block.AbstractBlock;
+import me.dustin.jex.helper.network.NetworkHelper;
+import me.dustin.jex.feature.mod.core.Feature;
+import me.dustin.jex.helper.player.PlayerHelper;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.util.math.Vec3d;
 
 public class Slippy extends Feature {
  
@@ -16,11 +19,13 @@ public class Slippy extends Feature {
    super(Category.MOVEMENT, "Сhanges the block to ice for fast movement");
     }
  
- @EventPointer
- public final EventListener<EventMove> eventMoveEventListener = new EventListener<>(event -> {
- if (Wrapper.INSTANCE.getLocalPlayer().isOnGround()){
-  Block block;
-  this.slipperiness = 0.98F;
-    }
-  });
+private final EventListener<EventPlayerPackets> eventPlayerPacketsEventListener = new EventListener<>(event -> {
+		if (Wrapper.INSTANCE.getLocalPlayer().horizontalCollision) {
+			Vec3d orig = Wrapper.INSTANCE.getLocalPlayer().getVelocity();
+    Wrapper.INSTANCE.getLocalPlayer().setVelocity(orig.getX(), 0, orig.getZ());
+				NetworkHelper.INSTANCE.sendPacket(new PlayerMoveC2SPacket.Full(Wrapper.INSTANCE.getLocalPlayer().getX() + orig.getX() * 2, Wrapper.INSTANCE.getLocalPlayer().getZ() + orig.getZ() * 2, PlayerHelper.INSTANCE.getYaw(), PlayerHelper.INSTANCE.getPitch(), false));
+				NetworkHelper.INSTANCE.sendPacket(new PlayerMoveC2SPacket.Full(Wrapper.INSTANCE.getLocalPlayer().getX() + orig.getX(), Wrapper.INSTANCE.getLocalPlayer().getZ() + orig.getZ(), PlayerHelper.INSTANCE.getYaw(), PlayerHelper.INSTANCE.getPitch(), true));
+			}
+		}
+	}, new PlayerPacketsFilter(EventPlayerPackets.Mode.PRE));
 }
