48d3908a93f5ac646a21a3b8ebbfa9796dd8b798
mdenials <46415607+mdenials@users.noreply.github.com>
2022-11-29 15:31:08 +0500

    Update Slippy.java

--- src/main/java/me/dustin/jex/feature/mod/impl/movement/Slippy.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/movement/Slippy.java
@@ -11,15 +11,6 @@ import me.dustin.jex.helper.misc.Wrapper;
 import net.minecraft.block.AbstractBlock;
 
 public class Slippy extends Feature {
-
-public final Property<Float> friction = new Property.PropertyBuilder<Float>(this.getClass())
-.name("Friction")
-.description("The base friction level.")
-.value(0.01f)
-.min(0.01f)
-.max(1.10f)
-.inc(0.01f)
-.build();
  
  public Slippy() {
    super(Category.MOVEMENT, "Сhanges the block to ice for fast movement");
@@ -27,9 +18,9 @@ public final Property<Float> friction = new Property.PropertyBuilder<Float>(this
  
  @EventPointer
  private final EventListener<EventMove> eventMoveEventListener = new EventListener<>(event -> {
- if (Wrapper.INSTANCE.getBlock().getLocalPlayer().isOnGround()){
+ if (Wrapper.INSTANCE.getLocalPlayer().isOnGround()){
   Block block;
-  block.slipperiness = friction.value();
+  block.slipperiness = 0.98F;
     }
   });
 }
