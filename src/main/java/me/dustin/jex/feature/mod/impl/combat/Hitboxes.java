7d7110cd16b464a571f38aaada06c49e18939048
mdenials <46415607+mdenials@users.noreply.github.com>
2022-11-29 19:23:44 +0500

    Update Hitboxes.java

--- src/main/java/me/dustin/jex/feature/mod/impl/combat/Hitboxes.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/combat/Hitboxes.java
@@ -10,24 +10,24 @@ import me.dustin.jex.feature.mod.core.Feature;
 
 public class Hitboxes extends Feature {
 
-    public final Property<Float> expansionXProperty = new Property.PropertyBuilder<Float>(this.getClass())
-            .name("Expansion")
+    public final Property<Float> expandXProperty = new Property.PropertyBuilder<Float>(this.getClass())
+            .name("ExpandX")
             .value(0.1f)
             .min(0.1f)
             .max(1f)
             .inc(0.1f)
             .build();
     
-     public final Property<Float> expansionYProperty = new Property.PropertyBuilder<Float>(this.getClass())
-            .name("Expansion")
+     public final Property<Float> expandYProperty = new Property.PropertyBuilder<Float>(this.getClass())
+            .name("ExpandY")
             .value(0.1f)
             .min(0.1f)
             .max(1f)
             .inc(0.1f)
             .build();
     
-     public final Property<Float> expansionZProperty = new Property.PropertyBuilder<Float>(this.getClass())
-            .name("Expansion")
+     public final Property<Float> expandZProperty = new Property.PropertyBuilder<Float>(this.getClass())
+            .name("ExpandZ")
             .value(0.1f)
             .min(0.1f)
             .max(1f)
@@ -42,6 +42,6 @@ public class Hitboxes extends Feature {
     private final EventListener<EventEntityHitbox> eventEntityHitboxEventListener = new EventListener<>(event -> {
         if (event.getEntity() == null || Wrapper.INSTANCE.getLocalPlayer() == null || event.getEntity().getId() == Wrapper.INSTANCE.getLocalPlayer().getId())
             return;
-        event.setBox(event.getBox().expand(expansionXProperty.value(), expansionyProperty.value(), expansionZProperty.value()));
+        event.setBox(event.getBox().expand(expandXProperty.value(), expandYProperty.value(), expandZProperty.value()));
     });
 }
