2ddbe956883def7994e8e6ea054d08d41d0f924e
mdenials <46415607+mdenials@users.noreply.github.com>
2022-11-29 18:51:18 +0500

    Update Slippy.java

--- src/main/java/me/dustin/jex/feature/mod/impl/movement/Slippy.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/movement/Slippy.java
@@ -22,7 +22,7 @@ public class Slippy extends Feature {
 
 	@EventPointer
 	private final EventListener<EventPlayerPackets> eventPlayerPacketsEventListener = new EventListener<>(event -> {
-		if (Wrapper.INSTANCE.getLocalPlayer().isOnGround().horizontalCollision) {
+		if (Wrapper.INSTANCE.getLocalPlayer().isOnGround()) {
 			Vec3d orig = Wrapper.INSTANCE.getLocalPlayer().getVelocity();
 				Wrapper.INSTANCE.getLocalPlayer().setVelocity(0.98F, orig.getY() ,0.98F);	
 		}
