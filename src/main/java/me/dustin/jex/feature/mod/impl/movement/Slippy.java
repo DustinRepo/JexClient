54916795aab4aaf645bd067b2a800aca82a59a89
mdenials <46415607+mdenials@users.noreply.github.com>
2022-11-29 13:30:07 +0500

    Add files via upload

---
+++ src/main/java/me/dustin/jex/feature/mod/impl/movement/Slippy.java
@@ -0,0 +1,19 @@
+package me.dustin.jex.feature.mod.impl.movement.speed;
+import net.minecraft.block.Block;
+
+public class Slippy extends Feature {
+
+ public Slippy() {
+        super(Category.MOVEMENT, "Сhanges the block to ice for fast movement");
+    }
+
+public final Property<Float> friction = new Property.PropertyBuilder<Float>(this.getClass())
+.name("Friction")
+.description("The base friction level.")
+.value(0.01f)
+.min(0.01f)
+.max(1.10f)
+.inc(0.01f)
+.build();
+
+}
