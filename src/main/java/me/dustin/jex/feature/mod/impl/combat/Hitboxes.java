5be41f49b3a18d4a1633bef69fa75aca5f825d4b
mdenials <46415607+mdenials@users.noreply.github.com>
2022-11-29 19:22:10 +0500

    Update Hitboxes.java

--- src/main/java/me/dustin/jex/feature/mod/impl/combat/Hitboxes.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/combat/Hitboxes.java
@@ -10,7 +10,23 @@ import me.dustin.jex.feature.mod.core.Feature;
 
 public class Hitboxes extends Feature {
 
-    public final Property<Float> expansionProperty = new Property.PropertyBuilder<Float>(this.getClass())
+    public final Property<Float> expansionXProperty = new Property.PropertyBuilder<Float>(this.getClass())
+            .name("Expansion")
+            .value(0.1f)
+            .min(0.1f)
+            .max(1f)
+            .inc(0.1f)
+            .build();
+    
+     public final Property<Float> expansionYProperty = new Property.PropertyBuilder<Float>(this.getClass())
+            .name("Expansion")
+            .value(0.1f)
+            .min(0.1f)
+            .max(1f)
+            .inc(0.1f)
+            .build();
+    
+     public final Property<Float> expansionZProperty = new Property.PropertyBuilder<Float>(this.getClass())
             .name("Expansion")
             .value(0.1f)
             .min(0.1f)
@@ -26,6 +42,6 @@ public class Hitboxes extends Feature {
     private final EventListener<EventEntityHitbox> eventEntityHitboxEventListener = new EventListener<>(event -> {
         if (event.getEntity() == null || Wrapper.INSTANCE.getLocalPlayer() == null || event.getEntity().getId() == Wrapper.INSTANCE.getLocalPlayer().getId())
             return;
-        event.setBox(event.getBox().expand(expansionProperty.value(), 0, expansionProperty.value()));
+        event.setBox(event.getBox().expand(expansionXProperty.value(), expansionyProperty.value(), expansionZProperty.value()));
     });
 }
