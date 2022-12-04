5c380a709d4f57a9e86d73d24d64bced87838e12
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-04 12:54:14 +0500

    Update PacketCanceller.java

--- src/main/java/me/dustin/jex/feature/mod/impl/misc/PacketCanceller.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/misc/PacketCanceller.java
@@ -1,5 +1,9 @@
 package me.dustin.jex.feature.mod.impl.misc;
 
+import io.netty.buffer.Unpooled;
+import me.dustin.jex.event.filters.ClientPacketFilter;
+import me.dustin.jex.event.filters.DirectClientPacketFilter;
+import net.minecraft.network.PacketByteBuf;
 import me.dustin.jex.feature.property.Property;
 import me.dustin.jex.feature.mod.core.Category;
 import me.dustin.jex.feature.mod.core.Feature;
@@ -8,6 +12,7 @@ import me.dustin.events.core.EventListener;
 import me.dustin.jex.event.packet.EventPacketSent;
 import net.minecraft.network.packet.c2s.play.*;
 import net.minecraft.network.packet.s2c.play.*;
+import java.nio.charset.StandardCharsets;
 
 public class PacketCanceller extends Feature {
 
