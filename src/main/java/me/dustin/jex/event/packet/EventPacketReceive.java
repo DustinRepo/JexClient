0d919ff4935c372001dda0fc89a0b4d3f8e6efbc
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-04 16:47:19 +0500

    Update EventPacketReceive.java

--- src/main/java/me/dustin/jex/event/packet/EventPacketReceive.java
+++ src/main/java/me/dustin/jex/event/packet/EventPacketReceive.java
@@ -12,6 +12,16 @@ public class EventPacketReceive extends Event {
         this.packet = packet;
         this.mode = mode;
     }
+    
+       public static class Receive extends PacketEvent {
+        private static final Receive INSTANCE = new Receive();
+
+        public static Receive get(Packet<?> packet) {
+            INSTANCE.setCancelled(false);
+            INSTANCE.packet = packet;
+            return INSTANCE;
+        }
+    }
 
     public Packet<?> getPacket() {
         return packet;
