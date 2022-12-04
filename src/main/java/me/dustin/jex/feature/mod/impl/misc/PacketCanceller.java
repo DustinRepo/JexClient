5aea912c003f49df041c426bea6f544b6ba1b903
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-04 17:16:36 +0500

    Update PacketCanceller.java

--- src/main/java/me/dustin/jex/feature/mod/impl/misc/PacketCanceller.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/misc/PacketCanceller.java
@@ -778,7 +778,7 @@ public final Property<Boolean> worldtimeupdates2c = new Property.PropertyBuilder
 .build();
 
 @EventPointer
-private void onSendPacket(PacketEventSent.Sent event) {
+private void onSendPacket(EventPacketSent.Sent event) {
 
 if(boatpaddlestate.value()){
 BoatPaddleStateC2SPacket packet2;
