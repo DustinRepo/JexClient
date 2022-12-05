8eb50ef579c6cf189672fa273650b854f42ae108
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-05 17:17:30 +0500

    Update OverlayDisabler.java

--- src/main/java/me/dustin/jex/feature/mod/impl/render/OverlayDisabler.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/render/OverlayDisabler.java
@@ -9,19 +9,14 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import me.dustin.jex.event.render.EventRenderHud;
 import net.minecraft.client.gui.hud.ClientBossBar;
 import net.minecraft.entity.boss.BossBar;
-import java.util.HashMap;
-import java.util.WeakHashMap;
 
 public class OverlayDisabler extends Feature {
-	
-public static final WeakHashMap<ClientBossBar, Integer> barMap = new WeakHashMap<>();
 			
      public OverlayDisabler() {
         super(Category.VISUAL, "Removes unnecessary interface elements");
     }
 public final EventListener<EventRenderHud> eventRenderHudEventListener = new EventListener<>(event -> {
-	public ClientBossBar {
-	event.cancel();
-	}
+        ClientBossBar bossbar;
+	event.remove(bossbar);
 	)};	
 }
