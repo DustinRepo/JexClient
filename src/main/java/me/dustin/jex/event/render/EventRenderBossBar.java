a8978369c01137d13efa06ddb4542c6eb330231d
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-05 21:05:55 +0500

    Update EventRenderBossBar.java

--- src/main/java/me/dustin/jex/event/render/EventRenderBossBar.java
+++ src/main/java/me/dustin/jex/event/render/EventRenderBossBar.java
@@ -1,23 +1,15 @@
 package me.dustin.jex.event.render;
 
 import me.dustin.events.core.Event;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.boss.BossBar;
 
 public class EventRenderBossBar extends Event {
     private final BossBar bossBar;
-    private final MatrixStack poseStack;
 
-    public EventRenderBossBar(MatrixStack poseStack, BossBar bossBar) {
-        this.poseStack = poseStack;
-		this.bossBar = bossBar;
-    }
-
-    public MatrixStack getPoseStack() {
-        return this.poseStack;
+    public EventRenderBossBar(BossBar bossBar) {
+	this.bossBar = bossBar;
     }
 	public BossBar getBossBar() {
         return this.bossBar;
     }
-
 }
