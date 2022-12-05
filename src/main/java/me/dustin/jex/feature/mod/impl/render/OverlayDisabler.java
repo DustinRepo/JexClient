7f0fb9f3080ff5e4f30312b7b53d3eddaa260018
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-05 21:15:22 +0500

    Add files via upload

---
+++ src/main/java/me/dustin/jex/feature/mod/impl/render/OverlayDisabler.java
@@ -0,0 +1,35 @@
+package me.dustin.jex.feature.mod.impl.render;
+
+import me.dustin.jex.feature.property.Property;
+import me.dustin.jex.feature.mod.core.Category;
+import me.dustin.jex.feature.mod.core.Feature;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.BossBar;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.scoreboard.ScoreboardObjective;
+import net.minecraft.text.Text;
+import net.minecraft.client.gui.hud.*;
+import me.dustin.events.core.EventListener;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+public class OverlayDisabler extends Feature {
+ 
+  public final Property<Boolean> bossbarProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("BossBar")
+            .value(true)
+            .build();
+			
+			public AntiOverlay() {
+        super(Category.VISUAL, "Removes unnecessary interface elements");
+    }
+@EventPointer
+	private final EventListener<EventRenderCrosshair> eventRenderCrosshairEventListener = new EventListener<>(event -> event.cancel());	
+}
