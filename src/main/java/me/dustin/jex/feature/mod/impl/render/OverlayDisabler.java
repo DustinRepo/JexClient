c510443ceb02da6a2ff9b66f956ab4c901af22ae
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-05 21:25:46 +0500

    Update OverlayDisabler.java

--- src/main/java/me/dustin/jex/feature/mod/impl/render/OverlayDisabler.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/render/OverlayDisabler.java
@@ -3,22 +3,11 @@ package me.dustin.jex.feature.mod.impl.render;
 import me.dustin.jex.feature.property.Property;
 import me.dustin.jex.feature.mod.core.Category;
 import me.dustin.jex.feature.mod.core.Feature;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.BossBar;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.entity.EntityRenderer;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.entity.Entity;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.scoreboard.ScoreboardObjective;
 import net.minecraft.text.Text;
-import net.minecraft.client.gui.hud.*;
 import me.dustin.events.core.EventListener;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import me.dustin.jex.event.render.EventRenderBossBar;
+import me.dustin.events.core.annotate.EventPointer;
 
 public class OverlayDisabler extends Feature {
  
