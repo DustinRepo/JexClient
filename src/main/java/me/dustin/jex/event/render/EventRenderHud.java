fb6eb8e15ac7ea47c304ff5206aa048aaee0b2ab
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-05 14:53:34 +0500

    Update EventRenderHud.java

--- src/main/java/me/dustin/jex/event/render/EventRenderHud.java
+++ src/main/java/me/dustin/jex/event/render/EventRenderHud.java
@@ -4,12 +4,12 @@ import me.dustin.events.core.Event;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
  
- public class EventRenderChatHud extends Event {
+ public class EventRenderHud extends Event {
     private final InGameHud ingamehud;
     private final MatrixStack poseStack;
     private final int tickDelta;
 	
-	public EventRenderHud(InGameHud ingamehud, MatrixStack poseStack, int tickDelta) {
+public EventRenderHud(InGameHud ingamehud, MatrixStack poseStack, int tickDelta) {
         this.ingamehud = ingamehud;
         this.poseStack = poseStack;
         this.tickDelta = tickDelta;
