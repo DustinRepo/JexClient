513d4871629bf4e9a2cb455082e270000a172ad3
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-05 14:29:42 +0500

    Add files via upload

---
+++ src/main/java/me/dustin/jex/event/render/EventRenderHud.java
@@ -0,0 +1,29 @@
+package me.dustin.jex.event.render;
+
+import me.dustin.events.core.Event;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+ 
+ public class EventRenderChatHud extends Event {
+    private final InGameHud ingamehud;
+    private final MatrixStack poseStack;
+    private final int tickDelta;
+	
+	public EventRenderChatHud(InGameHud ingamehud, MatrixStack poseStack, int tickDelta) {
+        this.ingamehud = ingamehud;
+        this.poseStack = poseStack;
+        this.tickDelta = tickDelta;
+    }
+	
+	 public ChatHud getChatHud() {
+        return chatHud;
+    }
+	
+	  public MatrixStack getPoseStack() {
+        return poseStack;
+    }
+
+    public int getTickDelta() {
+        return tickDelta;
+    }
+}
