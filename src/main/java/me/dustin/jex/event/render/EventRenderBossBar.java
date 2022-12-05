01d531180b6a69b3d6d67637e7653c5b841acffd
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-05 21:43:36 +0500

    Update EventRenderBossBar.java

--- src/main/java/me/dustin/jex/event/render/EventRenderBossBar.java
+++ src/main/java/me/dustin/jex/event/render/EventRenderBossBar.java
@@ -1,15 +1,15 @@
 package me.dustin.jex.event.render;
 
 import me.dustin.events.core.Event;
-import net.minecraft.entity.boss.BossBar;
+import net.minecraft.client.gui.hud.ClientBossBar;
 
 public class EventRenderBossBar extends Event {
-    private final BossBar bossBar;
+    private final ClientBossBar bossBar;
 
-    public EventRenderBossBar(BossBar bossBar) {
+    public EventRenderBossBar(ClientBossBar bossBar) {
 	this.bossBar = bossBar;
     }
-	public BossBar getBossBar() {
+	public ClientBossBar getBossBar() {
         return this.bossBar;
     }
 }
