a486279ed78c8dfefb96c9040d3eb4aa0e3a704a
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-08 10:46:53 +0500

    Update EventRenderBossBar.java

--- src/main/java/me/dustin/jex/event/render/EventRenderBossBar.java
+++ src/main/java/me/dustin/jex/event/render/EventRenderBossBar.java
@@ -2,14 +2,43 @@ package me.dustin.jex.event.render;
 
 import me.dustin.events.core.Event;
 import net.minecraft.client.gui.hud.BossBarHud;
+import net.minecraft.client.gui.hud.ClientBossBar;
+import net.minecraft.text.Text;
+import java.util.Iterator;
 
 public class EventRenderBossBar extends Event {
-    private final BossBarHud bossBar;
+ public static class BossText {
+        private static final BossText INSTANCE = new BossText();
 
-    public EventRenderBossBar(BossBarHud bossBar) {
-	this.bossBar = bossBar;
+        public ClientBossBar bossBar;
+        public Text name;
+
+        public static BossText get(ClientBossBar bossBar, Text name) {
+            INSTANCE.bossBar = bossBar;
+            INSTANCE.name = name;
+            return INSTANCE;
+        }
     }
-	public BossBarHud getBossBar() {
-        return this.bossBar;
+
+    public static class BossSpacing {
+        private static final BossSpacing INSTANCE = new BossSpacing();
+
+        public int spacing;
+
+        public static BossSpacing get(int spacing) {
+            INSTANCE.spacing = spacing;
+            return INSTANCE;
+        }
+    }
+
+    public static class BossIterator {
+        private static final BossIterator INSTANCE = new BossIterator();
+
+        public Iterator<ClientBossBar> iterator;
+
+        public static BossIterator get(Iterator<ClientBossBar> iterator) {
+            INSTANCE.iterator = iterator;
+            return INSTANCE;
+        }
     }
 }
