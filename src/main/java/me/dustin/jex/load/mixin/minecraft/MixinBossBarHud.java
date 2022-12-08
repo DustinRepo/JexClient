52c13fa07d15efac4c20e7ab36a83b1da0775225
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-08 11:31:53 +0500

    Update MixinBossBarHud.java

--- src/main/java/me/dustin/jex/load/mixin/minecraft/MixinBossBarHud.java
+++ src/main/java/me/dustin/jex/load/mixin/minecraft/MixinBossBarHud.java
@@ -1,15 +1,15 @@
 package me.dustin.jex.load.mixin.minecraft;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import me.dustin.jex.feature.mod.impl.render.UIDisabler;
 import net.minecraft.client.gui.hud.BossBarHud;
+import net.minecraft.client.gui.hud.ClientBossBar;
 import me.dustin.jex.feature.property.Property;
-import me.dustin.jex.feature.mod.core.FeatureManager;
 import net.minecraft.client.util.math.MatrixStack;
 import me.dustin.jex.event.render.EventRenderBossBar;
+import net.minecraft.text.Text;
 import java.util.Collection;
 import java.util.Iterator;
 
