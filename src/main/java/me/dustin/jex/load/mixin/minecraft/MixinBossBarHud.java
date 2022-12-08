2ed7bb2d05045eea627f8143a0fdefa2e3e8142e
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-08 11:48:13 +0500

    Update MixinBossBarHud.java

--- src/main/java/me/dustin/jex/load/mixin/minecraft/MixinBossBarHud.java
+++ src/main/java/me/dustin/jex/load/mixin/minecraft/MixinBossBarHud.java
@@ -6,9 +6,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import me.dustin.jex.feature.mod.impl.render.UIDisabler;
 import net.minecraft.client.gui.hud.BossBarHud;
 import net.minecraft.client.gui.hud.ClientBossBar;
-import me.dustin.jex.feature.property.Property;
 import net.minecraft.client.util.math.MatrixStack;
 import me.dustin.jex.event.render.EventRenderBossBar;
+import me.dustin.jex.feature.mod.core.Feature;
 import net.minecraft.text.Text;
 import java.util.Collection;
 import java.util.Iterator;
@@ -17,25 +17,7 @@ import java.util.Iterator;
 public class MixinBossBarHud {
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
     private void onRender(CallbackInfo info) {
-        if (bossbarProperty.value()) 
+        if (Feature.get().get(UIDisabler.class).onEnable()) 
             info.cancel();
-    }
-
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Ljava/util/Collection;iterator()Ljava/util/Iterator;"))
-    public Iterator<ClientBossBar> onRender(Collection<ClientBossBar> collection) {
-        EventRenderBossBar.BossIterator event = EventRenderBossBar(EventRenderBossBar.BossIterator.get(collection.iterator()));
-        return event.iterator;
-    }
-
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/ClientBossBar;getName()Lnet/minecraft/text/Text;"))
-    public Text onAsFormattedString(ClientBossBar clientBossBar) {
-        EventRenderBossBar.BossText event = EventRenderBossBar(EventRenderBossBar.BossText.get(clientBossBar, clientBossBar.getName()));
-        return event.name;
-    }
-
-    @ModifyConstant(method = "render", constant = @Constant(intValue = 9, ordinal = 1))
-    public int modifySpacingConstant(int j) {
-        EventRenderBossBar.BossSpacing event = EventRenderBossBar(EventRenderBossBar.BossSpacing.get(j));
-        return event.spacing;
     }
 }
