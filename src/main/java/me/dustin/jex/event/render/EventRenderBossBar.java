5e0a892fc1265a57d462e0737d3bb2f346e99f73
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-09 13:30:58 +0500

    Update EventRenderBossBar.java

--- src/main/java/me/dustin/jex/event/render/EventRenderBossBar.java
+++ src/main/java/me/dustin/jex/event/render/EventRenderBossBar.java
@@ -4,10 +4,6 @@ import me.dustin.events.core.Event;
 import net.minecraft.client.gui.hud.BossBarHud;
 import net.minecraft.client.gui.hud.ClientBossBar;
 import net.minecraft.text.Text;
-import java.util.Iterator;
-
-package me.dustin.jex.event.render;
-
 import me.dustin.events.core.Event;
 import net.minecraft.client.util.math.MatrixStack;
 
