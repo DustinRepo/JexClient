dac7291d2ba6df9d3c6b83f4a6c7c7f3fb91d7de
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-09 16:13:33 +0500

    Update MixinBossBarHud.java

--- src/main/java/me/dustin/jex/load/mixin/minecraft/MixinBossBarHud.java
+++ src/main/java/me/dustin/jex/load/mixin/minecraft/MixinBossBarHud.java
@@ -19,15 +19,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BossBarHud.class)
 public class MixinBossBarHud {
  @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/BossBarHud;render(Lnet/minecraft/client/util/math/MatrixStack;)V"))
-    public void renderBossBarHud(BossBarHud bossbarhud, MatrixStack matrixStack,CallbackInfo ci) {
-        if (UIDisabler.INSTANCE.bossbar) {
+    public void renderBossBarHud(BossBarHud bossbarhud) {
+     EventRenderBossBar eventRenderBossBar = new EventRenderBossBar(bossbarhud).run();
+        if (eventRenderBossBar.isCancelled()) {
             bossbarhud.render(matrixStack);
         }
     }
  
  @Inject(method = "render", at=@At("HEAD"), cancellable = true)
     public void render(CallbackInfo info) {
-        if (UIDisabler.INSTANCE.removehud) {
+  EventRenderBossBar eventRenderBossBar;
+        if (eventRenderBossBar.isCancelled()) {
             info.cancel();
         }
     }
