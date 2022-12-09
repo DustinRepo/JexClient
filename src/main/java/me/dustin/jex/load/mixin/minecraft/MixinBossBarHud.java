ce9e641df14d0b27649c19610d161564d032fe28
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-09 15:26:43 +0500

    Update MixinBossBarHud.java

--- src/main/java/me/dustin/jex/load/mixin/minecraft/MixinBossBarHud.java
+++ src/main/java/me/dustin/jex/load/mixin/minecraft/MixinBossBarHud.java
@@ -10,6 +10,7 @@ import me.dustin.jex.event.render.EventRenderBossBar;
 import me.dustin.jex.feature.property.Property;
 import me.dustin.jex.feature.mod.core.Feature;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -17,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BossBarHud.class)
 public class MixinBossBarHud {
- @Redirect(method = "render", at = @At(value = "INVOKE", cancellable = true, target = "Lnet/minecraft/client/gui/hud/BossBarHud;render(Lnet/minecraft/client/util/math/MatrixStack;)V"))
+ @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/BossBarHud;render(Lnet/minecraft/client/util/math/MatrixStack;)V"))
     public void renderBossBarHud(BossBarHud bossbarhud, MatrixStack matrixStack,CallbackInfo ci) {
         if (UIDisabler.INSTANCE.bossbar) {
             bossbarhud.render(matrixStack);
