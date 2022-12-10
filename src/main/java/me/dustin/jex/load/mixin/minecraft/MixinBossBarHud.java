e45eadbbf10f80c6270f1d68fbb987eb0bc8ee19
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-10 11:09:49 +0500

    Update MixinBossBarHud.java

--- src/main/java/me/dustin/jex/load/mixin/minecraft/MixinBossBarHud.java
+++ src/main/java/me/dustin/jex/load/mixin/minecraft/MixinBossBarHud.java
@@ -19,8 +19,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BossBarHud.class)
 public class MixinBossBarHud {
  @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/BossBarHud;render(Lnet/minecraft/client/util/math/MatrixStack;)V"))
-    public void renderBossBarHud(BossBarHud bossbarhud, MatrixStack matrixStack, CallbackInfo info) {
-     EventRenderBossBar eventRenderBossBar = new EventRenderBossBar(bossbarhud, matrixStack).run();
+    public void renderBossBarHud(BossBarHud bossbarhud, ClientBossBar cbossbar, MatrixStack matrixStack, CallbackInfo info) {
+     EventRenderBossBar eventRenderBossBar = new EventRenderBossBar(bossbarhud, cbossbar, matrixStack).run();
         if (eventRenderBossBar.isCancelled()) {
             info.cancel();
         }
