ef81e6724bcf3fcc3a565f830285bc8663d0461e
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-10 13:12:39 +0500

    Update MixinBossBarHud.java

--- src/main/java/me/dustin/jex/load/mixin/minecraft/MixinBossBarHud.java
+++ src/main/java/me/dustin/jex/load/mixin/minecraft/MixinBossBarHud.java
@@ -19,7 +19,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BossBarHud.class)
 public class MixinBossBarHud {
  @Inject(method = "render", at = @At("HEAD"), cancellable = true)
-    public void renderBossBarHud(ClientBossBar cbossbar, CallbackInfo info) {
+    public void renderBossBarHud(BossBarHud cbossbar, CallbackInfo info) {
      EventRenderBossBar eventRenderBossBar = new EventRenderBossBar(cbossbar).run();
         if (eventRenderBossBar.isCancelled()) {
             info.cancel();
