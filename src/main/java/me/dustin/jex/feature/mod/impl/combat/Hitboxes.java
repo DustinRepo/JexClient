523d61a57c47a3f2a94a43329475ee80946a4329
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-15 08:25:52 +0500

    Update Hitboxes.java

--- src/main/java/me/dustin/jex/feature/mod/impl/combat/Hitboxes.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/combat/Hitboxes.java
@@ -33,6 +33,56 @@ public class Hitboxes extends Feature {
             .max(1f)
             .inc(0.1f)
             .build();
+    
+    	public final Property<Boolean> playerProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("Player")
+            .value(true)
+            .build();
+			
+    public final Property<Boolean> neutralProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("Neutral")
+            .value(false)
+            .build();
+			
+    public final Property<Boolean> bossProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("Boss")
+            .value(true)
+            .build();
+			
+    public final Property<Boolean> hostileProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("Hostile")
+            .value(true)
+            .build();
+			
+    public final Property<Boolean> passiveProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("Passive")
+            .value(true)
+            .build();
+			public final Property<Boolean> specificFilterProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("Specific Filter")
+            .value(true)
+            .build();
+			
+    public final Property<Boolean> ironGolemProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("Iron Golem")
+            .value(true)
+            .parent(specificFilterProperty)
+            .depends(parent -> (boolean) parent.value())
+            .build();
+			
+    public final Property<Boolean> piglinProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("Piglin")
+            .value(true)
+            .parent(specificFilterProperty)
+            .depends(parent -> (boolean) parent.value())
+            .build();
+			
+    public final Property<Boolean> zombiePiglinProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("Zombie Piglin")
+            .value(false)
+            .parent(specificFilterProperty)
+            .depends(parent -> (boolean) parent.value())
+            .build();
 
     public Hitboxes() {
         super(Category.COMBAT, "Resize entity hitboxes to make them easier to hit");
@@ -40,8 +90,27 @@ public class Hitboxes extends Feature {
 
     @EventPointer
     private final EventListener<EventEntityHitbox> eventEntityHitboxEventListener = new EventListener<>(event -> {
+        public boolean isEnabled(Entity entity) {
         if (event.getEntity() == null || Wrapper.INSTANCE.getLocalPlayer() == null || event.getEntity().getId() == Wrapper.INSTANCE.getLocalPlayer().getId())
             return;
-        event.setBox(event.getBox().expand(expandXProperty.value(), expandYProperty.value(), expandZProperty.value()));
+			if (specificFilterProperty.value()) {
+            if (entity instanceof IronGolemEntity)
+                return ironGolemProperty.value();
+            if (entity instanceof ZombifiedPiglinEntity)
+                return zombiePiglinProperty.value();
+            if (entity instanceof PiglinEntity)
+                return piglinProperty.value();
+        }
+         if (EntityHelper.INSTANCE.isPassiveMob(entity) && !EntityHelper.INSTANCE.doesPlayerOwn(entity))
+            return passiveProperty.value();
+        if (EntityHelper.INSTANCE.isBossMob(entity))
+            return bossProperty.value();
+        if (EntityHelper.INSTANCE.isHostileMob(entity))
+            return hostileProperty.value();
+        if (EntityHelper.INSTANCE.isNeutralMob(entity))
+            return neutralProperty.value();
+        return false;
+    }
+event.setBox(event.getBox().expand(expandXProperty.value(), expandYProperty.value(), expandZProperty.value()));        
     });
 }
