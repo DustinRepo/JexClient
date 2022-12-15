1d66d161268f4a98f9718aaf84f7f94e0fb9a152
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-15 08:26:50 +0500

    Update Hitboxes.java

--- src/main/java/me/dustin/jex/feature/mod/impl/combat/Hitboxes.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/combat/Hitboxes.java
@@ -3,6 +3,7 @@ package me.dustin.jex.feature.mod.impl.combat;
 import me.dustin.events.core.EventListener;
 import me.dustin.events.core.annotate.EventPointer;
 import me.dustin.jex.event.misc.EventEntityHitbox;
+import me.dustin.jex.helper.entity.EntityHelper;
 import me.dustin.jex.feature.mod.core.Category;
 import me.dustin.jex.feature.property.Property;
 import me.dustin.jex.helper.misc.Wrapper;
