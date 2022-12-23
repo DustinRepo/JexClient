f523bfe8759538bee3b9ddd2b2f485e0ee1908e8
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-23 22:17:35 +0500

    Add files via upload

---
+++ src/main/java/me/dustin/jex/feature/mod/impl/movement/Gravity.java
@@ -0,0 +1,27 @@
+package me.dustin.jex.feature.mod.impl.movement;
+
+import me.dustin.events.core.EventListener;
+import me.dustin.events.core.annotate.EventPointer;
+import me.dustin.jex.event.filters.PlayerPacketsFilter;
+import me.dustin.jex.event.player.EventPlayerPackets;
+import me.dustin.jex.feature.mod.core.Category;
+import me.dustin.jex.helper.entity.EntityHelper;
+import me.dustin.jex.helper.misc.Wrapper;
+import me.dustin.jex.helper.player.PlayerHelper;
+import me.dustin.jex.feature.mod.core.Feature;
+
+public class Gravity extends Feature {
+
+    public Gravity() {
+        super(Category.MOVEMENT, "Changes gravity to moon gravity.");
+    }
+
+    @EventPointer
+    private final EventListener<EventPlayerPackets> eventPlayerPacketsEventListener = new EventListener<>(event -> {
+	boolean gravity = Wrapper.INSTANCE.getOptions().sneakKey.isPressed();
+	 Vec3d orig = Wrapper.INSTANCE.getPlayer().getVelocity();
+        if (gravity) {
+            Wrapper.INSTANCE.getLocalPlayer().setVelocity(orig.getX(), orig.getY + 0.0568000030517578, orig.getZ());
+        }
+    }, new PlayerPacketsFilter(EventPlayerPackets.Mode.PRE));
+}
