9bc6e269dd6d4205ea418a4d43ec3dd547ba9c05
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-24 11:34:09 +0500

    Update Gravity.java

--- src/main/java/me/dustin/jex/feature/mod/impl/movement/Gravity.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/movement/Gravity.java
@@ -6,6 +6,7 @@ import me.dustin.jex.event.filters.PlayerPacketsFilter;
 import me.dustin.jex.event.player.EventPlayerPackets;
 import me.dustin.jex.feature.mod.core.Category;
 import me.dustin.jex.helper.entity.EntityHelper;
+import me.dustin.jex.event.player.EventMove;
 import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.helper.player.PlayerHelper;
 import me.dustin.jex.feature.mod.core.Feature;
