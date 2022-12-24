73e3089db67f3d91a5663347088506b58c077f10
mdenials <46415607+mdenials@users.noreply.github.com>
2022-12-24 11:33:14 +0500

    Update AirJump.java

--- src/main/java/me/dustin/jex/feature/mod/impl/movement/AirJump.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/movement/AirJump.java
@@ -5,7 +5,6 @@ import me.dustin.events.core.annotate.EventPointer;
 import me.dustin.jex.event.filters.PlayerPacketsFilter;
 import me.dustin.jex.event.player.EventPlayerPackets;
 import me.dustin.jex.feature.mod.core.Category;
-import me.dustin.jex.helper.entity.EntityHelper;
 import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.helper.player.PlayerHelper;
 import me.dustin.jex.feature.mod.core.Feature;
