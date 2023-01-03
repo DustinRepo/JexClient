3797f311370b81dcd54e134e865473bea61b9845
mdenials <46415607+mdenials@users.noreply.github.com>
2023-01-03 21:11:41 +0700

    Update JetpackFly.java

--- src/main/java/me/dustin/jex/feature/mod/impl/movement/fly/impl/JetpackFly.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/movement/fly/impl/JetpackFly.java
@@ -2,6 +2,7 @@ package me.dustin.jex.feature.mod.impl.movement.fly.impl;
 
 import me.dustin.events.core.Event;
 import me.dustin.jex.event.player.EventPlayerPackets;
+import me.dustin.jex.feature.mod.core.Feature;
 import me.dustin.jex.feature.mod.core.FeatureExtension;
 import me.dustin.jex.feature.mod.impl.movement.fly.Fly;
 import me.dustin.jex.helper.misc.Wrapper;
