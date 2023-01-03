775498bf9baf4d4de3dec81a8f29b0718228ccf7
mdenials <46415607+mdenials@users.noreply.github.com>
2023-01-03 20:31:38 +0700

    Update JetpackFly.java

--- src/main/java/me/dustin/jex/feature/mod/impl/movement/fly/impl/JetpackFly.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/movement/fly/impl/JetpackFly.java
@@ -1,25 +1,24 @@
 package me.dustin.jex.feature.mod.impl.movement.fly.impl;
 
-import me.dustin.events.core.EventListener;
-import me.dustin.jex.event.filters.PlayerPacketsFilter;
+import me.dustin.events.core.Event;
+import me.dustin.jex.event.player.EventPlayerPackets;
 import me.dustin.jex.feature.mod.core.FeatureExtension;
 import me.dustin.jex.feature.mod.impl.movement.fly.Fly;
-import me.dustin.jex.event.player.EventPlayerPackets;
 import me.dustin.jex.helper.misc.Wrapper;
-import me.dustin.jex.helper.player.PlayerHelper;
-import me.dustin.jex.feature.mod.core.Feature;
 
-public class JetpackFly extends FeatureExtension {
+public class CreativeFly extends FeatureExtension {
 
-    public JetpackFly() {
-        super(Fly.Mode.JETPACK, Fly.class);
+    public CreativeFly() {
+        super(Fly.Mode.CREATIVE, Fly.class);
     }
 
     @Override
-    private final EventListener<EventPlayerPackets> eventPlayerPacketsEventListener = new EventListener<>(event -> {
+    public void pass(Event event) {
+        if (event instanceof EventPlayerPackets eventPlayerPackets && eventPlayerPackets.getMode() == EventPlayerPackets.Mode.PRE) {
 	boolean jumping = Wrapper.INSTANCE.getOptions().jumpKey.isPressed();
         if (jumping) {
             Wrapper.INSTANCE.getLocalPlayer().jump();
-        }
-    }, new PlayerPacketsFilter(EventPlayerPackets.Mode.PRE));
+}    
+}
+}
 }
