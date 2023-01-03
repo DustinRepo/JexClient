fc4fea31a61dda6ada8c5434bb8f29815f627bd1
mdenials <46415607+mdenials@users.noreply.github.com>
2023-01-03 19:40:45 +0700

    Update JetpackFly.java

--- src/main/java/me/dustin/jex/feature/mod/impl/movement/fly/impl/JetpackFly.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/movement/fly/impl/JetpackFly.java
@@ -1,7 +1,6 @@
 package me.dustin.jex.feature.mod.impl.movement.fly.impl;
 
 import me.dustin.events.core.EventListener;
-import me.dustin.events.core.annotate.EventPointer;
 import me.dustin.jex.event.filters.PlayerPacketsFilter;
 import me.dustin.jex.feature.mod.core.FeatureExtension;
 import me.dustin.jex.feature.mod.impl.movement.fly.Fly;
@@ -16,7 +15,7 @@ public class JetpackFly extends FeatureExtension {
         super(Fly.Mode.JETPACK, Fly.class);
     }
 
-    @EventPointer
+    @Override
     private final EventListener<EventPlayerPackets> eventPlayerPacketsEventListener = new EventListener<>(event -> {
 	boolean jumping = Wrapper.INSTANCE.getOptions().jumpKey.isPressed();
         if (jumping) {
