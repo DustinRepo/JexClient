0f21ecd3e945ee182ac70f672288686850f5d43b
mdenials <46415607+mdenials@users.noreply.github.com>
2023-01-10 14:35:17 +0500

    Update AttackClicker.java

--- src/main/java/me/dustin/jex/feature/mod/impl/combat/AttackClicker.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/combat/AttackClicker.java
@@ -3,6 +3,7 @@ package me.dustin.jex.feature.mod.impl.combat;
 import me.dustin.events.core.EventListener;
 import me.dustin.events.core.annotate.EventPointer;
 import me.dustin.jex.event.filters.PlayerPacketsFilter;
+import me.dustin.jex.helper.misc.StopWatch;
 import me.dustin.jex.event.player.EventPlayerPackets;
 import me.dustin.jex.feature.mod.core.Category;
 import me.dustin.jex.feature.mod.core.Feature;
@@ -18,8 +19,9 @@ public class AttackClicker extends Feature {
     @EventPointer
     private final EventListener<EventPlayerPackets> eventPlayerPacketsEventListener = new EventListener<>(event -> {
              boolean attack = Wrapper.INSTANCE.getOptions().attackKey.isPressed();
-            if (attack && Wrapper.INSTANCE.getLocalPlayer().getAttackCooldownProgress(0) == 1) {
+            if (attack && stopWatch.hasPassed(1000)) {
                 Wrapper.INSTANCE.getLocalPlayer().swingHand(Hand.MAIN_HAND);
+                stopWatch.reset();
             }
     }, new PlayerPacketsFilter(EventPlayerPackets.Mode.PRE));
 }
