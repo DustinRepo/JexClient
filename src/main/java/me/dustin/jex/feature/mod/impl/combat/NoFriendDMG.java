a0592bc500289b999c804d4a9fca027b0de74998
mdenials <46415607+mdenials@users.noreply.github.com>
2023-01-12 10:21:31 +0500

    Update NoFriendDMG.java

--- src/main/java/me/dustin/jex/feature/mod/impl/combat/NoFriendDMG.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/combat/NoFriendDMG.java
@@ -7,9 +7,41 @@ import me.dustin.jex.feature.mod.core.Category;
 import me.dustin.jex.helper.entity.EntityHelper;
 import me.dustin.jex.helper.player.FriendHelper;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.mob.MobEntity;
 import me.dustin.jex.feature.mod.core.Feature;
 
 public class NoFriendDMG extends Feature {
+    
+     public final Property<Boolean> neutralProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("Neutral")
+            .value(false)
+            .build();
+    public final Property<Boolean> passiveProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("Passive")
+            .value(true)
+            .build();
+    public final Property<Boolean> specificFilterProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("Specific Filter")
+            .value(true)
+            .build();
+    public final Property<Boolean> ironGolemProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("Iron Golem")
+            .value(true)
+            .parent(specificFilterProperty)
+            .depends(parent -> (boolean) parent.value())
+            .build();
+    public final Property<Boolean> piglinProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("Piglin")
+            .value(true)
+            .parent(specificFilterProperty)
+            .depends(parent -> (boolean) parent.value())
+            .build();
+    public final Property<Boolean> zombiePiglinProperty = new Property.PropertyBuilder<Boolean>(this.getClass())
+            .name("Zombie Piglin")
+            .value(false)
+            .parent(specificFilterProperty)
+            .depends(parent -> (boolean) parent.value())
+            .build();
 
     public NoFriendDMG() {
         super(Category.COMBAT, "Prevent yourself from attacking your friends and pets");
@@ -22,5 +54,24 @@ public class NoFriendDMG extends Feature {
                 event.cancel();
         } else if (EntityHelper.INSTANCE.doesPlayerOwn(event.getEntity()))
             event.cancel();
+        
+        if (event.getEntity instanceof MobEntity) {
+        if (neutralProperty.value()) {
+        if (EntityHelper.INSTANCE.isPassiveMob(event.getEntity()))
+            event.cancel();
+        }
+          if (passiveProperty.value()) {
+        if (EntityHelper.INSTANCE.isNeutralMob(entity))
+            event.cancel();
+          }
+            if (specificFilterProperty.value()) {
+            if (entity instanceof IronGolemEntity)
+                return ironGolemProperty.value();
+            if (entity instanceof ZombifiedPiglinEntity)
+                return zombiePiglinProperty.value();
+            if (entity instanceof PiglinEntity)
+                return piglinProperty.value();
+        }
+}
     });
 }
