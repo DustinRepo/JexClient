ea1861db914317f37ba54ddf297ae6f07c34fc66
mdenials <46415607+mdenials@users.noreply.github.com>
2023-01-15 11:59:27 +0500

    Update Category.java

--- src/main/java/me/dustin/jex/feature/mod/core/Category.java
+++ src/main/java/me/dustin/jex/feature/mod/core/Category.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 
 public record Category(String name, int color) {
     private static final ArrayList<Category> categories = new ArrayList<>();
-    public static final Category COMBAT = new Category("Combat", new Color(255, 61, 56).getRGB());
-    public static final Category PLAYER = new Category("Player", new Color(64, 255, 83).getRGB());
-    public static final Category MOVEMENT = new Category("Movement", new Color(141, 95, 255).getRGB());
-    public static final Category WORLD = new Category("World", new Color(74, 84, 255).getRGB());
-    public static final Category VISUAL = new Category("Visual", new Color(255, 92, 252).getRGB());
+    public static final Category COMBAT = new Category("Combat", new Color(113, 12, 20).getRGB());
+    public static final Category PLAYER = new Category("Player", new Color(22, 116, 38).getRGB());
+    public static final Category MOVEMENT = new Category("Movement", new Color(106, 16, 116).getRGB());
+    public static final Category WORLD = new Category("World", new Color(18, 81, 125).getRGB());
+    public static final Category VISUAL = new Category("Visual", new Color(251, 80, 8).getRGB());
     public static final Category MISC = new Category("Misc", new Color(247, 255, 65).getRGB());
 
     public Category(String name, int color) {
