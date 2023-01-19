da4d7cd977412466e760bda5dfe51050f4d9cd7d
mdenials <46415607+mdenials@users.noreply.github.com>
2023-01-19 08:43:39 +0500

    Update MixinCamera.java

--- src/main/java/me/dustin/jex/load/mixin/minecraft/MixinCamera.java
+++ src/main/java/me/dustin/jex/load/mixin/minecraft/MixinCamera.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @Mixin(Camera.class)
 public class MixinCamera {
 
-    @Inject(method = "clipToSpace", at = @At(value = "INVOKE", target = "net/minecraft/util/math/Vec3d.distanceTo(Lnet/minecraft/util/math/Vec3d;)D"), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
+    @Inject(method = "clipToSpace", at = @At(value = "HEAD", target = "net/minecraft/util/math/Vec3d.distanceTo(Lnet/minecraft/util/math/Vec3d;)D"), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
     public void clipToSpace(double double_1, CallbackInfoReturnable<Double> cir, int int_1, float float_1, float float_2, float float_3, Vec3d vec3d_1, Vec3d vec3d_2, HitResult hitResult_1) {
         EventClipCamera eventClipCamera = new EventClipCamera().run();
         if (eventClipCamera.isCancelled())
