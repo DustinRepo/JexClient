1741cd809e0fbf22a90c2bd10513b6611be97128
mdenials <46415607+mdenials@users.noreply.github.com>
2023-01-20 20:18:29 +0500

    Update FastPlace.java

--- src/main/java/me/dustin/jex/feature/mod/impl/player/FastPlace.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/player/FastPlace.java
@@ -4,11 +4,21 @@ import me.dustin.events.core.EventListener;
 import me.dustin.events.core.annotate.EventPointer;
 import me.dustin.jex.event.filters.PlayerPacketsFilter;
 import me.dustin.jex.event.player.EventPlayerPackets;
+import me.dustin.jex.feature.property.Property;
 import me.dustin.jex.feature.mod.core.Category;
 import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.feature.mod.core.Feature;
 
 public class FastPlace extends Feature {
+	
+	public Property<Integer> delayProperty = new Property.PropertyBuilder<Integer>(this.getClass())
+            .name("RightClickDelay (Tick)")
+            .description("The amount of ticks to wait between breaking blocks. Default MC is 5.")
+            .value(1)
+            .min(0)
+            .max(3)
+            .inc(1)
+            .build();
 
 	public FastPlace() {
 		super(Category.PLAYER, "Place fast.");
@@ -16,7 +26,7 @@ public class FastPlace extends Feature {
 
 	@EventPointer
 	private final EventListener<EventPlayerPackets> eventPlayerPacketsEventListener = new EventListener<>(event -> {
-		Wrapper.INSTANCE.getIMinecraft().setRightClickDelayTimer(0);
+		Wrapper.INSTANCE.getIMinecraft().setRightClickDelayTimer(delayProperty.value());
 	}, new PlayerPacketsFilter(EventPlayerPackets.Mode.PRE));
 
 	@Override
