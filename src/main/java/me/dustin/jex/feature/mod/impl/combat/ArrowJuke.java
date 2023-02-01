41a9d6cad3b59c85fd3bbc46135c906242780a19
mdenials <46415607+mdenials@users.noreply.github.com>
2023-02-01 15:32:29 +0500

    Update ArrowJuke.java

--- src/main/java/me/dustin/jex/feature/mod/impl/combat/ArrowJuke.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/combat/ArrowJuke.java
@@ -68,6 +68,7 @@ private final List<Vec3d> possibleMoveDirections = Arrays.asList(
                 Vec3d velocity = direction.multiply(speedProperty.value());
                     move(velocity);
                     didMove = true;
+		    break;
 	    }
 		if (didMove) break;
 		    speed += speedProperty.value();
