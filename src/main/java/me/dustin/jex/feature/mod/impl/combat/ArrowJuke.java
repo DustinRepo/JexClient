4fdca46b5d9fd7dbac37502818d2b0b50898ff4a
mdenials <46415607+mdenials@users.noreply.github.com>
2023-02-01 14:03:55 +0500

    Update ArrowJuke.java

--- src/main/java/me/dustin/jex/feature/mod/impl/combat/ArrowJuke.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/combat/ArrowJuke.java
@@ -51,8 +51,9 @@ private final List<Vec3d> possibleMoveDirections = Arrays.asList(
 
     @EventPointer
     public final EventListener<EventMove> eventMoveEventListener = new EventListener<>(event -> Wrapper.INSTANCE.getWorld().getEntities().forEach(entity -> {
-	    Vec3d vel;
-	    Vec3d move = move(vel.x, vel.y, vel.z);
+private void move(Vec3d vel) {
+   move(vel.x, vel.y, vel.z);
+    }
 	    float speed = speedProperty.value();
 	    if (entity instanceof ArrowEntity arrowEntity) {
               if (arrowEntity.age < 75) {
@@ -60,7 +61,6 @@ private final List<Vec3d> possibleMoveDirections = Arrays.asList(
             boolean didMove = false;
             Collections.shuffle(possibleMoveDirections);
             for (Vec3d direction : possibleMoveDirections) {
-		    Vec3d vel;
                 Vec3d velocity = direction.multiply(speedProperty.value());
                     move(velocity);
                     didMove = true;
