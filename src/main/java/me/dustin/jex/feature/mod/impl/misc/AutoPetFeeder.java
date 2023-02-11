cfca4cc5c20f1cfad0d32037153f678523772982
mdenials <46415607+mdenials@users.noreply.github.com>
2023-02-11 15:57:05 +0500

    Update AutoPetFeeder.java

--- src/main/java/me/dustin/jex/feature/mod/impl/misc/AutoPetFeeder.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/misc/AutoPetFeeder.java
@@ -89,7 +89,7 @@ if (modeProperty.value() == TargetMode.CAT) {
         return InventoryHelper.INSTANCE.getFromHotbar(Items.COOKED_RABBIT);
     }
 	
-private void int getCatFoodSlot() {
+public int getCatFoodSlot() {
         int cod = InventoryHelper.INSTANCE.getFromHotbar(Items.COD);
         if (cod != -1)
             return cod;
