7740727fcebd5469facd303a9f85b5dcc10e054f
mdenials <46415607+mdenials@users.noreply.github.com>
2023-02-13 18:14:36 +0500

    Update Triggerbot.java

--- src/main/java/me/dustin/jex/feature/mod/impl/combat/Triggerbot.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/combat/Triggerbot.java
@@ -14,6 +14,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.Hand;
 
 public class Triggerbot extends Feature {
 
