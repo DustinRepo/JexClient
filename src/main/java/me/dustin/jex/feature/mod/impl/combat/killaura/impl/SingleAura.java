89906243639b96b1d724e365fa1b3c6a49110ccf
mdenials <46415607+mdenials@users.noreply.github.com>
2023-02-13 18:17:00 +0500

    Update SingleAura.java

--- src/main/java/me/dustin/jex/feature/mod/impl/combat/killaura/impl/SingleAura.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/combat/killaura/impl/SingleAura.java
@@ -118,7 +118,7 @@ public class SingleAura extends FeatureExtension {
                     PlayerHelper.INSTANCE.unblock();
                 }
                 Wrapper.INSTANCE.getClientPlayerInteractionManager().attackEntity(Wrapper.INSTANCE.getLocalPlayer(), target);
-                if (swingProperty.value())
+                if (KillAura.INSTANCE.swingProperty.value())
                 PlayerHelper.INSTANCE.swing(Hand.MAIN_HAND);
                 if (KillAura.INSTANCE.autoBlockProperty.value() && reblock) {
                     PlayerHelper.INSTANCE.block(KillAura.INSTANCE.ignoreNewCombatProperty.value());
