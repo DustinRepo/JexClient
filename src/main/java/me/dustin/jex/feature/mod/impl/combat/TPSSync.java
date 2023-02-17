e776cb934aff9a2d4a39fdf16e44e09296617d0a
mdenials <46415607+mdenials@users.noreply.github.com>
2023-02-17 12:55:02 +0500

    Update TPSSync.java

--- src/main/java/me/dustin/jex/feature/mod/impl/combat/TPSSync.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/combat/TPSSync.java
@@ -10,14 +10,6 @@ import me.dustin.jex.feature.mod.core.Feature;
 
 public class TPSSync extends Feature {
 
-    public Property<Integer> sampleSizeProperty = new Property.PropertyBuilder<Integer>(this.getClass())
-            .name("Sample Size")
-            .description("The amount of ticks to store for the average.")
-            .value(15)
-            .min(2)
-            .max(100)
-            .build();
-
     public TPSSync() {
         super(Category.COMBAT, "Sync attacks to server TPS to deal more damage on laggy servers.");
     }
