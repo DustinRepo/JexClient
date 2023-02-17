9bd6b6a6bc84ad65a5a6b223479a9461308b4a8b
mdenials <46415607+mdenials@users.noreply.github.com>
2023-02-17 12:13:24 +0500

    Update Criticals.java

--- src/main/java/me/dustin/jex/feature/mod/impl/combat/Criticals.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/combat/Criticals.java
@@ -48,6 +48,7 @@ public class Criticals extends Feature {
     }
     
     private final StopWatch stopWatch = new StopWatch();
+    Long delay = delayProperty.value() * 1000
 
     @EventPointer
     private final EventListener<EventAttackEntity> eventAttackEntityEventListener = new EventListener<>(event -> {
@@ -64,7 +65,7 @@ public class Criticals extends Feature {
     });
 
     public void crit() {
-        if (stopWatch.hasPassed(delayProperty.value() * 1000))
+        if (stopWatch.hasPassed(delay))
         if (Wrapper.INSTANCE.getLocalPlayer().isOnGround()) {
             Wrapper.INSTANCE.getLocalPlayer().networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(Wrapper.INSTANCE.getLocalPlayer().getX(), Wrapper.INSTANCE.getLocalPlayer().getY() + 0.05F, Wrapper.INSTANCE.getLocalPlayer().getZ(), false));
             Wrapper.INSTANCE.getLocalPlayer().networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(Wrapper.INSTANCE.getLocalPlayer().getX(), Wrapper.INSTANCE.getLocalPlayer().getY(), Wrapper.INSTANCE.getLocalPlayer().getZ(), false));
