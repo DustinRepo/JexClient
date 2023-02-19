14261b8d6e62aadc77459d8fdd92b14273f675f1
mdenials <46415607+mdenials@users.noreply.github.com>
2023-02-19 17:46:53 +0500

    Update TypeResolver.java

--- src/main/java/net/jodah/typetools/TypeResolver.java
+++ src/main/java/net/jodah/typetools/TypeResolver.java
@@ -14,7 +14,7 @@ package net.jodah.typetools;/*
  * limitations under the License.
  */
 
-import java.lang.ref.Unsafe;
+import jdk.internal.misc.Unsafe;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
