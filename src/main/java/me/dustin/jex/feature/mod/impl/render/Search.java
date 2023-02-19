ef64fddad95aaaf9ac6212380cc8d79c7f2da6b5
mdenials <46415607+mdenials@users.noreply.github.com>
2023-02-19 21:28:48 +0500

    Update Search.java

--- src/main/java/me/dustin/jex/feature/mod/impl/render/Search.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/render/Search.java
@@ -61,7 +61,6 @@ public class Search extends Feature {
 
     private static final ConcurrentMap<Block, Integer> blocks = Maps.newConcurrentMap();
     private static final ConcurrentMap<BlockPos, Block> worldBlocks = Maps.newConcurrentMap();
-    Blockpos pos;
 
     private Thread thread;
     private final ConcurrentLinkedQueue<Chunk> chunksToUpdate = new ConcurrentLinkedQueue<>();
@@ -111,6 +110,7 @@ public class Search extends Feature {
 
     @EventPointer
     private final EventListener<EventRender3D> eventRender3DEventListener = new EventListener<>(event -> {
+        Blockpos pos;
         if (blocks == null || blocks.isEmpty())
             return;
         ArrayList<Render3DHelper.BoxStorage> boxList = new ArrayList<>();
@@ -134,6 +134,7 @@ public class Search extends Feature {
 
     @EventPointer
     private final EventListener<EventRender3D.EventRender3DNoBob> eventRender3DNoBobEventListener = new EventListener<>(event -> {
+        Blockpos pos;
         if (!tracersProperty.value())
             return;
         for (Map.Entry <BlockPos, Block> entry : worldBlocks.entrySet()) {
