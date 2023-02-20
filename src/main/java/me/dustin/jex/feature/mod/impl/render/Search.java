a5e7f8fc5a0d8bd542ec59212c6898973cb7966c
mdenials <46415607+mdenials@users.noreply.github.com>
2023-02-20 14:28:58 +0500

    Update Search.java

--- src/main/java/me/dustin/jex/feature/mod/impl/render/Search.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/render/Search.java
@@ -117,7 +117,7 @@ public class Search extends Feature {
         for (Map.Entry <BlockPos, Block> entry : worldBlocks.entrySet()) {
             Block block = worldBlocks.get(pos);
             if (WorldHelper.INSTANCE.getBlock(pos) != block) {
-                entry.remove(pos);
+                entry.remove(worldBlocks);
                 continue;
             }
             Entity cameraEntity = Wrapper.INSTANCE.getMinecraft().getCameraEntity();
@@ -140,7 +140,7 @@ public class Search extends Feature {
         for (Map.Entry <BlockPos, Block> entry : worldBlocks.entrySet()) {
             Block block = worldBlocks.get(pos);
             if (!blocks.containsKey(block) || WorldHelper.INSTANCE.getBlock(pos) != block) {
-                entry.remove(pos);
+                entry.remove(worldBlocks);
                 continue;  
             }
             Entity cameraEntity = Wrapper.INSTANCE.getMinecraft().getCameraEntity();
