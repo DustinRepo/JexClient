a1e064711bb75ff705b7051d4a00ead77532d1a5
mdenials <46415607+mdenials@users.noreply.github.com>
2023-02-23 12:58:36 +0500

    Update ConnectedServerHelper.java

--- src/main/java/me/dustin/jex/helper/network/ConnectedServerHelper.java
+++ src/main/java/me/dustin/jex/helper/network/ConnectedServerHelper.java
@@ -8,19 +8,13 @@ import net.minecraft.client.network.ServerAddress;
 public enum ConnectedServerHelper {
     INSTANCE;
     private ServerAddress serverAddress;
-    private ServerPort serverPort;
 
     @EventPointer
     private final EventListener<EventConnect> eventConnectEventListener = new EventListener<>(event -> {
-        this.serverAddress = event.getServerAddress();
-        this.serverPort = event.getServerPort();
-        
+        this.serverAddress = event.getServerAddress();  
     });
 
     public ServerAddress getServerAddress() {
         return serverAddress;
-    }
-    public ServerAddress getServerPort() {
-        return serverPort;
     }
 }
