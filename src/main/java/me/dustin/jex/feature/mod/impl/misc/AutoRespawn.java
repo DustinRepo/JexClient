222c6ef487537419916562eff5a71812f6482f39
mdenials <46415607+mdenials@users.noreply.github.com>
2023-03-09 08:28:46 +0500

    Update AutoRespawn.java

--- src/main/java/me/dustin/jex/feature/mod/impl/misc/AutoRespawn.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/misc/AutoRespawn.java
@@ -11,7 +11,7 @@ import me.dustin.jex.feature.mod.core.Feature;
 public class AutoRespawn extends Feature {
 
     public AutoRespawn() {
-        super(Category.MISC, "Respawn without having to click anything.");
+        super(Category.MISC, "");
     }
 
     @EventPointer
