2ca6308b1da9fa3815e3753aa488555e06cfdaf3
mdenials <46415607+mdenials@users.noreply.github.com>
2023-05-23 10:36:56 +0500

    Update AntiKnockback.java

--- src/main/java/me/dustin/jex/feature/mod/impl/combat/AntiKnockback.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/combat/AntiKnockback.java
@@ -13,42 +13,42 @@ import java.lang.Math;
 
 public class AntiKnockback extends Feature {
 
-    public final Property<Integer> percentxProperty = new Property.PropertyBuilder<Integer>(this.getClass())
+    public final Property<Integer> pxProperty = new Property.PropertyBuilder<Integer>(this.getClass())
             .name("PercentX")
             .value(0)
             .min(-100)
             .max(100)
             .inc(2)
             .build();
-    public final Property<Integer> percentyProperty = new Property.PropertyBuilder<Integer>(this.getClass())
+    public final Property<Integer> pyProperty = new Property.PropertyBuilder<Integer>(this.getClass())
             .name("PercentY")
             .value(0)
             .min(-100)
             .max(100)
             .inc(2)
             .build();
-    public final Property<Integer> percentzProperty = new Property.PropertyBuilder<Integer>(this.getClass())
+    public final Property<Integer> pzProperty = new Property.PropertyBuilder<Integer>(this.getClass())
             .name("PercentZ")
             .value(0)
             .min(-100)
             .max(100)
             .inc(2)
             .build();
-    public final Property<Integer> epercentxProperty = new Property.PropertyBuilder<Integer>(this.getClass())
+    public final Property<Integer> epxProperty = new Property.PropertyBuilder<Integer>(this.getClass())
             .name("ExplosionX")
             .value(0)
             .min(-100)
             .max(100)
             .inc(2)
             .build();
-    public final Property<Integer> epercentyProperty = new Property.PropertyBuilder<Integer>(this.getClass())
+    public final Property<Integer> epyProperty = new Property.PropertyBuilder<Integer>(this.getClass())
             .name("ExplosionY")
             .value(0)
             .min(-100)
             .max(100)
             .inc(2)
             .build();
-    public final Property<Integer> epercentzProperty = new Property.PropertyBuilder<Integer>(this.getClass())
+    public final Property<Integer> epzProperty = new Property.PropertyBuilder<Integer>(this.getClass())
             .name("ExplosionZ")
             .value(0)
             .min(-100)
@@ -62,39 +62,22 @@ public class AntiKnockback extends Feature {
 
     @EventPointer
     private final EventListener<EventExplosionVelocity> eventExplosionVelocityEventListener = new EventListener<>(event -> {
-        float epx = epercentxProperty.value() / 100.0f;
-        float epy = epercentyProperty.value() / 100.0f;
-        float epz = epercentzProperty.value() / 100.0f;
-        if (epercentxProperty.value() == 0){
-        if (epercentyProperty.value() == 0){
-        if (epercentzProperty.value() == 0){
-            event.cancel();
-        }
-        }
-        }
-        else {
-            event.setMultX(epx);
-            event.setMultY(epy);
-            event.setMultZ(epz);
-        }
+        float epx = epxProperty.value() / 100.0f;
+        float epy = epyProperty.value() / 100.0f;
+        float epz = epzProperty.value() / 100.0f;
+        event.setMultX(epx);
+        event.setMultY(epy);
+        event.setMultZ(epz);
+
     });
 
     @EventPointer
     private final EventListener<EventPlayerVelocity> eventPlayerVelocityEventListener = new EventListener<>(event -> {
-        float px = percentxProperty.value() / 100.0f;
-        float py = percentyProperty.value() / 100.0f;
-        float pz = percentzProperty.value() / 100.0f;
-        if (percentxProperty.value() == 0) {
-        if (percentyProperty.value() == 0) {
-        if (percentzProperty.value() == 0) {
-            event.cancel();
-         }
-         }
-         }
-        else {
-            event.setVelocityX((int)(event.getVelocityX() * px));
-            event.setVelocityY((int)(event.getVelocityY() * py));
-            event.setVelocityZ((int)(event.getVelocityZ() * pz));
-        }
+        float px = pxProperty.value() / 100.0f;
+        float py = pyProperty.value() / 100.0f;
+        float pz = pzProperty.value() / 100.0f;
+        event.setVelocityX((int)(event.getVelocityX() * px));
+        event.setVelocityY((int)(event.getVelocityY() * py));
+        event.setVelocityZ((int)(event.getVelocityZ() * pz));
     });
 }
