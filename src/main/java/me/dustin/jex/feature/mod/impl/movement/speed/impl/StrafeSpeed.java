ea0f4088ca7bb6387e00077af5b0f7892704030e
mdenials <46415607+mdenials@users.noreply.github.com>
2023-06-03 20:33:38 +0500

    Update StrafeSpeed.java

--- src/main/java/me/dustin/jex/feature/mod/impl/movement/speed/impl/StrafeSpeed.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/movement/speed/impl/StrafeSpeed.java
@@ -25,15 +25,15 @@ public class StrafeSpeed extends FeatureExtension {
             if ((BaritoneHelper.INSTANCE.isBaritoneRunning() || PathingHelper.INSTANCE.isPathing()) && !Wrapper.INSTANCE.getLocalPlayer().isOnGround())
                 return;
             if (PathingHelper.INSTANCE.isPathing()) {
-                eventMove.setX(eventMove.getX() * speed.vanillaSpeedProperty.value() * kmh);
-                eventMove.setZ(eventMove.getZ() * speed.vanillaSpeedProperty.value() * kmh);
+                eventMove.setX(eventMove.getX() * speed.multipleProperty.value() * kmh);
+                eventMove.setZ(eventMove.getZ() * speed.multipleProperty.value() * kmh);
             } else
                 PlayerHelper.INSTANCE.setMoveSpeed(eventMove, speed.strafeSpeedProperty.value() * kmh);
         } else if (event instanceof EventPlayerPackets eventPlayerPackets) {
             Speed speed = Feature.get(Speed.class);
             if (eventPlayerPackets.getMode() == EventPlayerPackets.Mode.PRE) {
                 if (Wrapper.INSTANCE.getLocalPlayer().isOnGround() && PlayerHelper.INSTANCE.isMoving())
-                    PlayerHelper.INSTANCE.setVelocityY(speed.hopAmountProperty.value() * kmh);
+                    PlayerHelper.INSTANCE.setVelocityY(speed.hopAmountProperty.value());
             }
         }
     }
