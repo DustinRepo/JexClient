6eb081554be58e2169584bdd5ea8fa0491c8640d
mdenials <46415607+mdenials@users.noreply.github.com>
2023-06-12 10:43:37 +0500

    Update ChestStealer.java

--- src/main/java/me/dustin/jex/feature/mod/impl/world/ChestStealer.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/world/ChestStealer.java
@@ -64,7 +64,7 @@ public class ChestStealer extends Feature {
         Wrapper.INSTANCE.getMinecraft().currentScreen = screen;
         if (!stopWatch.hasPassed(delayProperty.value()))
             return;
-        if (Wrapper.INSTANCE.getMinecraft().currentScreen instanceof HandledScreen && isValid(screen)) {
+        if (Wrapper.INSTANCE.getMinecraft().currentScreen instanceof GenericContainerScreen && chestProperty.value()|| instanceof ShulkerBoxScreen && shulkerProperty.value()|| instanceof HopperScreen && hopperProperty.value()|| instanceof HorseScreen && horseProperty.value()|| instanceof BrewingStandScreen && bsProperty.value()) {
             if (InventoryHelper.INSTANCE.isInventoryFull() && !dumpProperty.value()) {
                 Wrapper.INSTANCE.getLocalPlayer().closeHandledScreen();
                 return;
@@ -87,16 +87,4 @@ public class ChestStealer extends Feature {
             }
         }
     }, new PlayerPacketsFilter(EventPlayerPackets.Mode.PRE));
-private boolean isValid() {
-if (Wrapper.INSTANCE.getMinecraft().currentScreen instanceof GenericContainerScreen)
-return chestProperty.value();
-if (Wrapper.INSTANCE.getMinecraft().currentScreen instanceof ShulkerBoxScreen)
-return shulkerProperty.value();
-if (Wrapper.INSTANCE.getMinecraft().currentScreen instanceof HopperScreen)
-return hopperProperty.value();
-if (Wrapper.INSTANCE.getMinecraft().currentScreen instanceof HorseScreen)
-return horseProperty.value();
-if (Wrapper.INSTANCE.getMinecraft().currentScreen instanceof BrewingStandScreen)
-return bsProperty.value();
-   }
 }
