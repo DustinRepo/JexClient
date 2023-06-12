2b2c40ca975837adbd1c485cbeb290ca7714bf5c
mdenials <46415607+mdenials@users.noreply.github.com>
2023-06-12 13:07:43 +0500

    Update NormalFly.java

--- src/main/java/me/dustin/jex/feature/mod/impl/movement/fly/impl/NormalFly.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/movement/fly/impl/NormalFly.java
@@ -1,7 +1,7 @@
 package me.dustin.jex.feature.mod.impl.movement.fly.impl;
 
 import me.dustin.events.core.Event;
-import me.dustin.jex.event.player.EventPlayerPackets;
+import me.dustin.jex.event.player.EventMove;
 import me.dustin.jex.feature.mod.core.FeatureExtension;
 import me.dustin.jex.feature.mod.core.Feature;
 import me.dustin.jex.feature.mod.impl.movement.fly.Fly;
@@ -17,14 +17,14 @@ public class NormalFly extends FeatureExtension {
 
     @Override
     public void pass(Event event) {
-        if (event instanceof EventPlayerPackets eventPlayerPackets && eventPlayerPackets.getMode() == EventPlayerPackets.Mode.PRE) {
+        if (event instanceof EventMove eventMove) {
             if (fly == null)
                 fly = Feature.get(Fly.class);
             boolean jumping = Wrapper.INSTANCE.getOptions().jumpKey.isPressed();
             boolean sneaking = Wrapper.INSTANCE.getOptions().sneakKey.isPressed();
             double hkmh = fly.hspeedProperty.value() * fly.multipleProperty.value() * 0.01388888888888;
             double vkmh = fly.vspeedProperty.value() * fly.multipleProperty.value() * 0.01388888888888;
-            PlayerHelper.INSTANCE.setMoveSpeed(eventPlayerPackets, hkmh);
+            PlayerHelper.INSTANCE.setMoveSpeed(eventMove, hkmh);
             if (!PathingHelper.INSTANCE.isPathing() || PathingHelper.INSTANCE.isThinking()) {
                 PlayerHelper.INSTANCE.setVelocityX(0);
                 PlayerHelper.INSTANCE.setVelocityZ(0);
