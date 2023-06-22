f708c80f5b122d896161667f28574632525065cd
mdenials <46415607+mdenials@users.noreply.github.com>
2023-06-22 17:15:30 +0500

    Update SingleAimbot.java

--- src/main/java/me/dustin/jex/feature/mod/impl/combat/aimbot/impl/SingleAimbot.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/combat/aimbot/impl/SingleAimbot.java
@@ -10,6 +10,7 @@ import me.dustin.jex.feature.mod.impl.player.Freecam;
 import me.dustin.jex.feature.mod.impl.world.Excavator;
 import me.dustin.jex.helper.entity.EntityHelper;
 import me.dustin.jex.helper.math.vector.RotationVector;
+import me.dustin.jex.helper.misc.StopWatch;
 import me.dustin.jex.helper.misc.Wrapper;
 import me.dustin.jex.helper.player.PlayerHelper;
 import me.dustin.jex.helper.render.Render3DHelper;
@@ -24,6 +25,8 @@ import me.dustin.jex.feature.mod.impl.player.AutoEat;
 public class SingleAimbot extends FeatureExtension {
 
     private LivingEntity target;
+    private final StopWatch stopWatch = new StopWatch();
+
 
     public SingleAimbot() {
         super(Aimbot.TargetMode.SINGLE, Aimbot.class);
@@ -42,17 +45,21 @@ public class SingleAimbot extends FeatureExtension {
                 }
                 Aimbot.INSTANCE.setHasTarget(target != null);
                 if (target != null) {
+		if (stopWatch.hasPassed(Aimbot.INSTANCE.rotdelProperty.value())) {
                     if (Aimbot.INSTANCE.rotateProperty.value()) {
                         RotationVector rotationVector = PlayerHelper.INSTANCE.rotateToEntity(target);
+			    
                         if (Aimbot.INSTANCE.randomizeProperty.value()) {
                             rotationVector = PlayerHelper.INSTANCE.randomRotateTo(target, Aimbot.INSTANCE.randomWidthProperty.value(), Aimbot.INSTANCE.randomHeightProperty.value());
                         }
+			    
                         event.setRotation(rotationVector);
                         Wrapper.INSTANCE.getLocalPlayer().headYaw = event.getYaw();
                         Wrapper.INSTANCE.getLocalPlayer().bodyYaw = event.getYaw();
                         if (Aimbot.INSTANCE.lockviewProperty.value()) {
                             PlayerHelper.INSTANCE.setRotation(event.getRotation());
                         }
+		      }    
                     }
                 }
             }
