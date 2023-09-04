1f9cbc4afcdebe33dc031f7b5513d41d94cf3388
mdenials <46415607+mdenials@users.noreply.github.com>
2023-09-04 20:18:55 +0500

    Update Hud.java

--- src/main/java/me/dustin/jex/feature/mod/impl/render/hud/Hud.java
+++ src/main/java/me/dustin/jex/feature/mod/impl/render/hud/Hud.java
@@ -479,7 +479,7 @@ public class Hud extends Feature {
         CLIENT_COLOR, RAINBOW, CATEGORY
     }
     public enum DistanceMode {
-        BLOCKS, FEET, MILES, KM
+        BLOCKS, FEET, MILES, M, KM
     }
     public enum TimeMode {
         SECOND, TICK, MINUTE, HOUR, DAY
